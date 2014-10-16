8e3fe2e28ce462d34b3bf0dbeb9ec60bd4eac77b
Andrew Svetlov <andrew.svetlov@gmail.com>
2014-10-16 15:20:59 -0700

    Add logo

--- docs/conf.py
+++ docs/conf.py
@@ -141,12 +141,12 @@ html_theme = 'default'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = 'aiohttp-icon.svg'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = 'aiohttp-icon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
