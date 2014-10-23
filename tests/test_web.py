9ea0967d12eb2ea84427aa3ea44f1366e2a2970c
Andrew Svetlov <andrew.svetlov@gmail.com>
2014-10-23 10:24:31 -0700

    Add tests for finish callback

--- tests/test_web.py
+++ tests/test_web.py
@@ -39,3 +39,41 @@ class TestWeb(unittest.TestCase):
         asyncio.set_event_loop(self.loop)
         app = web.Application()
         self.assertIs(self.loop, app.loop)
+
+    def test_app_register_on_finish(self):
+        app = web.Application(loop=self.loop)
+        cb1 = mock.Mock()
+        cb2 = mock.Mock()
+        app.register_on_finish(cb1, 1, b=2)
+        app.register_on_finish(cb2, 2, c=3)
+        self.loop.run_until_complete(app.finish())
+        cb1.assert_called_once_with(1, b=2)
+        cb2.assert_called_once_with(2, c=3)
+
+    def test_app_register_coro(self):
+        app = web.Application(loop=self.loop)
+
+        fut = asyncio.Future(loop=self.loop)
+
+        @asyncio.coroutine
+        def cb():
+            yield from asyncio.sleep(0.001, loop=self.loop)
+            fut.set_result(123)
+
+        app.register_on_finish(cb)
+        self.loop.run_until_complete(app.finish())
+        self.assertTrue(fut.done())
+        self.assertEqual(123, fut.result())
+
+    def test_app_error_in_finish_callbacks(self):
+        app = web.Application(loop=self.loop)
+
+        err = RuntimeError('bad call')
+        app.register_on_finish(mock.Mock(side_effect=err))
+        handler = mock.Mock()
+        self.loop.set_exception_handler(handler)
+        self.loop.run_until_complete(app.finish())
+        exc_info = {'exception': err,
+                    'application': app,
+                    'message': 'Error in finish callback'}
+        handler.assert_called_once_with(self.loop, exc_info)
