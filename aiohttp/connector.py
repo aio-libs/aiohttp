7dd8a962d2dee129e1c613690eb078070704de20
Andrew Svetlov <andrew.svetlov@gmail.com>
2014-12-29 20:04:45 +0200

    Set ssl NO_COMPRESSION if present

--- aiohttp/connector.py
+++ aiohttp/connector.py
@@ -263,6 +263,7 @@ class TCPConnector(BaseConnector):
                 sslcontext = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
                 sslcontext.options |= ssl.OP_NO_SSLv2
                 sslcontext.options |= ssl.OP_NO_SSLv3
+                sslcontext.options |= getattr(ssl, "OP_NO_COMPRESSION", 0)
                 sslcontext.set_default_verify_paths()
                 sslcontext.verify_mode = ssl.CERT_REQUIRED
             self._ssl_context = sslcontext
