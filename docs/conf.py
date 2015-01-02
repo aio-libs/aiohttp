106ce9c4bfbac0548c404373e5434bfdcf708539
Andrew Svetlov <andrew.svetlov@gmail.com>
2015-01-02 05:31:40 +0200

    Add 2015 to copyrights

--- docs/conf.py
+++ docs/conf.py
@@ -68,7 +68,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'aiohttp'
-copyright = '2014, KeepSafe'
+copyright = '2013, 2014, 2015, KeepSafe'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
