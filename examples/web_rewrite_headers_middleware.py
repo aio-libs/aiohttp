57ebeae529a408b95b1ed2775449b44c7ad20f67
Andrew Svetlov <andrew.svetlov@gmail.com>
2015-02-04 20:23:36 +0200

    Add example for rewriting response headers in middleware

---
+++ examples/web_rewrite_headers_middleware.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Example for rewriting response headers by middleware.
+"""
+
+import asyncio
+from aiohttp.web import Application, Response
+
+
+@asyncio.coroutine
+def handler(request):
+    return Response(text="Everything is fine")
+
+
+@asyncio.coroutine
+def middleware_factory(app, next_handler):
+
+    @asyncio.coroutine
+    def middleware(request):
+        response = yield from next_handler(request)
+        if not response.started:
+            response.headers['SERVER'] = "Secured Server Software"
+        return response
+
+    return middleware
+
+
+@asyncio.coroutine
+def init(loop):
+    app = Application(loop=loop, middlewares=[middleware_factory])
+    app.router.add_route('GET', '/', handler)
+
+    requests_handler = app.make_handler()
+    srv = yield from loop.create_server(requests_handler, '127.0.0.1', 8080)
+    print("Server started at http://127.0.0.1:8080")
+    return srv, requests_handler
+
+
+loop = asyncio.get_event_loop()
+srv, requests_handler = loop.run_until_complete(init(loop))
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    loop.run_until_complete(requests_handler.finish_connections())
