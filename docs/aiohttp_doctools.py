fbbbb3a1764abd9d043fb3091ddb3f2885966c1b
Andrew Svetlov <andrew.svetlov@gmail.com>
2015-04-28 19:36:24 +0300

    Add sphinx extension for coroutinemethod and coroutinefunction markup

---
+++ docs/aiohttp_doctools.py
@@ -0,0 +1,27 @@
+from sphinx.domains.python import PyModulelevel, PyClassmember
+from sphinx import addnodes
+
+
+class PyCoroutineMixin(object):
+    def handle_signature(self, sig, signode):
+        ret = super(PyCoroutineMixin, self).handle_signature(sig, signode)
+        signode.insert(0, addnodes.desc_annotation('coroutine ', 'coroutine '))
+        return ret
+
+
+class PyCoroutineFunction(PyCoroutineMixin, PyModulelevel):
+    def run(self):
+        self.name = 'py:function'
+        return PyModulelevel.run(self)
+
+
+class PyCoroutineMethod(PyCoroutineMixin, PyClassmember):
+    def run(self):
+        self.name = 'py:method'
+        return PyClassmember.run(self)
+
+
+def setup(app):
+    app.add_directive_to_domain('py', 'coroutinefunction', PyCoroutineFunction)
+    app.add_directive_to_domain('py', 'coroutinemethod', PyCoroutineMethod)
+    return {'version': '1.0', 'parallel_read_safe': True}
