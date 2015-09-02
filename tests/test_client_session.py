568adcffcb049707d98464c85f89c77b69ac35a6
Andrew Svetlov <andrew.svetlov@gmail.com>
2015-09-01 23:52:56 -0400

    More tests

--- tests/test_client_session.py
+++ tests/test_client_session.py
@@ -155,6 +155,22 @@ class TestClientSession(unittest.TestCase):
         ]))
         session.close()
 
+    def test_merge_headers_with_list_of_tuples_duplicated_names(self):
+        session = ClientSession(
+            headers={
+                "h1": "header1",
+                "h2": "header2"
+            }, loop=self.loop)
+        headers = session._prepare_headers([("h1", "v1"),
+                                            ("h1", "v2")])
+        self.assertIsInstance(headers, CIMultiDict)
+        self.assertEqual(headers, CIMultiDict([
+            ("H2", "header2"),
+            ("H1", "v1"),
+            ("H1", "v2"),
+        ]))
+        session.close()
+
     def _make_one(self, **kwargs):
         session = ClientSession(loop=self.loop, **kwargs)
         params = dict(
