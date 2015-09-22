e286d4f9fb1993de2438bdca40712cf1660faf9e
Andrew Svetlov <andrew.svetlov@gmail.com>
2015-09-22 17:05:39 +0300

    Fix test for ssl fingerprint check

--- aiohttp/connector.py
+++ aiohttp/connector.py
@@ -571,6 +571,8 @@ class TCPConnector(BaseConnector):
                 has_cert = transp.get_extra_info('sslcontext')
                 if has_cert and self._fingerprint:
                     sock = transp.get_extra_info('socket')
+                    if not hasattr(sock, 'getpeercert'):
+                        sock = transp._ssl_protocol._sslpipe.ssl_object
                     # gives DER-encoded cert as a sequence of bytes (or None)
                     cert = sock.getpeercert(binary_form=True)
                     assert cert
