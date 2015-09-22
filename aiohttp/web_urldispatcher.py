804764839913b74f46f15170190ebacd0506dd0a
Andrew Svetlov <andrew.svetlov@gmail.com>
2015-09-22 21:20:11 +0300

    Implement RoutesView

--- aiohttp/web_urldispatcher.py
+++ aiohttp/web_urldispatcher.py
@@ -363,6 +363,23 @@ class _MethodNotAllowedMatchInfo(UrlMappingMatchInfo):
                         ', '.join(sorted(self._allowed_methods))))
 
 
+class RoutesView:
+
+    __slots__ = '_urls'
+
+    def __init__(self, urls):
+        self._urls = urls
+
+    def __len__(self):
+        return len(self._urls)
+
+    def __iter__(self):
+        yield from self._urls
+
+    def __contains__(self, route):
+        return route in self._urls
+
+
 class UrlDispatcher(AbstractRouter, collections.abc.Mapping):
 
     DYN = re.compile(r'^\{(?P<var>[a-zA-Z][_a-zA-Z0-9]*)\}$')
@@ -416,6 +433,9 @@ class UrlDispatcher(AbstractRouter, collections.abc.Mapping):
 
     def __getitem__(self, name):
         return self._routes[name]
+
+    def routes(self):
+        return RoutesView(self._urls)
 
     def register_route(self, route):
         assert isinstance(route, Route), 'Instance of Route class is required.'
