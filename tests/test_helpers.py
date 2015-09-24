8d62de0b8726625e81e300b1ac9e92d2f5a705a8
Shaw Terwilliger <sterwill@tinfig.com>
2015-09-24 09:29:56 -0400

    Extend precision of access log "T" to milliseconds
    The fractional time part had a precision of 10 milliseconds
    due to a too-short truncation of the time string.  This commit
    adds one digit and includes a test for the case.

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,3 +1,4 @@
+import datetime
 import unittest
 import unittest.mock
 
@@ -114,6 +115,17 @@ class TestHelpers(unittest.TestCase):
 
         with self.assertRaises(AttributeError):
             a.prop = 123
+
+
+class TestAtoms(unittest.TestCase):
+
+    def test_get_seconds_and_milliseconds(self):
+        response = dict(status=200, output_length=1)
+        request_time = 321.012345678901234
+
+        atoms = helpers.atoms(None, None, response, None, request_time)
+        self.assertEqual(atoms['T'], '321')
+        self.assertEqual(atoms['D'], '012345')
 
 
 class TestSafeAtoms(unittest.TestCase):
