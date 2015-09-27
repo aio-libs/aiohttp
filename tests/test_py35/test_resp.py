4fd1e91ab0c7c070f070b80016b236ccbba64181
Andrew Svetlov <andrew.svetlov@gmail.com>
2015-09-27 16:12:31 +0300

    Add test for async with

--- tests/test_py35/test_resp.py
+++ tests/test_py35/test_resp.py
@@ -14,4 +14,21 @@ async def test_await(create_server, loop):
     app.router.add_route('GET', '/', handler)
     resp = await aiohttp.get(url+'/', loop=loop)
     assert resp.status == 200
+    assert resp.connection is not None
     await resp.release()
+    assert resp.connection is None
+
+
+@pytest.mark.run_loop
+async def test_response_context_manager(create_server, loop):
+
+    async def handler(request):
+        return web.HTTPOk()
+
+    app, url = await create_server()
+    app.router.add_route('GET', '/', handler)
+    resp = await aiohttp.get(url+'/', loop=loop)
+    async with resp:
+        assert resp.status == 200
+        assert resp.connection is not None
+    assert resp.connection is None
