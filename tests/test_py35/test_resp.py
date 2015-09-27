b0fef4ed92cde72305a2d85f3e96adde93f82547
Andrew Svetlov <andrew.svetlov@gmail.com>
2015-09-27 18:59:28 +0300

    Add test for context manager

--- tests/test_py35/test_resp.py
+++ tests/test_py35/test_resp.py
@@ -32,3 +32,18 @@ async def test_response_context_manager(create_server, loop):
         assert resp.status == 200
         assert resp.connection is not None
     assert resp.connection is None
+
+
+@pytest.mark.run_loop
+async def test_client_api_context_manager(create_server, loop):
+
+    async def handler(request):
+        return web.HTTPOk()
+
+    app, url = await create_server()
+    app.router.add_route('GET', '/', handler)
+
+    async with aiohttp.get(url+'/', loop=loop) as resp:
+        assert resp.status == 200
+        assert resp.connection is not None
+    assert resp.connection is None
