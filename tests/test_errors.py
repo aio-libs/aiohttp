895482364a0b3f57ef22541d2f893b0d4f716328
Andrew Svetlov <andrew.svetlov@gmail.com>
2015-10-13 22:42:35 +0300

    Convert test_errors to pytest style

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -1,16 +1,13 @@
 """Tests for errors.py"""
 
-import unittest
-import unittest.mock
-
 import aiohttp
 
 
-class TestErrors(unittest.TestCase):
+def test_bad_status_line1():
+    err = aiohttp.BadStatusLine(b'')
+    assert str(err) == "b''"
 
-    def test_bad_status_line(self):
-        err = aiohttp.BadStatusLine(b'')
-        self.assertEqual(str(err), "b''")
 
-        err = aiohttp.BadStatusLine('Test')
-        self.assertEqual(str(err), 'Test')
+def test_bad_status_line2():
+    err = aiohttp.BadStatusLine('Test')
+    assert str(err) == 'Test'
