f81aa40b5fe1ccec06bc42b1cd416c741183da59
Andrew Svetlov <andrew.svetlov@gmail.com>
2015-10-16 20:53:29 +0300

    Drop unused code

--- tests/test_web_response.py
+++ tests/test_web_response.py
@@ -590,34 +590,6 @@ def test_prepare_calls_signal():
     sig.assert_called_with(request=req, response=resp)
 
 
-class TestStreamResponse(unittest.TestCase):
-
-    def setUp(self):
-        self.loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(None)
-
-    def tearDown(self):
-        self.loop.close()
-
-    def make_request(self, method, path, headers=CIMultiDict(),
-                     version=HttpVersion11):
-        message = RawRequestMessage(method, path, version, headers,
-                                    False, False)
-        return self.request_from_message(message)
-
-    def request_from_message(self, message):
-        self.app = mock.Mock()
-        self.app._debug = False
-        self.app.on_response_prepare = signals.Signal(self.app)
-        self.payload = mock.Mock()
-        self.transport = mock.Mock()
-        self.reader = mock.Mock()
-        self.writer = mock.Mock()
-        req = Request(self.app, message, self.payload,
-                      self.transport, self.reader, self.writer)
-        return req
-
-
 class TestResponse(unittest.TestCase):
 
     def setUp(self):
