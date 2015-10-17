88b77fa62e80ef5dd688cb33344c2106d687908d
Andrew Svetlov <andrew.svetlov@gmail.com>
2015-10-17 15:01:54 +0300

    Convert web middlewares test to pytest style

--- tests/test_web_middleware.py
+++ tests/test_web_middleware.py
@@ -1,123 +1,89 @@
 import asyncio
-import socket
 import pytest
-import unittest
-from aiohttp import web, request
+from aiohttp import web
 
 
 @pytest.mark.run_loop
 def test_middleware_modifies_response(create_app_and_client):
 
-        @asyncio.coroutine
-        def handler(request):
-            return web.Response(body=b'OK')
-
-        @asyncio.coroutine
-        def middleware_factory(app, handler):
-            def middleware(request):
-                resp = yield from handler(request)
-                assert 200 == resp.status
-                resp.set_status(201)
-                resp.text = resp.text + '[MIDDLEWARE]'
-                return resp
-            return middleware
-
-        app, client = yield from create_app_and_client()
-        app.middlewares.append(middleware_factory)
-        app.router.add_route('GET', '/', handler)
-        resp = yield from client.get('/')
-        assert 201 == resp.status
-        txt = yield from resp.text()
-        assert 'OK[MIDDLEWARE]' == txt
-
-
-class TestWebMiddlewareFunctional(unittest.TestCase):
-
-    def setUp(self):
-        self.handler = None
-        self.loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(None)
-
-    def tearDown(self):
-        if self.handler:
-            self.loop.run_until_complete(self.handler.finish_connections())
-        self.loop.close()
-
-    def find_unused_port(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind(('127.0.0.1', 0))
-        port = s.getsockname()[1]
-        s.close()
-        return port
+    @asyncio.coroutine
+    def handler(request):
+        return web.Response(body=b'OK')
 
     @asyncio.coroutine
-    def create_server(self, method, path, handler, *middlewares):
-        app = web.Application(loop=self.loop, middlewares=middlewares)
-        app.router.add_route(method, path, handler)
-
-        port = self.find_unused_port()
-        self.handler = app.make_handler()
-        srv = yield from self.loop.create_server(self.handler, '127.0.0.1',
-                                                 port)
-
-        url = "http://127.0.0.1:{}".format(port) + path
-        self.addCleanup(srv.close)
-        return app, srv, url
-
-    def test_middleware_handles_exception(self):
+    def middleware_factory(app, handler):
 
         @asyncio.coroutine
-        def handler(request):
-            raise RuntimeError('Error text')
+        def middleware(request):
+            resp = yield from handler(request)
+            assert 200 == resp.status
+            resp.set_status(201)
+            resp.text = resp.text + '[MIDDLEWARE]'
+            return resp
+        return middleware
+
+    app, client = yield from create_app_and_client()
+    app.middlewares.append(middleware_factory)
+    app.router.add_route('GET', '/', handler)
+    resp = yield from client.get('/')
+    assert 201 == resp.status
+    txt = yield from resp.text()
+    assert 'OK[MIDDLEWARE]' == txt
+
+
+@pytest.mark.run_loop
+def test_middleware_handles_exception(create_app_and_client):
+
+    @asyncio.coroutine
+    def handler(request):
+        raise RuntimeError('Error text')
+
+    @asyncio.coroutine
+    def middleware_factory(app, handler):
 
         @asyncio.coroutine
-        def middleware_factory(app, handler):
+        def middleware(request):
+            with pytest.raises(RuntimeError) as ctx:
+                yield from handler(request)
+            return web.Response(status=501,
+                                text=str(ctx.value) + '[MIDDLEWARE]')
+
+        return middleware
+
+    app, client = yield from create_app_and_client()
+    app.middlewares.append(middleware_factory)
+    app.router.add_route('GET', '/', handler)
+    resp = yield from client.get('/')
+    assert 501 == resp.status
+    txt = yield from resp.text()
+    assert 'Error text[MIDDLEWARE]' == txt
+
+
+@pytest.mark.run_loop
+def test_middleware_chain(create_app_and_client):
+
+    @asyncio.coroutine
+    def handler(request):
+        return web.Response(text='OK')
+
+    def make_factory(num):
+
+        @asyncio.coroutine
+        def factory(app, handler):
+
             def middleware(request):
-                with self.assertRaises(RuntimeError) as ctx:
-                    yield from handler(request)
-                return web.Response(status=501,
-                                    text=str(ctx.exception) + '[MIDDLEWARE]')
+                resp = yield from handler(request)
+                resp.text = resp.text + '[{}]'.format(num)
+                return resp
 
             return middleware
+        return factory
 
-        @asyncio.coroutine
-        def go():
-            _, _, url = yield from self.create_server('GET', '/', handler,
-                                                      middleware_factory)
-            resp = yield from request('GET', url, loop=self.loop)
-            self.assertEqual(501, resp.status)
-            txt = yield from resp.text()
-            self.assertEqual('Error text[MIDDLEWARE]', txt)
-
-        self.loop.run_until_complete(go())
-
-    def test_middleware_chain(self):
-
-        @asyncio.coroutine
-        def handler(request):
-            return web.Response(text='OK')
-
-        def make_factory(num):
-
-            @asyncio.coroutine
-            def factory(app, handler):
-
-                def middleware(request):
-                    resp = yield from handler(request)
-                    resp.text = resp.text + '[{}]'.format(num)
-                    return resp
-
-                return middleware
-            return factory
-
-        @asyncio.coroutine
-        def go():
-            _, _, url = yield from self.create_server('GET', '/', handler,
-                                                      make_factory(1),
-                                                      make_factory(2))
-            resp = yield from request('GET', url, loop=self.loop)
-            self.assertEqual(200, resp.status)
-            txt = yield from resp.text()
-            self.assertEqual('OK[2][1]', txt)
-
-        self.loop.run_until_complete(go())
+    app, client = yield from create_app_and_client()
+    app.middlewares.append(make_factory(1))
+    app.middlewares.append(make_factory(2))
+    app.router.add_route('GET', '/', handler)
+    resp = yield from client.get('/')
+    assert 200 == resp.status
+    txt = yield from resp.text()
+    assert 'OK[2][1]' == txt
