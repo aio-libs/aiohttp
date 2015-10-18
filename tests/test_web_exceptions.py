e7c8b6067e44a73666a2a8c4d5d8910910f950af
Andrew Svetlov <andrew.svetlov@gmail.com>
2015-10-18 11:14:15 +0300

    Convert HTTPExceptions tests

--- tests/test_web_exceptions.py
+++ tests/test_web_exceptions.py
@@ -1,6 +1,6 @@
-import asyncio
 import collections
-import unittest
+import pytest
+import re
 from unittest import mock
 from aiohttp.multidict import CIMultiDict
 from aiohttp.web import Request
@@ -9,151 +9,160 @@ from aiohttp.protocol import RawRequestMessage, HttpVersion11
 from aiohttp import signals, web
 
 
-class TestHTTPExceptions(unittest.TestCase):
+@pytest.fixture
+def buf():
+    return bytearray()
 
-    def setUp(self):
-        self.loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(None)
 
-        self.payload = mock.Mock()
-        self.transport = mock.Mock()
-        self.reader = mock.Mock()
-        self.writer = mock.Mock()
-        self.writer.drain.return_value = ()
-        self.buf = b''
+@pytest.fixture
+def request(buf):
+    method = 'GET'
+    path = '/'
+    headers = CIMultiDict()
+    transport = mock.Mock()
+    payload = mock.Mock()
+    reader = mock.Mock()
+    writer = mock.Mock()
+    writer.drain.return_value = ()
 
-        self.writer.write.side_effect = self.append
+    def append(data):
+        buf.extend(data)
+    writer.write.side_effect = append
+    app = mock.Mock()
+    app._debug = False
+    app.on_response_prepare = signals.Signal(app)
+    message = RawRequestMessage(method, path, HttpVersion11, headers,
+                                False, False)
+    req = Request(app, message, payload,
+                  transport, reader, writer)
+    return req
 
-    def tearDown(self):
-        self.loop.close()
 
-    def append(self, data):
-        self.buf += data
+def test_all_http_exceptions_exported():
+    assert 'HTTPException' in web.__all__
+    for name in dir(web):
+        if name.startswith('_'):
+            continue
+        obj = getattr(web, name)
+        if isinstance(obj, type) and issubclass(obj, web.HTTPException):
+            assert name in web.__all__
 
-    def make_request(self, method='GET', path='/', headers=CIMultiDict()):
-        self.app = mock.Mock()
-        self.app._debug = False
-        self.app.on_response_prepare = signals.Signal(self.app)
-        message = RawRequestMessage(method, path, HttpVersion11, headers,
-                                    False, False)
-        req = Request(self.app, message, self.payload,
-                      self.transport, self.reader, self.writer)
-        return req
 
-    def test_all_http_exceptions_exported(self):
-        self.assertIn('HTTPException', web.__all__)
-        for name in dir(web):
-            if name.startswith('_'):
-                continue
-            obj = getattr(web, name)
-            if isinstance(obj, type) and issubclass(obj, web.HTTPException):
-                self.assertIn(name, web.__all__)
+@pytest.mark.run_loop
+def test_HTTPOk(buf, request):
+    resp = web.HTTPOk()
+    yield from resp.prepare(request)
+    yield from resp.write_eof()
+    txt = buf.decode('utf8')
+    assert re.match(('HTTP/1.1 200 OK\r\n'
+                     'CONTENT-TYPE: text/plain; charset=utf-8\r\n'
+                     'CONTENT-LENGTH: 7\r\n'
+                     'CONNECTION: keep-alive\r\n'
+                     'DATE: .+\r\n'
+                     'SERVER: .+\r\n\r\n'
+                     '200: OK'), txt)
 
-    def test_HTTPOk(self):
-        req = self.make_request()
-        resp = web.HTTPOk()
-        self.loop.run_until_complete(resp.prepare(req))
-        self.loop.run_until_complete(resp.write_eof())
-        txt = self.buf.decode('utf8')
-        self.assertRegex(txt, ('HTTP/1.1 200 OK\r\n'
-                               'CONTENT-TYPE: text/plain; charset=utf-8\r\n'
-                               'CONTENT-LENGTH: 7\r\n'
-                               'CONNECTION: keep-alive\r\n'
-                               'DATE: .+\r\n'
-                               'SERVER: .+\r\n\r\n'
-                               '200: OK'))
 
-    def test_terminal_classes_has_status_code(self):
-        terminals = set()
-        for name in dir(web):
-            obj = getattr(web, name)
-            if isinstance(obj, type) and issubclass(obj, web.HTTPException):
-                terminals.add(obj)
+def test_terminal_classes_has_status_code():
+    terminals = set()
+    for name in dir(web):
+        obj = getattr(web, name)
+        if isinstance(obj, type) and issubclass(obj, web.HTTPException):
+            terminals.add(obj)
 
-        dup = frozenset(terminals)
-        for cls1 in dup:
-            for cls2 in dup:
-                if cls1 in cls2.__bases__:
-                    terminals.discard(cls1)
+    dup = frozenset(terminals)
+    for cls1 in dup:
+        for cls2 in dup:
+            if cls1 in cls2.__bases__:
+                terminals.discard(cls1)
 
-        for cls in terminals:
-            self.assertIsNotNone(cls.status_code, cls)
-        codes = collections.Counter(cls.status_code for cls in terminals)
-        self.assertNotIn(None, codes)
-        self.assertEqual(1, codes.most_common(1)[0][1])
+    for cls in terminals:
+        assert cls.status_code is not None
+    codes = collections.Counter(cls.status_code for cls in terminals)
+    assert None not in codes
+    assert 1 == codes.most_common(1)[0][1]
 
-    def test_HTTPFound(self):
-        req = self.make_request()
-        resp = web.HTTPFound(location='/redirect')
-        self.assertEqual('/redirect', resp.location)
-        self.assertEqual('/redirect', resp.headers['location'])
-        self.loop.run_until_complete(resp.prepare(req))
-        self.loop.run_until_complete(resp.write_eof())
-        txt = self.buf.decode('utf8')
-        self.assertRegex(txt, ('HTTP/1.1 302 Found\r\n'
-                               'CONTENT-TYPE: text/plain; charset=utf-8\r\n'
-                               'CONTENT-LENGTH: 10\r\n'
-                               'LOCATION: /redirect\r\n'
-                               'CONNECTION: keep-alive\r\n'
-                               'DATE: .+\r\n'
-                               'SERVER: .+\r\n\r\n'
-                               '302: Found'))
 
-    def test_HTTPFound_empty_location(self):
-        with self.assertRaises(ValueError):
-            web.HTTPFound(location='')
+@pytest.mark.run_loop
+def test_HTTPFound(buf, request):
+    resp = web.HTTPFound(location='/redirect')
+    assert '/redirect' == resp.location
+    assert '/redirect' == resp.headers['location']
+    yield from resp.prepare(request)
+    yield from resp.write_eof()
+    txt = buf.decode('utf8')
+    assert re.match('HTTP/1.1 302 Found\r\n'
+                    'CONTENT-TYPE: text/plain; charset=utf-8\r\n'
+                    'CONTENT-LENGTH: 10\r\n'
+                    'LOCATION: /redirect\r\n'
+                    'CONNECTION: keep-alive\r\n'
+                    'DATE: .+\r\n'
+                    'SERVER: .+\r\n\r\n'
+                    '302: Found', txt)
 
-        with self.assertRaises(ValueError):
-            web.HTTPFound(location=None)
 
-    def test_HTTPMethodNotAllowed(self):
-        req = self.make_request()
-        resp = web.HTTPMethodNotAllowed('get', ['POST', 'PUT'])
-        self.assertEqual('GET', resp.method)
-        self.assertEqual(['POST', 'PUT'], resp.allowed_methods)
-        self.assertEqual('POST,PUT', resp.headers['allow'])
-        self.loop.run_until_complete(resp.prepare(req))
-        self.loop.run_until_complete(resp.write_eof())
-        txt = self.buf.decode('utf8')
-        self.assertRegex(txt, ('HTTP/1.1 405 Method Not Allowed\r\n'
-                               'CONTENT-TYPE: text/plain; charset=utf-8\r\n'
-                               'CONTENT-LENGTH: 23\r\n'
-                               'ALLOW: POST,PUT\r\n'
-                               'CONNECTION: keep-alive\r\n'
-                               'DATE: .+\r\n'
-                               'SERVER: .+\r\n\r\n'
-                               '405: Method Not Allowed'))
+def test_HTTPFound_empty_location():
+    with pytest.raises(ValueError):
+        web.HTTPFound(location='')
 
-    def test_override_body_with_text(self):
-        resp = web.HTTPNotFound(text="Page not found")
-        self.assertEqual(404, resp.status)
-        self.assertEqual("Page not found".encode('utf-8'), resp.body)
-        self.assertEqual("Page not found", resp.text)
-        self.assertEqual("text/plain", resp.content_type)
-        self.assertEqual("utf-8", resp.charset)
+    with pytest.raises(ValueError):
+        web.HTTPFound(location=None)
 
-    def test_override_body_with_binary(self):
-        txt = "<html><body>Page not found</body></html>"
-        resp = web.HTTPNotFound(body=txt.encode('utf-8'),
-                                content_type="text/html")
-        self.assertEqual(404, resp.status)
-        self.assertEqual(txt.encode('utf-8'), resp.body)
-        self.assertEqual(txt, resp.text)
-        self.assertEqual("text/html", resp.content_type)
-        self.assertIsNone(resp.charset)
 
-    def test_default_body(self):
-        resp = web.HTTPOk()
-        self.assertEqual(b'200: OK', resp.body)
+@pytest.mark.run_loop
+def test_HTTPMethodNotAllowed(buf, request):
+    resp = web.HTTPMethodNotAllowed('get', ['POST', 'PUT'])
+    assert 'GET' == resp.method
+    assert ['POST', 'PUT'] == resp.allowed_methods
+    assert 'POST,PUT' == resp.headers['allow']
+    yield from resp.prepare(request)
+    yield from resp.write_eof()
+    txt = buf.decode('utf8')
+    assert re.match('HTTP/1.1 405 Method Not Allowed\r\n'
+                    'CONTENT-TYPE: text/plain; charset=utf-8\r\n'
+                    'CONTENT-LENGTH: 23\r\n'
+                    'ALLOW: POST,PUT\r\n'
+                    'CONNECTION: keep-alive\r\n'
+                    'DATE: .+\r\n'
+                    'SERVER: .+\r\n\r\n'
+                    '405: Method Not Allowed', txt)
 
-    def test_empty_body_204(self):
-        resp = web.HTTPNoContent()
-        self.assertIsNone(resp.body)
 
-    def test_empty_body_205(self):
-        resp = web.HTTPNoContent()
-        self.assertIsNone(resp.body)
+def test_override_body_with_text():
+    resp = web.HTTPNotFound(text="Page not found")
+    assert 404 == resp.status
+    assert "Page not found".encode('utf-8') == resp.body
+    assert "Page not found" == resp.text
+    assert "text/plain" == resp.content_type
+    assert "utf-8" == resp.charset
 
-    def test_empty_body_304(self):
-        resp = web.HTTPNoContent()
-        self.assertIsNone(resp.body)
+
+def test_override_body_with_binary():
+    txt = "<html><body>Page not found</body></html>"
+    resp = web.HTTPNotFound(body=txt.encode('utf-8'),
+                            content_type="text/html")
+    assert 404 == resp.status
+    assert txt.encode('utf-8') == resp.body
+    assert txt == resp.text
+    assert "text/html" == resp.content_type
+    assert resp.charset is None
+
+
+def test_default_body():
+    resp = web.HTTPOk()
+    assert b'200: OK' == resp.body
+
+
+def test_empty_body_204():
+    resp = web.HTTPNoContent()
+    assert resp.body is None
+
+
+def test_empty_body_205():
+    resp = web.HTTPNoContent()
+    assert resp.body is None
+
+
+def test_empty_body_304():
+    resp = web.HTTPNoContent()
+    resp.body is None
