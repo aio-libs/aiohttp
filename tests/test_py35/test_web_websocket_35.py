206df4e20b8317b1a6ac4d6435b4e2a3e028bce2
Andrew Svetlov <andrew.svetlov@gmail.com>
2015-10-24 12:57:58 +0300

    Drop unused function

--- tests/test_py35/test_web_websocket_35.py
+++ tests/test_py35/test_web_websocket_35.py
@@ -6,13 +6,6 @@ from aiohttp import web, websocket
 from aiohttp.websocket_client import MsgType, ws_connect
 
 
-async def create_server(loop, port, method, path, route_handler):
-    app = web.Application(loop=loop)
-    app.router.add_route(method, path, route_handler)
-    handler = app.make_handler(keep_alive_on=False)
-    return await loop.create_server(handler, '127.0.0.1', port)
-
-
 @pytest.mark.run_loop
 async def test_await(loop, create_server):
     closed = asyncio.Future(loop=loop)
