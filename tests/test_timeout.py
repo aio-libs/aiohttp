8da5d4febccb3ad61760bef13d207ad552a1f5fd
Nickolai Novik <is.infinity@yahoo.com>
2015-11-24 06:58:47 +0200

    refactor and move tests from py35 directory

--- tests/test_timeout.py
+++ tests/test_timeout.py
@@ -8,18 +8,20 @@ from aiohttp.helpers import Timeout
 def test_timeout(loop):
     canceled_raised = False
 
-    async def long_running_task():
+    @asyncio.coroutine
+    def long_running_task():
         try:
-            await asyncio.sleep(10, loop=loop)
+            yield from asyncio.sleep(10, loop=loop)
         except asyncio.CancelledError:
             nonlocal canceled_raised
             canceled_raised = True
             raise
 
-    async def run():
+    @asyncio.coroutine
+    def run():
         with pytest.raises(asyncio.TimeoutError):
-            async with Timeout(0.01, loop=loop) as t:
-                await long_running_task()
+            with Timeout(0.01, loop=loop) as t:
+                yield from long_running_task()
                 assert t._loop is loop
         assert canceled_raised, 'CancelledError was not raised'
 
@@ -27,13 +29,15 @@ def test_timeout(loop):
 
 
 def test_timeout_finish_in_time(loop):
-    async def long_running_task():
-        await asyncio.sleep(0.01, loop=loop)
+    @asyncio.coroutine
+    def long_running_task():
+        yield from asyncio.sleep(0.01, loop=loop)
         return 'done'
 
-    async def run():
-        async with Timeout(0.1, loop=loop):
-            resp = await long_running_task()
+    @asyncio.coroutine
+    def run():
+        with Timeout(0.1, loop=loop):
+            resp = yield from long_running_task()
         assert resp == 'done'
 
     loop.run_until_complete(run())
@@ -42,57 +46,63 @@ def test_timeout_finish_in_time(loop):
 def test_timeout_gloabal_loop(loop):
     asyncio.set_event_loop(loop)
 
-    async def run():
-        async with Timeout(0.1) as t:
-            await asyncio.sleep(0.01)
+    @asyncio.coroutine
+    def run():
+        with Timeout(0.1) as t:
+            yield from asyncio.sleep(0.01)
             assert t._loop is loop
 
     loop.run_until_complete(run())
 
 
 def test_timeout_not_relevant_exception(loop):
-    async def run():
+    @asyncio.coroutine
+    def run():
         with pytest.raises(KeyError):
-            async with Timeout(0.1, loop=loop):
+            with Timeout(0.1, loop=loop):
                 raise KeyError
 
     loop.run_until_complete(run())
 
 
 def test_timeout_blocking_loop(loop):
-    async def long_running_task():
+    @asyncio.coroutine
+    def long_running_task():
         time.sleep(0.1)
         return 'done'
 
-    async def run():
-        async with Timeout(0.01, loop=loop):
-            result = await long_running_task()
+    @asyncio.coroutine
+    def run():
+        with Timeout(0.01, loop=loop):
+            result = yield from long_running_task()
         assert result == 'done'
 
     loop.run_until_complete(run())
 
 
 def test_for_race_conditions(loop):
-    async def run():
+    @asyncio.coroutine
+    def run():
         fut = asyncio.Future(loop=loop)
         loop.call_later(0.1, fut.set_result('done'))
-        async with Timeout(0.2, loop=loop):
-            resp = await fut
+        with Timeout(0.2, loop=loop):
+            resp = yield from fut
         assert resp == 'done'
 
     loop.run_until_complete(run())
 
 
 def test_timeout_time(loop):
-    async def go():
+    @asyncio.coroutine
+    def go():
         foo_running = None
 
         start = loop.time()
         with pytest.raises(asyncio.TimeoutError):
-            async with Timeout(0.1, loop=loop):
+            with Timeout(0.1, loop=loop):
                 foo_running = True
                 try:
-                    await asyncio.sleep(0.2, loop=loop)
+                    yield from asyncio.sleep(0.2, loop=loop)
                 finally:
                     foo_running = False
 
