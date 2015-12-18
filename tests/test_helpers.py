7b93782244709e3d83346444181dbb652750bbc3
Andrew Svetlov <andrew.svetlov@gmail.com>
2015-12-18 19:29:27 +0200

    Skip test if unix sockets are not supported

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -234,6 +234,8 @@ def test_stop_listening_inet6(loop, unused_port_ipv6):
     assert not loop.remove_reader(fileno)
 
 
+@pytest.mark.skipif(hasattr(socket, 'AF_UNIX'),
+                    reason='Requires unix sockets support')
 @pytest.mark.run_loop
 def test_stop_listening_unix(loop, tmpdir):
     srv = yield from loop.create_unix_server(
