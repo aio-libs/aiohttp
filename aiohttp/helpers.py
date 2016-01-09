0ed488d7bd6c928d768526e116396fdf84151a6f
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-01-10 00:59:01 +0200

    reify is actually a data descriptor

--- aiohttp/helpers.py
+++ aiohttp/helpers.py
@@ -371,10 +371,10 @@ _marker = object()
 
 class reify:
     """Use as a class method decorator.  It operates almost exactly like
-    the Python ``@property`` decorator, but it puts the result of the
+    the Python `@property` decorator, but it puts the result of the
     method it decorates into the instance dict after the first call,
     effectively replacing the function it decorates with an instance
-    variable.  It is, in Python parlance, a non-data descriptor.
+    variable.  It is, in Python parlance, a data descriptor.
 
     """
 
