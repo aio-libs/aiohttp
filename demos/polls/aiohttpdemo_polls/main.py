5e09519289b2ff5ca804ba8742a71001daf1ff6b
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-01-13 22:28:15 +0200

    Use run_app im demo

--- demos/polls/aiohttpdemo_polls/main.py
+++ demos/polls/aiohttpdemo_polls/main.py
@@ -13,7 +13,6 @@ from aiohttpdemo_polls.views import SiteHandler
 
 PROJ_ROOT = pathlib.Path(__file__).parent.parent
 TEMPLATES_ROOT = pathlib.Path(__file__).parent / 'templates'
-log = logging.getLogger(__name__)
 
 
 async def init(loop):
@@ -27,40 +26,28 @@ async def init(loop):
     # create connection to the database
     pg = await init_postgres(conf['postgres'], loop)
 
-    async def close_pg():
+    async def close_pg(app):
         pg.close()
         await pg.wait_closed()
 
-    app.register_on_finish(close_pg)
+    app.on_cleanup.append(close_pg)
 
     # setup views and routes
     handler = SiteHandler(pg)
     setup_routes(app, handler, PROJ_ROOT)
 
-    # init logging and attach access_log
-    logging.basicConfig(level=logging.DEBUG)
     app_handler = app.make_handler(access_log=log)
     host, port = conf['host'], conf['port']
-    srv = await loop.create_server(app_handler, host, port)
-    print("Server started at http://{0}:{1}".format(host, port))
-    return srv, app_handler
+    return app, host, port
 
 
 def main():
-    loop = asyncio.get_event_loop()
-    srv, app_handler = loop.run_until_complete(init(loop))
+    # init logging
+    logging.basicConfig(level=logging.DEBUG)
 
-    try:
-        loop.run_forever()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        srv.close()
-        loop.run_until_complete(srv.wait_closed())
-        loop.run_until_complete(app.shutdown())
-        loop.run_until_complete(handler.finish_connections(shutdown_timeout))
-        loop.run_until_complete(app.finish())
-    loop.close()
+    loop = asyncio.get_event_loop()
+    app, host, port = loop.run_until_complete(init(loop))
+    web.run_app(app, host=host, port=port)
 
 
 if __name__ == '__main__':
