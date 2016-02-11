07e986d0bb1c4eee0c373c55f88a4c1b086230b5
Louis-Philippe Huberdeau <lp@huberdeau.info>
2016-02-11 09:19:26 -0500

    Converting tests to py.test style

--- tests/test_resolver.py
+++ tests/test_resolver.py
@@ -1,62 +1,74 @@
+import pytest
 import asyncio
 import socket
 import ipaddress
 import aiodns
-import unittest
 from aiohttp.resolver import AsyncResolver, DefaultResolver
 
 
-class BaseResolverTestCase(unittest.TestCase):
-
-    __test__ = False
-
-    def setUp(self):
-        self.loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(None)
-
-    def test_positive_lookup(self):
-        @asyncio.coroutine
-        def go():
-            real = yield from self.resolver.resolve('www.python.org')
-            ipaddress.ip_address(real[0]['host'])
-        self.loop.run_until_complete(go())
-
-    def test_multiple_replies(self):
-        @asyncio.coroutine
-        def go():
-            real = yield from self.resolver.resolve('www.google.com')
-            ips = [ipaddress.ip_address(x['host']) for x in real]
-            self.assertGreater(len(ips), 3)
-        self.loop.run_until_complete(go())
+@pytest.fixture
+def loop():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(None)
+    return loop
 
 
-class TestAsyncResolver(BaseResolverTestCase):
+def test_async_resolver_positive_lookup(loop):
+    @asyncio.coroutine
+    def go():
+        resolver = AsyncResolver(loop=loop)
+        real = yield from resolver.resolve('www.python.org')
+        ipaddress.ip_address(real[0]['host'])
 
-    __test__ = True
+    loop.run_until_complete(go())
 
-    def setUp(self):
-        super(TestAsyncResolver, self).setUp()
-        self.resolver = AsyncResolver(loop=self.loop)
+def test_async_resolver_multiple_replies(loop):
+    @asyncio.coroutine
+    def go():
+        resolver = AsyncResolver(loop=loop)
+        real = yield from resolver.resolve('www.google.com')
+        ips = [ipaddress.ip_address(x['host']) for x in real]
+        assert len(ips) > 3, "Expecting multiple addresses"
+    loop.run_until_complete(go())
 
-    def test_negative_lookup(self):
-        @asyncio.coroutine
-        def go():
-            with self.assertRaises(aiodns.error.DNSError):
-                yield from self.resolver.resolve('doesnotexist.bla')
-        self.loop.run_until_complete(go())
+def test_async_negative_lookup(loop):
+    @asyncio.coroutine
+    def go():
+        resolver = AsyncResolver(loop=loop)
+        try:
+            yield from resolver.resolve('doesnotexist.bla')
+            assert False, "Expecting aiodns.error.DNSError"
+        except aiodns.error.DNSError:
+            pass
 
+    loop.run_until_complete(go())
 
-class TestExecutorResolver(BaseResolverTestCase):
+def test_default_resolver_positive_lookup(loop):
+    @asyncio.coroutine
+    def go():
+        resolver = DefaultResolver(loop=loop)
+        real = yield from resolver.resolve('www.python.org')
+        ipaddress.ip_address(real[0]['host'])
 
-    __test__ = True
+    loop.run_until_complete(go())
 
-    def setUp(self):
-        super(TestExecutorResolver, self).setUp()
-        self.resolver = DefaultResolver(loop=self.loop)
+def test_default_resolver_multiple_replies(loop):
+    @asyncio.coroutine
+    def go():
+        resolver = DefaultResolver(loop=loop)
+        real = yield from resolver.resolve('www.google.com')
+        ips = [ipaddress.ip_address(x['host']) for x in real]
+        assert len(ips) > 3, "Expecting multiple addresses"
+    loop.run_until_complete(go())
 
-    def test_negative_lookup(self):
-        @asyncio.coroutine
-        def go():
-            with self.assertRaises(socket.gaierror):
-                yield from self.resolver.resolve('doesnotexist.bla')
-        self.loop.run_until_complete(go())
+def test_default_negative_lookup(loop):
+    @asyncio.coroutine
+    def go():
+        resolver = DefaultResolver(loop=loop)
+        try:
+            yield from resolver.resolve('doesnotexist.bla')
+            assert False, "Expecting socket.gaierror"
+        except socket.gaierror:
+            pass
+
+    loop.run_until_complete(go())
