7aed49ba407a783d5a87a18b7cdabbfb42c1996e
Louis-Philippe Huberdeau <lp@huberdeau.info>
2016-02-11 09:59:08 -0500

    Mocking async resolver tests

--- tests/test_resolver.py
+++ tests/test_resolver.py
@@ -4,6 +4,12 @@ import socket
 import ipaddress
 import aiodns
 from aiohttp.resolver import AsyncResolver, DefaultResolver
+from unittest.mock import patch, Mock
+
+
+class FakeResult:
+    def __init__(self, host):
+        self.host = host
 
 
 @pytest.fixture
@@ -12,34 +18,44 @@ def loop():
     asyncio.set_event_loop(None)
     return loop
 
+@asyncio.coroutine
+def fake_result(result):
+    return [FakeResult(host=h)
+            for h in result]
 
 def test_async_resolver_positive_lookup(loop):
     @asyncio.coroutine
     def go():
-        resolver = AsyncResolver(loop=loop)
-        real = yield from resolver.resolve('www.python.org')
-        ipaddress.ip_address(real[0]['host'])
-
+        with patch('aiodns.DNSResolver.query') as mock_query:
+            mock_query.return_value = fake_result(['127.0.0.1'])
+            resolver = AsyncResolver(loop=loop)
+            real = yield from resolver.resolve('www.python.org')
+            ipaddress.ip_address(real[0]['host'])
     loop.run_until_complete(go())
 
 def test_async_resolver_multiple_replies(loop):
     @asyncio.coroutine
     def go():
-        resolver = AsyncResolver(loop=loop)
-        real = yield from resolver.resolve('www.google.com')
-        ips = [ipaddress.ip_address(x['host']) for x in real]
-        assert len(ips) > 3, "Expecting multiple addresses"
+        with patch('aiodns.DNSResolver.query') as mock_query:
+            ips = ['127.0.0.1', '127.0.0.2', '127.0.0.3', '127.0.0.4']
+            mock_query.return_value = fake_result(ips)
+            resolver = AsyncResolver(loop=loop)
+            real = yield from resolver.resolve('www.google.com')
+            ips = [ipaddress.ip_address(x['host']) for x in real]
+            assert len(ips) > 3, "Expecting multiple addresses"
     loop.run_until_complete(go())
 
 def test_async_negative_lookup(loop):
     @asyncio.coroutine
     def go():
-        resolver = AsyncResolver(loop=loop)
-        try:
-            yield from resolver.resolve('doesnotexist.bla')
-            assert False, "Expecting aiodns.error.DNSError"
-        except aiodns.error.DNSError:
-            pass
+        with patch('aiodns.DNSResolver.query') as mock_query:
+            mock_query.side_effect = aiodns.error.DNSError()
+            resolver = AsyncResolver(loop=loop)
+            try:
+                yield from resolver.resolve('doesnotexist.bla')
+                assert False, "Expecting aiodns.error.DNSError"
+            except aiodns.error.DNSError:
+                pass
 
     loop.run_until_complete(go())
 
