afb5440d5d5a16c65677e4e3a0d75d48a8758701
banthonywalker <bobo.anthony.walker@gmail.com>
2016-03-08 03:52:37 +0100

    urldispatcher AbstractRoute handler coroutine wrapper fix & test

--- tests/test_web_urldispatcher.py
+++ tests/test_web_urldispatcher.py
@@ -2,6 +2,9 @@ import pytest
 import os
 import shutil
 import tempfile
+import functools
+import asyncio
+import aiohttp.web
 
 
 @pytest.fixture(scope='function')
@@ -43,4 +46,20 @@ def test_access_root_of_static_handler(tmp_dir_path, create_app_and_client):
     r = yield from client.get('/')
     assert r.status == 404
     # data = (yield from r.read())
+    yield from r.release()
+
+
+@pytest.mark.run_loop
+def test_partialy_applied_handler(create_app_and_client):
+    app, client = yield from create_app_and_client()
+
+    @asyncio.coroutine
+    def handler(data, request):
+        return aiohttp.web.Response(body=data)
+
+    app.router.add_route('GET', '/', functools.partial(handler, b'hello'))
+
+    r = yield from client.get('/')
+    data = (yield from r.read())
+    assert data == b'hello'
     yield from r.release()
