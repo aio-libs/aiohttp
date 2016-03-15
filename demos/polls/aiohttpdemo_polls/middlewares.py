e056914ee4d1558fec5ecd61fe22f88fd40d51d0
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-03-15 13:54:23 +0200

    Add 500 handler

--- demos/polls/aiohttpdemo_polls/middlewares.py
+++ demos/polls/aiohttpdemo_polls/middlewares.py
@@ -9,6 +9,13 @@ async def handle_404(request, response):
     return response
 
 
+async def handle_500(request, response):
+    response = aiohttp_jinja2.render_template('500.html',
+                                              request,
+                                              {})
+    return response
+
+
 def error_pages(overrides):
     async def middleware(app, handler):
         async def middleware_handler(request):
@@ -30,5 +37,6 @@ def error_pages(overrides):
 
 
 def setup_middlewares(app):
-    error_middleware = error_pages({404: handle_404})
+    error_middleware = error_pages({404: handle_404,
+                                    500: handle_500})
     app.middlewares.append(error_middleware)
