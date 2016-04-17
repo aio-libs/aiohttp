b43ce7af86af9efdedfdb32039fbbd1fc54b17cc
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-04-17 07:42:00 +0200

    Add simple chat

--- demos/chat/setup.py
+++ demos/chat/setup.py
@@ -7,26 +7,24 @@ from setuptools import find_packages, setup
 def read_version():
     regexp = re.compile(r"^__version__\W*=\W*'([\d.abrc]+)'")
     init_py = os.path.join(os.path.dirname(__file__),
-                           'aiohttpdemo_polls', '__init__.py')
+                           'aiohttpdemo_chat', '__init__.py')
     with open(init_py) as f:
         for line in f:
             match = regexp.match(line)
             if match is not None:
                 return match.group(1)
         else:
-            msg = 'Cannot find version in aiohttpdemo_polls/__init__.py'
+            msg = 'Cannot find version in aiohttpdemo_chat/__init__.py'
             raise RuntimeError(msg)
 
 
 install_requires = ['aiohttp',
-                    'aiopg[sa]',
-                    'aiohttp_jinja2',
-                    'pyyaml']
+                    'aiohttp_jinja2']
 
 
-setup(name='aiohttpdemo_polls',
+setup(name='aiohttpdemo_chat',
       version=read_version(),
-      description='Polls project example from aiohttp',
+      description='Chat example from aiohttp',
       platforms=['POSIX'],
       packages=find_packages(),
       include_package_data=True,
