0eb512e1a01aa771409b48e8c6a04cdc3879cc17
Yury Selivanov <yury@magic.io>
2016-05-16 22:47:27 -0400

    Fix the code to use event loop signaling API

--- aiohttp/worker.py
+++ aiohttp/worker.py
@@ -109,14 +109,26 @@ class GunicornWebWorker(base.Worker):
 
         yield from self.close()
 
-    def init_signal(self):
-        # init new signaling
-        self.loop.add_signal_handler(signal.SIGQUIT, self.handle_quit)
-        self.loop.add_signal_handler(signal.SIGTERM, self.handle_exit)
-        self.loop.add_signal_handler(signal.SIGINT, self.handle_quit)
-        self.loop.add_signal_handler(signal.SIGWINCH, self.handle_winch)
-        self.loop.add_signal_handler(signal.SIGUSR1, self.handle_usr1)
-        self.loop.add_signal_handler(signal.SIGABRT, self.handle_abort)
+    def init_signals(self):
+        # Set up signals through the event loop API.
+
+        self.loop.add_signal_handler(signal.SIGQUIT, self.handle_quit,
+                                     signal.SIGQUIT, None)
+
+        self.loop.add_signal_handler(signal.SIGTERM, self.handle_exit,
+                                     signal.SIGTERM, None)
+
+        self.loop.add_signal_handler(signal.SIGINT, self.handle_quit,
+                                     signal.SIGINT, None)
+
+        self.loop.add_signal_handler(signal.SIGWINCH, self.handle_winch,
+                                     signal.SIGWINCH, None)
+
+        self.loop.add_signal_handler(signal.SIGUSR1, self.handle_usr1,
+                                     signal.SIGUSR1, None)
+
+        self.loop.add_signal_handler(signal.SIGABRT, self.handle_abort,
+                                     signal.SIGABRT, None)
 
         # Don't let SIGTERM and SIGUSR1 disturb active requests
         # by interrupting system calls
