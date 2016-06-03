d27e1093bbdc90b7682a09f6bc197cf14c0b07f9
Amy Boyle <aboyle@newrelic.com>
2016-06-02 21:58:01 -0700

    Use regex to determine ip addresses for efficiency

--- tests/test_connector.py
+++ tests/test_connector.py
@@ -861,6 +861,14 @@ class TestHttpClientConnector(unittest.TestCase):
             '0.0.0.0',
             '127.0.0.1',
             '255.255.255.255',
+            '0:0:0:0:0:0:0:0',
+            'FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF',
+            '00AB:0002:3008:8CFD:00AB:0002:3008:8CFD',
+            '00ab:0002:3008:8cfd:00ab:0002:3008:8cfd',
+            'AB:02:3008:8CFD:AB:02:3008:8CFD',
+            'AB:02:3008:8CFD::02:3008:8CFD',
+            '::',
+            '1::1',
         ]
         for address in ip_addresses:
             assert host_is_ip(address) is True
