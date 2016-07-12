d46f472e3aeaee1bc7c76faa6ac0a7187920c2c3
cr0hn <cr0hn@cr0hn.com>
2016-07-12 10:56:57 +0100

    Update web.py (#892)
    Added backlog option to support more than 128 (default value in "create_server" function) concurrent connections.

--- aiohttp/web.py
+++ aiohttp/web.py
@@ -294,7 +294,7 @@ class Application(dict):
 
 def run_app(app, *, host='0.0.0.0', port=None,
             shutdown_timeout=60.0, ssl_context=None,
-            print=print):
+            print=print, backlog=128):
     """Run an app locally"""
     if port is None:
         if not ssl_context:
@@ -306,7 +306,8 @@ def run_app(app, *, host='0.0.0.0', port=None,
 
     handler = app.make_handler()
     srv = loop.run_until_complete(loop.create_server(handler, host, port,
-                                                     ssl=ssl_context))
+                                                     ssl=ssl_context, 
+                                                     backlog=backlog))
 
     scheme = 'https' if ssl_context else 'http'
     print("======== Running on {scheme}://{host}:{port}/ ========\n"
