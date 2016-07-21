0c323387d5be1bb195360a9633741900e18ac842
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-07-21 13:50:16 +0200

    Update docs

--- demos/polls/aiohttpdemo_polls/routes.py
+++ demos/polls/aiohttpdemo_polls/routes.py
@@ -2,12 +2,11 @@ from .views import index, poll, results, vote
 
 
 def setup_routes(app, project_root):
-    add_route = app.router.add_route
-    add_route('GET', '/', index)
-    add_route('GET', '/poll/{question_id}', poll, name='poll')
-    add_route('GET', '/poll/{question_id}/results',
-              results, name='results')
-    add_route('POST', '/poll/{question_id}/vote', vote, name='vote')
+    app.router.add_route('GET', '/', index)
+    app.router.add_route('GET', '/poll/{question_id}', poll, name='poll')
+    app.router.add_route('GET', '/poll/{question_id}/results',
+                         results, name='results')
+    app.router.add_route('POST', '/poll/{question_id}/vote', vote, name='vote')
     app.router.add_static('/static/',
                           path=str(project_root / 'static'),
                           name='static')
