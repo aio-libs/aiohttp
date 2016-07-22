6076301730279111d0be0ce7447576e743f8954b
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-07-22 14:56:23 +0200

    Refactor demo (#981)
    * Refactor demo code
    
    * Update docs

--- demos/polls/aiohttpdemo_polls/utils.py
+++ demos/polls/aiohttpdemo_polls/utils.py
@@ -1,5 +1,4 @@
 import yaml
-import aiopg.sa
 
 
 def load_config(fname):
@@ -7,16 +6,3 @@ def load_config(fname):
         data = yaml.load(f)
     # TODO: add config validation
     return data
-
-
-async def init_postgres(conf, loop):
-    engine = await aiopg.sa.create_engine(
-        database=conf['database'],
-        user=conf['user'],
-        password=conf['password'],
-        host=conf['host'],
-        port=conf['port'],
-        minsize=conf['minsize'],
-        maxsize=conf['maxsize'],
-        loop=loop)
-    return engine
