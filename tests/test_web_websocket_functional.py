2488b85d4f6fbc7466493a4b7b4381ab5896debb
vitalie maldur <maldur.vitalie@gmail.com>
2016-07-24 15:19:45 +0200

    Fix #976: Add support for websocket send_json and receive_json (#984)
    * Fix #976: Add support for websocket send_json and receive_json
    
    * Fix for (#976): use send_str instead of low level send method
    
    * Fix for (#976): add test for websocket send_json and receive_json
    
    * Fix for (#976): refactor websocket send_json and receive_json
    
    * Fix for (#976): refactor tests for websocket send_json and receive_json
    
    * Fix for (#976): add documentation for websocket send_json and receive_json
    
    * Fix for (#976): serialize receive_str response in receive_json
    
    * Fix for (#976): refactor docs for receive_json method

--- tests/test_web_websocket_functional.py
+++ tests/test_web_websocket_functional.py
@@ -40,14 +40,12 @@ def test_websocket_json_invalid_message(create_app_and_client):
     def handler(request):
         ws = web.WebSocketResponse()
         yield from ws.prepare(request)
-        msg = yield from ws.receive()
-
         try:
-            msg.json()
+            yield from ws.receive_json()
         except ValueError:
-            ws.send_str("ValueError raised: '%s'" % msg.data)
+            ws.send_str('ValueError was raised')
         else:
-            raise Exception("No ValueError was raised")
+            raise Exception('No Exception')
         finally:
             yield from ws.close()
         return ws
@@ -59,8 +57,32 @@ def test_websocket_json_invalid_message(create_app_and_client):
     payload = 'NOT A VALID JSON STRING'
     ws.send_str(payload)
 
-    resp = yield from ws.receive()
-    assert payload in resp.data
+    data = yield from ws.receive_str()
+    assert 'ValueError was raised' in data
+
+
+@pytest.mark.run_loop
+def test_websocket_send_json(create_app_and_client):
+    @asyncio.coroutine
+    def handler(request):
+        ws = web.WebSocketResponse()
+        yield from ws.prepare(request)
+
+        data = yield from ws.receive_json()
+        ws.send_json(data)
+
+        yield from ws.close()
+        return ws
+
+    app, client = yield from create_app_and_client()
+    app.router.add_route('GET', '/', handler)
+
+    ws = yield from client.ws_connect('/')
+    expected_value = 'value'
+    ws.send_json({'test': expected_value})
+
+    data = yield from ws.receive_json()
+    assert data['test'] == expected_value
 
 
 @pytest.mark.run_loop
