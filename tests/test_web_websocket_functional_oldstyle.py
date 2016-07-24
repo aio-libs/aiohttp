6b27a8a8e2ab785a80bccf17a3abb7a9f62f8c5f
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-07-24 14:12:48 +0200

    Convert another test

--- tests/test_web_websocket_functional_oldstyle.py
+++ tests/test_web_websocket_functional_oldstyle.py
@@ -82,41 +82,6 @@ class TestWebWebSocketFunctional(unittest.TestCase):
 
         return response, reader, writer
 
-    def test_send_recv_bytes(self):
-
-        closed = helpers.create_future(self.loop)
-
-        @asyncio.coroutine
-        def handler(request):
-            ws = web.WebSocketResponse()
-            yield from ws.prepare(request)
-
-            msg = yield from ws.receive_bytes()
-            ws.send_bytes(msg+b'/answer')
-            yield from ws.close()
-            closed.set_result(1)
-            return ws
-
-        @asyncio.coroutine
-        def go():
-            _, _, url = yield from self.create_server('GET', '/', handler)
-            resp, reader, writer = yield from self.connect_ws(url)
-            writer.send(b'ask', binary=True)
-            msg = yield from reader.read()
-            self.assertEqual(msg.tp, websocket.MSG_BINARY)
-            self.assertEqual(b'ask/answer', msg.data)
-
-            msg = yield from reader.read()
-            self.assertEqual(msg.tp, websocket.MSG_CLOSE)
-            self.assertEqual(msg.data, 1000)
-            self.assertEqual(msg.extra, '')
-
-            writer.close()
-            yield from closed
-            resp.close()
-
-        self.loop.run_until_complete(go())
-
     def test_auto_pong_with_closing_by_peer(self):
 
         closed = helpers.create_future(self.loop)
