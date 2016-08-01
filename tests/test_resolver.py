83ca6f83493bbbebf404e885d414aa6298d2a6ec
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-08-01 03:10:07 +0300

    Improve resolver tests

--- tests/test_resolver.py
+++ tests/test_resolver.py
@@ -37,80 +37,59 @@ def fake_addrinfo(hosts):
 
 
 @pytest.mark.skipif(aiodns is None, reason="aiodns required")
+@pytest.mark.run_loop
 def test_async_resolver_positive_lookup(loop):
-    @asyncio.coroutine
-    def go():
-        with patch('aiodns.DNSResolver.query') as mock_query:
-            mock_query.return_value = fake_result(['127.0.0.1'])
-            resolver = AsyncResolver(loop=loop)
-            real = yield from resolver.resolve('www.python.org')
-            ipaddress.ip_address(real[0]['host'])
-    loop.run_until_complete(go())
-
-
-@pytest.mark.skipif(aiodns is None, reason="aiodns required")
-def test_async_resolver_multiple_replies(loop):
-    @asyncio.coroutine
-    def go():
-        with patch('aiodns.DNSResolver.query') as mock_query:
-            ips = ['127.0.0.1', '127.0.0.2', '127.0.0.3', '127.0.0.4']
-            mock_query.return_value = fake_result(ips)
-            resolver = AsyncResolver(loop=loop)
-            real = yield from resolver.resolve('www.google.com')
-            ips = [ipaddress.ip_address(x['host']) for x in real]
-            assert len(ips) > 3, "Expecting multiple addresses"
-    loop.run_until_complete(go())
-
-
-@pytest.mark.skipif(aiodns is None, reason="aiodns required")
-def test_async_negative_lookup(loop):
-    @asyncio.coroutine
-    def go():
-        with patch('aiodns.DNSResolver.query') as mock_query:
-            mock_query.side_effect = aiodns.error.DNSError()
-            resolver = AsyncResolver(loop=loop)
-            try:
-                yield from resolver.resolve('doesnotexist.bla')
-                assert False, "Expecting aiodns.error.DNSError"
-            except aiodns.error.DNSError:
-                pass
-
-    loop.run_until_complete(go())
-
-
-def test_default_resolver_positive_lookup(loop):
-    @asyncio.coroutine
-    def go():
-        loop.getaddrinfo = fake_addrinfo(["127.0.0.1"])
-        resolver = DefaultResolver(loop=loop)
+    with patch('aiodns.DNSResolver.query') as mock_query:
+        mock_query.return_value = fake_result(['127.0.0.1'])
+        resolver = AsyncResolver(loop=loop)
         real = yield from resolver.resolve('www.python.org')
         ipaddress.ip_address(real[0]['host'])
 
-    loop.run_until_complete(go())
 
-
-def test_default_resolver_multiple_replies(loop):
-    @asyncio.coroutine
-    def go():
+@pytest.mark.skipif(aiodns is None, reason="aiodns required")
+@pytest.mark.run_loop
+def test_async_resolver_multiple_replies(loop):
+    with patch('aiodns.DNSResolver.query') as mock_query:
         ips = ['127.0.0.1', '127.0.0.2', '127.0.0.3', '127.0.0.4']
-        loop.getaddrinfo = fake_addrinfo(ips)
-        resolver = DefaultResolver(loop=loop)
+        mock_query.return_value = fake_result(ips)
+        resolver = AsyncResolver(loop=loop)
         real = yield from resolver.resolve('www.google.com')
         ips = [ipaddress.ip_address(x['host']) for x in real]
         assert len(ips) > 3, "Expecting multiple addresses"
-    loop.run_until_complete(go())
 
 
-def test_default_negative_lookup(loop):
-    @asyncio.coroutine
-    def go():
-        ips = []
-        loop.getaddrinfo = fake_addrinfo(ips)
-        resolver = DefaultResolver(loop=loop)
-        try:
+@pytest.mark.skipif(aiodns is None, reason="aiodns required")
+@pytest.mark.run_loop
+def test_async_negative_lookup(loop):
+    with patch('aiodns.DNSResolver.query') as mock_query:
+        mock_query.side_effect = aiodns.error.DNSError()
+        resolver = AsyncResolver(loop=loop)
+        with pytest.raises(aiodns.error.DNSError):
             yield from resolver.resolve('doesnotexist.bla')
-            assert False, "Expecting socket.gaierror"
-        except socket.gaierror:
-            pass
 
-    loop.run_until_complete(go())
+
+@pytest.mark.run_loop
+def test_default_resolver_positive_lookup(loop):
+    loop.getaddrinfo = fake_addrinfo(["127.0.0.1"])
+    resolver = DefaultResolver(loop=loop)
+    real = yield from resolver.resolve('www.python.org')
+    ipaddress.ip_address(real[0]['host'])
+
+
+@pytest.mark.run_loop
+def test_default_resolver_multiple_replies(loop):
+    ips = ['127.0.0.1', '127.0.0.2', '127.0.0.3', '127.0.0.4']
+    loop.getaddrinfo = fake_addrinfo(ips)
+    resolver = DefaultResolver(loop=loop)
+    real = yield from resolver.resolve('www.google.com')
+    ips = [ipaddress.ip_address(x['host']) for x in real]
+    assert len(ips) > 3, "Expecting multiple addresses"
+
+
+@pytest.mark.run_loop
+def test_default_negative_lookup(loop):
+    ips = []
+    loop.getaddrinfo = fake_addrinfo(ips)
+    resolver = DefaultResolver(loop=loop)
+    with pytest.raises(socket.gaierror):
+        yield from resolver.resolve('doesnotexist.bla')
