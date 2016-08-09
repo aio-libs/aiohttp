4d60ec8a09823d7bec04eb56622e98d376952e56
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-08-09 03:12:37 +0300

    Drop useless code

--- aiohttp/web.py
+++ aiohttp/web.py
@@ -169,14 +169,10 @@ class RequestHandlerFactory:
 
     def __call__(self):
         self.num_connections += 1
-        try:
-            return self._handler(
-                self, self._app, self._router, loop=self._loop,
-                secure_proxy_ssl_header=self._secure_proxy_ssl_header,
-                **self._kwargs)
-        except:
-            web_logger.exception(
-                'Can not create request handler: {!r}'.format(self._handler))
+        return self._handler(
+            self, self._app, self._router, loop=self._loop,
+            secure_proxy_ssl_header=self._secure_proxy_ssl_header,
+            **self._kwargs)
 
 
 class Application(dict):
