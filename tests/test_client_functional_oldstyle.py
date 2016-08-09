fbcde06591b14f10d6acb7f10f8275a1f8f17814
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-08-09 04:48:05 +0300

    Convert a test

--- tests/test_client_functional_oldstyle.py
+++ tests/test_client_functional_oldstyle.py
@@ -31,24 +31,6 @@ class TestHttpClientFunctional(unittest.TestCase):
         self.loop.close()
         gc.collect()
 
-    def test_HTTP_200_OK_METHOD(self):
-        with test_utils.run_server(self.loop, router=Functional) as httpd:
-            for meth in ('get', 'post', 'put', 'delete', 'head'):
-                r = self.loop.run_until_complete(
-                    client.request(meth, httpd.url('method', meth),
-                                   loop=self.loop))
-                content1 = self.loop.run_until_complete(r.read())
-                content2 = self.loop.run_until_complete(r.read())
-                content = content1.decode()
-
-                self.assertEqual(r.status, 200)
-                if meth == 'head':
-                    self.assertEqual(b'', content1)
-                else:
-                    self.assertIn('"method": "%s"' % meth.upper(), content)
-                self.assertEqual(content1, content2)
-                r.close()
-
     def test_HTTP_200_OK_METHOD_connector(self):
         with test_utils.run_server(self.loop, router=Functional) as httpd:
             conn = aiohttp.TCPConnector(
