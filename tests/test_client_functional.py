0baac725295eb1f51eca3d7561f30bf532cdb306
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-08-10 12:47:50 +0300

    Convert more tests

--- tests/test_client_functional.py
+++ tests/test_client_functional.py
@@ -1081,3 +1081,28 @@ def test_POST_MULTIPART(create_app_and_client):
     resp = yield from client.post('/', data=writer)
     assert 200 == resp.status
     resp.close()
+
+
+@pytest.mark.run_loop
+def test_POST_FILES_IO_WITH_PARAMS(create_app_and_client):
+    @asyncio.coroutine
+    def handler(request):
+        data = yield from request.post()
+        assert data['test'] == 'true'
+        assert data['unknown'].content_type == 'application/octet-stream'
+        assert data['unknown'].filename == 'unknown'
+        assert data['unknown'].file.read() == b'data'
+        assert data.getall('q') == ['t1', 't2']
+
+        return web.HTTPOk()
+
+    app, client = yield from create_app_and_client()
+    app.router.add_post('/', handler)
+
+    data = io.BytesIO(b'data')
+    resp = yield from client.post('/', data=(('test', 'true'),
+                                             MultiDict(
+                                                 [('q', 't1'), ('q', 't2')]),
+                                             data))
+    assert 200 == resp.status
+    resp.close()
--- tests/test_client_functional_oldstyle.py
+++ tests/test_client_functional_oldstyle.py
@@ -100,44 +100,6 @@ class TestHttpClientFunctional(unittest.TestCase):
             self.assertEqual(r.status, 200)
             r.close()
 
-    def test_POST_FILES_IO_WITH_PARAMS(self):
-        with test_utils.run_server(self.loop, router=Functional) as httpd:
-            url = httpd.url('method', 'post')
-
-            data = io.BytesIO(b'data')
-
-            r = self.loop.run_until_complete(
-                client.request('post', url,
-                               data=(('test', 'true'),
-                                     MultiDict(
-                                         [('q', 't1'), ('q', 't2')]),
-                                     data),
-                               loop=self.loop))
-
-            content = self.loop.run_until_complete(r.json())
-
-            self.assertEqual(4, len(content['multipart-data']))
-            self.assertEqual(
-                {'content-type': 'text/plain',
-                 'data': 'true',
-                 'name': 'test'}, content['multipart-data'][0])
-            self.assertEqual(
-                {'content-type': 'application/octet-stream',
-                 'data': 'data',
-                 'filename': 'unknown',
-                 'filename*': "utf-8''unknown",
-                 'name': 'unknown'}, content['multipart-data'][1])
-            self.assertEqual(
-                {'content-type': 'text/plain',
-                 'data': 't1',
-                 'name': 'q'}, content['multipart-data'][2])
-            self.assertEqual(
-                {'content-type': 'text/plain',
-                 'data': 't2',
-                 'name': 'q'}, content['multipart-data'][3])
-            self.assertEqual(r.status, 200)
-            r.close()
-
     def test_POST_FILES_WITH_DATA(self):
         with test_utils.run_server(self.loop, router=Functional) as httpd:
             url = httpd.url('method', 'post')
