af15f579ea29f489c0a6ebb7e3815ff84a945428
Eugene Naydenov <t.34.oxygen@gmail.com>
2016-08-24 13:58:33 +0300

    Call `Application.startup` in GunicornWebWorker (#1105)
    * Call `Application.startup` in GunicornWebWorker
    
    - Similar call as done before for `web.run_app`
    - Extended test case to check `worker.wsgi.startup` has been called once.
    
    * Run self.wsgi.startup() before self._runner

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -54,13 +54,16 @@ def test_init_process(worker):
 
 
 def test_run(worker, loop):
+    worker.wsgi = mock.Mock()
+
     worker.loop = loop
     worker._run = mock.Mock(
         wraps=asyncio.coroutine(lambda: None))
+    worker.wsgi.startup = make_mocked_coro(None)
     with pytest.raises(SystemExit):
         worker.run()
-
     assert worker._run.called
+    worker.wsgi.startup.assert_called_once_with()
     assert loop.is_closed()
 
 
