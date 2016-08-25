471df5c4c9bf54a7843ae97c0d48b8182eda2609
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-08-25 09:50:00 +0300

    Get rid of custom warning fixture

--- tests/test_web_application.py
+++ tests/test_web_application.py
@@ -59,15 +59,15 @@ def test_app_register_coro(loop):
 
 
 @asyncio.coroutine
-def test_app_register_and_finish_are_deprecated(loop, warning):
+def test_app_register_and_finish_are_deprecated(loop):
     app = web.Application(loop=loop)
     cb1 = mock.Mock()
     cb2 = mock.Mock()
-    with warning(DeprecationWarning):
+    with pytest.warns(DeprecationWarning):
         app.register_on_finish(cb1, 1, b=2)
-    with warning(DeprecationWarning):
+    with pytest.warns(DeprecationWarning):
         app.register_on_finish(cb2, 2, c=3)
-    with warning(DeprecationWarning):
+    with pytest.warns(DeprecationWarning):
         yield from app.finish()
     cb1.assert_called_once_with(app, 1, b=2)
     cb2.assert_called_once_with(app, 2, c=3)
