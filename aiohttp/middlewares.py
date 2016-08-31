0506abc5d56819792a8824699dcffcd9e989b015
argaen <manu.mirandad@gmail.com>
2016-08-31 23:22:27 +0200

    Defined new Request.clone method

--- aiohttp/middlewares.py
+++ aiohttp/middlewares.py
@@ -2,7 +2,6 @@ import asyncio
 
 # from aiohttp.web_exceptions import HTTPMovedPermanently, HTTPNotFound
 from aiohttp.web_urldispatcher import SystemRoute
-from aiohttp.protocol import RawRequestMessage
 
 
 @asyncio.coroutine
@@ -18,16 +17,11 @@ def normalize_path_middleware(app, handler):
     def middleware(request):
 
         router = request.app.router
-        match_info = yield from router.resolve(request)
-        if not isinstance(match_info.route, SystemRoute):
+        if not isinstance(request.match_info.route, SystemRoute):
             resp = yield from handler(request)
 
         elif not request.path.endswith('/'):
-            message = RawRequestMessage(
-                request.method, request.path + '/', request.version,
-                request.headers, request.raw_headers,
-                request.keep_alive, None)
-            request = request.copy(message=message)
+            request = request.clone(path=request.path + '/')
             match_info = yield from router.resolve(request)
             resp = yield from match_info.handler(request)
 
