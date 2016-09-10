96b4ff86a93ff1fc2b7a4893c70a7f2a2ca292f0
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-09-10 11:52:19 +0300

    Fix __aiter__

--- aiohttp/client_ws.py
+++ aiohttp/client_ws.py
@@ -5,9 +5,9 @@ import json
 import sys
 
 from ._ws_impl import CLOSED_MESSAGE, WebSocketError, WSMessage, WSMsgType
-from .helpers import _decorate_aiter
 
 PY_35 = sys.version_info >= (3, 5)
+PY_352 = sys.version_info >= (3, 5, 2)
 
 
 class ClientWebSocketResponse:
@@ -179,9 +179,11 @@ class ClientWebSocketResponse:
         return loads(data)
 
     if PY_35:
-        @_decorate_aiter
         def __aiter__(self):
             return self
+
+        if not PY_352:
+            __aiter__ = asyncio.coroutine(__aiter__)
 
         @asyncio.coroutine
         def __anext__(self):
