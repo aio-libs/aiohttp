5e8217ea158ebfaff4e43f633defc262b7a4a039
Pawel Miech <pawelmhm@gmail.com>
2016-11-12 11:27:27 +0100

    [web_request] add handling of client_max_size
    * default limit set to value same as in nginx 1mb;
    * if client tries to upload body that exceeds this limit it will get 413 HTTP Error;
    * limit can be updated by massing client_max_size argument to Application object

--- tests/test_web_request.py
+++ tests/test_web_request.py
@@ -6,6 +6,7 @@ import pytest
 from multidict import CIMultiDict, MultiDict
 from yarl import URL
 
+from aiohttp import web_exceptions
 from aiohttp.protocol import HttpVersion
 from aiohttp.streams import StreamReader
 from aiohttp.test_utils import make_mocked_request
@@ -318,3 +319,31 @@ def test_cannot_clone_after_read(loop):
     yield from req.read()
     with pytest.raises(RuntimeError):
         req.clone()
+
+
+@asyncio.coroutine
+def test_make_too_big_request(loop):
+    payload = StreamReader(loop=loop)
+    large_file = 1024 ** 2 * b'x'
+    too_large_file = large_file + b'x'
+    payload.feed_data(too_large_file)
+    payload.feed_eof()
+    req = make_mocked_request('POST', '/', payload=payload)
+    with pytest.raises(web_exceptions.HTTPRequestEntityTooLarge) as err:
+        yield from req.read()
+    assert err.value.status == 413
+    assert err.value.text == 'Request body too large'
+
+
+@asyncio.coroutine
+def test_make_too_big_request_adjust_limit(loop):
+    payload = StreamReader(loop=loop)
+    large_file = 1024 ** 2 * b'x'
+    too_large_file = large_file + b'x'
+    payload.feed_data(too_large_file)
+    payload.feed_eof()
+    max_size = 1024**2 + 2
+    req = make_mocked_request('POST', '/', payload=payload,
+                              client_max_size=max_size)
+    txt = yield from req.read()
+    assert len(txt) == 1024**2 + 1
