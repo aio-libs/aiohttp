d13a4bd01415fa76fe35a6eef49e49e376a3d99f
Nikolay Kim <nikolay.kim@affirm.com>
2017-01-21 13:06:22 -0800

    fully mock loop calls for multi bind test

--- tests/test_run_app.py
+++ tests/test_run_app.py
@@ -19,19 +19,13 @@ def test_run_app_http(loop, mocker):
     app.startup.assert_called_once_with()
 
 
-def test_run_app_multi_bind(loop, mocker):
-    mocker.spy(loop, 'create_server')
-    loop.call_later(0.05, loop.stop)
+def test_run_app_multi_bind():
+    app = mock.Mock()
 
-    app = web.Application(loop=loop)
-    mocker.spy(app, 'startup')
+    web.run_app(app, host=('0.0.0.0', '127.0.0.1'), print=lambda *args: None)
 
-    web.run_app(app, host=('0.0.0.0', '0.0.0.0'), print=lambda *args: None)
-
-    assert loop.is_closed()
-    loop.create_server.assert_called_with(
-        mock.ANY, ('0.0.0.0', '0.0.0.0'), 8080, ssl=None, backlog=128)
-    app.startup.assert_called_once_with()
+    app.loop.create_server.assert_called_with(
+        mock.ANY, ('0.0.0.0', '127.0.0.1'), 8080, ssl=None, backlog=128)
 
 
 def test_run_app_http_access_format(loop, mocker):
