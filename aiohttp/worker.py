a6f13c25b66b11d912ed1fa4e240d086b97375d0
Nikolay Kim <nikolay.kim@affirm.com>
2017-02-15 11:18:40 -0800

    disable access log if gunicron is not configured

--- aiohttp/worker.py
+++ aiohttp/worker.py
@@ -55,11 +55,12 @@ class GunicornWebWorker(base.Worker):
 
     def make_handler(self, app):
         if hasattr(self.wsgi, 'make_handler'):
+            access_log = self.log.access_log if self.cfg.accesslog else None
             return app.make_handler(
                 logger=self.log,
                 slow_request_timeout=self.cfg.timeout,
                 keepalive_timeout=self.cfg.keepalive,
-                access_log=self.log.access_log,
+                access_log=access_log,
                 access_log_format=self._get_valid_log_format(
                     self.cfg.access_log_format))
         else:
