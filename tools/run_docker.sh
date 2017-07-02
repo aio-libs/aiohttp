5f77bf190fb076b3c8beee7a2709a112a5ee7a77
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2017-07-02 21:21:04 +0300

    Optimize docker running script
    * Introduce package_name script param
    * Reduce nesting
    * Reduce code duplication

--- tools/run_docker.sh
+++ tools/run_docker.sh
@@ -1,25 +1,33 @@
-if [ ! -z $TRAVIS_TAG ] && [ -z $PYTHONASYNCIODEBUG ] && [ -z $AIOHTTP_NO_EXTENSIONS] ;then
-    echo "x86_64"
-    docker pull quay.io/pypa/manylinux1_x86_64
-    docker run --rm -v `pwd`:/io quay.io/pypa/manylinux1_x86_64 /io/tools/build-wheels.sh
-    echo "Dist folder content is:"
-    for f in dist/aiohttp*manylinux1_x86_64.whl
-    do
-        echo "Upload $f"
-        python -m twine upload $f --username andrew.svetlov --password $PYPI_PASSWD
-    done
-    echo "Cleanup"
-    docker run --rm -v `pwd`:/io quay.io/pypa/manylinux1_x86_64 rm -rf /io/dist
-
-    echo "i686"
-    docker pull quay.io/pypa/manylinux1_i686
-    docker run --rm -v `pwd`:/io quay.io/pypa/manylinux1_i686 linux32 /io/tools/build-wheels.sh
-    echo "Dist folder content is:"
-    for f in dist/aiohttp*manylinux1_i686.whl
-    do
-        echo "Upload $f"
-        python -m twine upload $f --username andrew.svetlov --password $PYPI_PASSWD
-    done
-    echo "Cleanup"
-    docker run --rm -v `pwd`:/io quay.io/pypa/manylinux1_i686 rm -rf /io/dist
+#!/bin/bash
+if [ -z $TRAVIS_TAG ] || [ ! -z $PYTHONASYNCIODEBUG ] || [ ! -z $AIOHTTP_NO_EXTENSIONS ]
+then
+    exit 1
 fi
+
+package_name="$1"
+if [ -z "$package_name" ]
+then
+    &>2 echo "Please pass package name as a first argument of this script ($0)"
+    exit 1
+fi
+
+dock_ext_args=""
+
+for arch in x86_64 i686
+do
+    [ $arch == "i686" ] && dock_ext_args="linux32"
+
+    echo "${arch}"
+    docker pull "quay.io/pypa/manylinux1_${arch}"
+    docker run --rm -v `pwd`:/io "quay.io/pypa/manylinux1_${arch}" $dock_ext_args /io/tools/build-wheels.sh "$package_name"
+
+    echo "Dist folder content is:"
+    for f in dist/aiohttp*manylinux1_${arch}.whl
+    do
+        echo "Upload $f"
+        python -m twine upload "$f" --username andrew.svetlov --password "$PYPI_PASSWD"
+    done
+
+    echo "Cleanup"
+    docker run --rm -v `pwd`:/io "quay.io/pypa/manylinux1_${arch}" rm -rf /io/dist
+done
