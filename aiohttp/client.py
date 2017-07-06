08e43c97dea47182677485da3b2ab00a46a2634b
litwisha <litwisha@gmail.com>
2017-07-06 15:25:32 +0300

    `yield from session.close()` for asynchronous context manager ClientSession

--- aiohttp/client.py
+++ aiohttp/client.py
@@ -584,7 +584,7 @@ class ClientSession:
 
         @asyncio.coroutine
         def __aexit__(self, exc_type, exc_val, exc_tb):
-            self.close()
+            yield from self.close()
 
 
 if PY_35:
