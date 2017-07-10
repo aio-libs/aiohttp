594b9a68285923a1e8119caa7bd759cde5469873
Cecile Tonglet <cecile.tonglet@gmail.com>
2017-07-10 14:09:38 +0200

    Fix missing app.loop in startup hook during test (#2068)
    Fixes #2060

--- aiohttp/test_utils.py
+++ aiohttp/test_utils.py
@@ -148,6 +148,7 @@ class TestServer(BaseTestServer):
 
     @asyncio.coroutine
     def _make_factory(self, **kwargs):
+        self.app._set_loop(self._loop)
         yield from self.app.startup()
         self.handler = self.app.make_handler(loop=self._loop, **kwargs)
         return self.handler
