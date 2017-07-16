8854fcb7125650cf33aeb0cfa7da97892893d97e
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2017-07-16 15:02:01 +0300

    Parallelize pulling manylinux1 docker images

--- tools/run_docker.sh
+++ tools/run_docker.sh
@@ -6,23 +6,27 @@ then
     exit 1
 fi
 
+manylinux1_image_prefix="quay.io/pypa/manylinux1_"
 dock_ext_args=""
+declare -A docker_pull_pids=()  # This syntax requires at least bash v4
 
 for arch in x86_64 i686
 do
+    docker pull "${manylinux1_image_prefix}${arch}" &
+    docker_pull_pids[$arch]=$!
+done
+
+for arch in x86_64 i686
+do
+    echo
+    echo
+    arch_pull_pid=${docker_pull_pids[$arch]}
+    echo waiting for docker pull pid $arch_pull_pid to complete downloading container for $arch arch...
+    wait $arch_pull_pid  # await for docker image for current arch to be pulled from hub
     [ $arch == "i686" ] && dock_ext_args="linux32"
 
-    echo "${arch}"
-    docker pull "quay.io/pypa/manylinux1_${arch}"
-    docker run --rm -v `pwd`:/io "quay.io/pypa/manylinux1_${arch}" $dock_ext_args /io/tools/build-wheels.sh "$package_name"
+    echo Building wheel for $arch arch
+    docker run --rm -v `pwd`:/io "${manylinux1_image_prefix}${arch}" $dock_ext_args /io/tools/build-wheels.sh "$package_name"
 
-    echo "Dist folder content is:"
-    for f in dist/aiohttp*manylinux1_${arch}.whl
-    do
-        echo "Upload $f"
-        python -m twine upload "$f" --username andrew.svetlov --password "$PYPI_PASSWD"
-    done
-
-    echo "Cleanup"
-    rm -rf ./dist
+    dock_ext_args=""  # Reset docker args, just in case
 done
