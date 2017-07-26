4fe28b5613b10c2c6cb801dc344832685eff64b6
Emmanuel Coirier <manuco@club-internet.fr>
2017-07-26 10:30:04 +0200

    Add test case for #2087 : base64 long line handling.

--- tests/test_multipart.py
+++ tests/test_multipart.py
@@ -914,6 +914,19 @@ def test_writer_with_content_transfer_encoding_base64(buf, stream, writer):
 
 
 @asyncio.coroutine
+def test_writer_with_content_te_base64_no_long_lines(buf, stream, writer):
+    writer.append('Time to write long lines!' * 10,
+                  {CONTENT_TRANSFER_ENCODING: 'base64'}
+    )
+    yield from writer.write(stream)
+    headers, message = bytes(buf).split(b'\r\n\r\n', 1)
+
+    line0 = message.split(b'\r\n')
+    assert len(line0) < 80
+
+
+
+@asyncio.coroutine
 def test_writer_content_transfer_encoding_quote_printable(buf, stream, writer):
     writer.append('Привет, мир!',
                   {CONTENT_TRANSFER_ENCODING: 'quoted-printable'})
