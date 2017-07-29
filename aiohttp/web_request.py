96e8d6edebcf3fc6981b527359e288a66ff26cfa
Andrew Svetlov <andrew.svetlov@gmail.com>
2017-07-29 18:25:25 +0300

    Access logger improvements (#2145)
    * Fix #2123: User request instead of message in access logger
    
    * Doc update
    
    * Optimize request.host property
    
    * Introduce BaseRequest.remote property
    
    * Fix spelling errors
    
    * Add tests

--- aiohttp/web_request.py
+++ aiohttp/web_request.py
@@ -275,7 +275,7 @@ class BaseRequest(collections.MutableMapping, HeadersMixin):
 
     @reify
     def host(self):
-        """ Hostname of the request.
+        """Hostname of the request.
 
         Hostname is resolved through the following headers, in this order:
 
@@ -288,11 +288,37 @@ class BaseRequest(collections.MutableMapping, HeadersMixin):
         host = next(
             (f['host'] for f in self.forwarded if 'host' in f), None
         )
-        if not host and hdrs.X_FORWARDED_HOST in self._message.headers:
-            host = self._message.headers[hdrs.X_FORWARDED_HOST]
-        elif hdrs.HOST in self._message.headers:
-            host = self._message.headers[hdrs.HOST]
+        if host is None:
+            host = self._message.headers.get(hdrs.X_FORWARDED_HOST)
+        if host is None:
+            host = self._message.headers.get(hdrs.HOST)
         return host
+
+    @reify
+    def remote(self):
+        """Remote IP of client initiated HTTP request.
+
+        The IP is resolved through the following headers, in this order:
+
+        - Forwarded
+        - X-Forwarded-For
+        - peername of opened socket
+        """
+        ip = next(
+            (f['for'] for f in self.forwarded if 'for' in f), None
+        )
+        if ip is None:
+            ips = self._message.headers.get(hdrs.X_FORWARDED_FOR)
+            if ips is not None:
+                ip = ips.split(',')[0].strip()
+        if ip is None:
+            transport = self._transport
+            peername = transport.get_extra_info('peername')
+            if isinstance(peername, (list, tuple)):
+                ip = peername[0]
+            else:
+                ip = peername
+        return ip
 
     @reify
     def url(self):
