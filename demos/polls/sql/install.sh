504aca34f4d7d8d43d060b1bf4975a6a74b8fa48
Thijs Triemstra <info@collab.nl>
2017-07-31 15:09:08 +0200

    use osx postgres user (#2149)
    * use osx postgres user
    
    * Create 2149.misc

--- demos/polls/sql/install.sh
+++ demos/polls/sql/install.sh
@@ -1,8 +1,15 @@
-sudo -u postgres psql -c "DROP DATABASE IF EXISTS aiohttpdemo_polls"
-sudo -u postgres psql -c "DROP ROLE IF EXISTS aiohttpdemo_user"
-sudo -u postgres psql -c "CREATE USER aiohttpdemo_user WITH PASSWORD 'aiohttpdemo_user';"
-sudo -u postgres psql -c "CREATE DATABASE aiohttpdemo_polls ENCODING 'UTF8';" 
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE aiohttpdemo_polls TO aiohttpdemo_user;"
+# determine os
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Darwin*)    pg_cmd="psql -U postgres";;
+    *)          pg_cmd="sudo -u postgres psql"
+esac
 
-cat sql/create_tables.sql | sudo -u postgres psql -d aiohttpdemo_polls -a 
-cat sql/sample_data.sql | sudo -u postgres psql -d aiohttpdemo_polls -a 
+${pg_cmd} -c "DROP DATABASE IF EXISTS aiohttpdemo_polls"
+${pg_cmd} -c "DROP ROLE IF EXISTS aiohttpdemo_user"
+${pg_cmd} -c "CREATE USER aiohttpdemo_user WITH PASSWORD 'aiohttpdemo_user';"
+${pg_cmd} -c "CREATE DATABASE aiohttpdemo_polls ENCODING 'UTF8';"
+${pg_cmd} -c "GRANT ALL PRIVILEGES ON DATABASE aiohttpdemo_polls TO aiohttpdemo_user;"
+
+cat sql/create_tables.sql | ${pg_cmd} -d aiohttpdemo_polls -a
+cat sql/sample_data.sql | ${pg_cmd} -d aiohttpdemo_polls -a
