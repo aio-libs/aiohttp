b32992432f9edbca345e7b8b3dd9990acde70984
Pau Freixes <pfreixes@gmail.com>
2017-07-31 07:04:47 +0200

    Throttle simultaneous DNS requests #1924 (#2111)
    * Throttle simultaneous DNS requests
    
    Added a new option at TCPConnector level called `throttle_dns` that
    allows to the connector throttle simulatnewous DNS requests for a specific
    domain. This functionality its only compatible when the DNS cache is
    enabled and it is dissabled by default. Therefore,  many requests can
    end up making the same DNS resolution.
    
    * Explicit loop for sleep calls
    
    * Fixed typos in documentation
    
    * Added DNS request throttling in CHANGES.rst
    
    * Dogpile invalid word by doc-spelling
    
    * Get full coverage for locks
    
    * Throttle DNS always enabled
    
    Will help the user to throttle DNS requests to the same domain. By
    design it will happen always when many URLs behind the same domain are
    scheduled at the same time, perhaps via *gather*
    
    The Event class is no longer a derivated class of the
    asyncio.locks.Event, and makes a full copy of this one to avoid future
    issues.
    
    * Remove implicit loop tests for locks.Event
    
    * Use create_future helper
    
    * Updated spelling wordlist
    
    * Use a simple Event wrapper instead of a full Event class implementation
    
    * Pass the loop explicitly
    
    * Cancel pending throttled DNS requests if connector is close
    
    * Removed not used attribute
    
    * Pass loop explicit
    
    * Throtlle DNS feature adapted to CHANGES protocol
    
    * Update 2111.feature
    
    * Changed class name

---
+++ aiohttp/locks.py
@@ -0,0 +1,42 @@
+import asyncio
+import collections
+
+from .helpers import ensure_future
+
+
+class EventResultOrError:
+    """
+    This class wrappers the Event asyncio lock allowing either awake the
+    locked Tasks without any error or raising an exception.
+
+    thanks to @vorpalsmith for the simple design.
+    """
+    def __init__(self, loop):
+        self._loop = loop
+        self._exc = None
+        self._event = asyncio.Event(loop=loop)
+        self._waiters = collections.deque()
+
+    def set(self, exc=None):
+        self._exc = exc
+        self._event.set()
+
+    @asyncio.coroutine
+    def wait(self):
+        fut = ensure_future(self._event.wait(), loop=self._loop)
+        self._waiters.append(fut)
+        try:
+            val = yield from fut
+        finally:
+            self._waiters.remove(fut)
+
+        if self._exc is not None:
+            raise self._exc
+
+        return val
+
+    def cancel(self):
+        """ Cancel all waiters """
+        for fut in self._waiters:
+            if not fut.done():
+                fut.cancel()
