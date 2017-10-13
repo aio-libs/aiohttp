e11c82a69f26694564fa94eb0f976bb621328e08
Victor K <hellysmile@gmail.com>
2017-10-13 17:39:57 +0300

    aiohttp.wep.Application.make_handler access_log_class support (#2316)
    * Initial access_log_factory support.
    
    * Added changes.
    
    * abstraction level changes for AccessLogger.
    
    * Added RequestHandler docstrings.
    
    * Fix changes naming.
    
    * Added access_log_class check for Application.make_handler
    
    * Added basic tests for access_log_class.
    
    * Added docs for access_log_class
    
    * Fix typo request->response
    
    * _log -> log
    
    * Added more details to TypeError regarding access_log_class.

--- tests/test_web_application.py
+++ tests/test_web_application.py
@@ -4,7 +4,7 @@ from unittest import mock
 import pytest
 
 from aiohttp import helpers, log, web
-from aiohttp.abc import AbstractRouter
+from aiohttp.abc import AbstractAccessLogger, AbstractRouter
 
 
 def test_app_ctor(loop):
@@ -65,6 +65,7 @@ def test_app_make_handler_debug_exc(loop, mocker, debug):
     app.make_handler(loop=loop)
     srv.assert_called_with(app._handle,
                            request_factory=app._make_request,
+                           access_log_class=mock.ANY,
                            loop=loop,
                            debug=debug)
 
@@ -76,7 +77,31 @@ def test_app_make_handler_args(loop, mocker):
     app.make_handler(loop=loop)
     srv.assert_called_with(app._handle,
                            request_factory=app._make_request,
+                           access_log_class=mock.ANY,
                            loop=loop, debug=mock.ANY, test=True)
+
+
+def test_app_make_handler_access_log_class(loop, mocker):
+    class Logger:
+        pass
+
+    app = web.Application()
+
+    with pytest.raises(TypeError):
+        app.make_handler(access_log_class=Logger, loop=loop)
+
+    class Logger(AbstractAccessLogger):
+
+        def log(self, request, response, time):
+            self.logger.info('msg')
+
+    srv = mocker.patch('aiohttp.web.Server')
+
+    app.make_handler(access_log_class=Logger, loop=loop)
+    srv.assert_called_with(app._handle,
+                           access_log_class=Logger,
+                           request_factory=app._make_request,
+                           loop=loop, debug=mock.ANY)
 
 
 @asyncio.coroutine
