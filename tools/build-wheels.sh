0ad4afdf8412068f69c1a988a31b3d55ac948f9d
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2017-10-18 00:38:21 +0300

    Cleanup pycache at start of test loop

--- tools/build-wheels.sh
+++ tools/build-wheels.sh
@@ -53,13 +53,13 @@ echo "dist directory:"
 ls /io/dist
 
 for PYTHON in ${PYTHON_VERSIONS}; do
+    # clear python cache
+    find /io -type d -name __pycache__ -delete
+
     echo
     echo -n "Test $PYTHON: "
     /opt/python/${PYTHON}/bin/python -c "import platform; print('Building wheel for {platform} platform.'.format(platform=platform.platform()))"
     /opt/python/${PYTHON}/bin/pip install -r /io/requirements/ci-wheel.txt
     /opt/python/${PYTHON}/bin/pip install "$package_name" --no-index -f file:///io/dist
     /opt/python/${PYTHON}/bin/py.test /io/tests
-
-    # clear python cache
-    find /io -type d -name __pycache__ -delete
 done
