21e12e044326bd737ebfd62a4588aadbcc91f8d4
Amin Etesamian <aminetesamian1371@gmail.com>
2017-10-19 00:39:38 +0330

    test_classbasedview now uses async/await syntax

--- tests/test_classbasedview.py
+++ tests/test_classbasedview.py
@@ -13,50 +13,44 @@ def test_ctor():
     assert view.request is request
 
 
-@asyncio.coroutine
-def test_render_ok():
+async def test_render_ok():
     resp = web.Response(text='OK')
 
     class MyView(View):
-        @asyncio.coroutine
-        def get(self):
+        async def get(self):
             return resp
 
     request = mock.Mock()
     request._method = 'GET'
-    resp2 = yield from MyView(request)
+    resp2 = await MyView(request)
     assert resp is resp2
 
 
-@asyncio.coroutine
-def test_render_unknown_method():
+async def test_render_unknown_method():
 
     class MyView(View):
-        @asyncio.coroutine
-        def get(self):
+        async def get(self):
             return web.Response(text='OK')
         options = get
 
     request = mock.Mock()
     request.method = 'UNKNOWN'
     with pytest.raises(web.HTTPMethodNotAllowed) as ctx:
-        yield from MyView(request)
+        await MyView(request)
     assert ctx.value.headers['allow'] == 'GET,OPTIONS'
     assert ctx.value.status == 405
 
 
-@asyncio.coroutine
-def test_render_unsupported_method():
+async def test_render_unsupported_method():
 
     class MyView(View):
-        @asyncio.coroutine
-        def get(self):
+        async def get(self):
             return web.Response(text='OK')
         options = delete = get
 
     request = mock.Mock()
     request.method = 'POST'
     with pytest.raises(web.HTTPMethodNotAllowed) as ctx:
-        yield from MyView(request)
+        await MyView(request)
     assert ctx.value.headers['allow'] == 'DELETE,GET,OPTIONS'
     assert ctx.value.status == 405
