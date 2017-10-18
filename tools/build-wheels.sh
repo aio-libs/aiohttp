8eedaff79704fc2ed01ed9a161a93faba8db73db
Sviatoslav Sydorenko <wk.cvs.github@sydorenko.org.ua>
2017-10-18 16:56:57 +0300

    Sync in-docker wheels creation w/ best practices (#2334)
    * Sync in-docker wheels creation w/ best practices
    
    * Fix redundant wheels deletion
    
    * Improve pycache cleanup
    
    * Improve bash strict mode
    
    * Cleanup pycache at start of test loop
    
    * Fix recursive dir deletion

--- tools/build-wheels.sh
+++ tools/build-wheels.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-set -e
+if [ -n "$DEBUG" ]
+then
+  set -x
+fi
+set -euo pipefail
+# ref: https://coderwall.com/p/fkfaqq/safer-bash-scripts-with-set-euxo-pipefail
 
 PYTHON_VERSIONS="cp34-cp34m cp35-cp35m cp36-cp36m"
 
@@ -26,13 +31,20 @@ done
 echo
 echo
 echo "Bundle external shared libraries into the wheels"
-for whl in /io/dist/${package_name}*${arch}.whl; do
+for whl in /io/dist/${package_name}-*-linux_${arch}.whl; do
     echo "Repairing $whl..."
     auditwheel repair "$whl" -w /io/dist/
 done
 
+echo
+echo
 echo "Cleanup OS specific wheels"
 rm -fv /io/dist/*-linux_*.whl
+
+echo
+echo
+echo "Cleanup non-$package_name wheels"
+find /io/dist -maxdepth 1 -type f ! -name "$package_name"'-*-manylinux1_'"$arch"'.whl' -print0 | xargs -0 rm -rf
 
 echo
 echo
@@ -41,13 +53,13 @@ echo "dist directory:"
 ls /io/dist
 
 for PYTHON in ${PYTHON_VERSIONS}; do
+    # clear python cache
+    find /io -type d -name __pycache__ -print0 | xargs -0 rm -rf
+
     echo
     echo -n "Test $PYTHON: "
-    /opt/python/${PYTHON}/bin/python -c "import platform;print(platform.platform())"
+    /opt/python/${PYTHON}/bin/python -c "import platform; print('Building wheel for {platform} platform.'.format(platform=platform.platform()))"
     /opt/python/${PYTHON}/bin/pip install -r /io/requirements/ci-wheel.txt
     /opt/python/${PYTHON}/bin/pip install "$package_name" --no-index -f file:///io/dist
     /opt/python/${PYTHON}/bin/py.test /io/tests
-
-    # clear python cache
-    find /io -name __pycache__ | xargs rm -rf
 done
