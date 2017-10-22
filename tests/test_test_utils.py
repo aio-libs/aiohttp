23f38bb35faa5d5d88c433d30ab1163f811de16c
Andrew Svetlov <andrew.svetlov@gmail.com>
2017-10-22 14:27:35 +0300

    Convert test_test_utils.py

--- tests/test_test_utils.py
+++ tests/test_test_utils.py
@@ -1,4 +1,3 @@
-import asyncio
 import gzip
 from unittest import mock
 
@@ -21,31 +20,27 @@ _hello_world_gz = gzip.compress(_hello_world_bytes)
 
 
 def _create_example_app():
-    @asyncio.coroutine
-    def hello(request):
+    async def hello(request):
         return web.Response(body=_hello_world_bytes)
 
-    @asyncio.coroutine
-    def gzip_hello(request):
+    async def gzip_hello(request):
         return web.Response(body=_hello_world_gz,
                             headers={'Content-Encoding': 'gzip'})
 
-    @asyncio.coroutine
-    def websocket_handler(request):
+    async def websocket_handler(request):
 
         ws = web.WebSocketResponse()
-        yield from ws.prepare(request)
-        msg = yield from ws.receive()
+        await ws.prepare(request)
+        msg = await ws.receive()
         if msg.type == aiohttp.WSMsgType.TEXT:
             if msg.data == 'close':
-                yield from ws.close()
+                await ws.close()
             else:
-                yield from ws.send_str(msg.data + '/answer')
+                await ws.send_str(msg.data + '/answer')
 
         return ws
 
-    @asyncio.coroutine
-    def cookie_handler(request):
+    async def cookie_handler(request):
         resp = web.Response(body=_hello_world_bytes)
         resp.set_cookie('cookie', 'val')
         return resp
@@ -83,12 +78,11 @@ def test_full_server_scenario():
         app = _create_example_app()
         with _TestClient(_TestServer(app, loop=loop), loop=loop) as client:
 
-            @asyncio.coroutine
-            def test_get_route():
+            async def test_get_route():
                 nonlocal client
-                resp = yield from client.request("GET", "/")
+                resp = await client.request("GET", "/")
                 assert resp.status == 200
-                text = yield from resp.text()
+                text = await resp.text()
                 assert _hello_world_str == text
 
             loop.run_until_complete(test_get_route())
@@ -99,12 +93,11 @@ def test_auto_gzip_decompress():
         app = _create_example_app()
         with _TestClient(_TestServer(app, loop=loop), loop=loop) as client:
 
-            @asyncio.coroutine
-            def test_get_route():
+            async def test_get_route():
                 nonlocal client
-                resp = yield from client.request("GET", "/gzip_hello")
+                resp = await client.request("GET", "/gzip_hello")
                 assert resp.status == 200
-                data = yield from resp.read()
+                data = await resp.read()
                 assert data == _hello_world_bytes
 
             loop.run_until_complete(test_get_route())
@@ -116,12 +109,11 @@ def test_noauto_gzip_decompress():
         with _TestClient(_TestServer(app, loop=loop), loop=loop,
                          auto_decompress=False) as client:
 
-            @asyncio.coroutine
-            def test_get_route():
+            async def test_get_route():
                 nonlocal client
-                resp = yield from client.request("GET", "/gzip_hello")
+                resp = await client.request("GET", "/gzip_hello")
                 assert resp.status == 200
-                data = yield from resp.read()
+                data = await resp.read()
                 assert data == _hello_world_gz
 
             loop.run_until_complete(test_get_route())
@@ -132,11 +124,10 @@ def test_server_with_create_test_teardown():
         app = _create_example_app()
         with _TestClient(_TestServer(app, loop=loop), loop=loop) as client:
 
-            @asyncio.coroutine
-            def test_get_route():
-                resp = yield from client.request("GET", "/")
+            async def test_get_route():
+                resp = await client.request("GET", "/")
                 assert resp.status == 200
-                text = yield from resp.text()
+                text = await resp.text()
                 assert _hello_world_str == text
 
             loop.run_until_complete(test_get_route())
@@ -161,71 +152,63 @@ class TestAioHTTPTestCase(AioHTTPTestCase):
         return _create_example_app()
 
     @unittest_run_loop
-    @asyncio.coroutine
-    def test_example_with_loop(self):
-        request = yield from self.client.request("GET", "/")
+    async def test_example_with_loop(self):
+        request = await self.client.request("GET", "/")
         assert request.status == 200
-        text = yield from request.text()
+        text = await request.text()
         assert _hello_world_str == text
 
     def test_example(self):
-        @asyncio.coroutine
-        def test_get_route():
-            resp = yield from self.client.request("GET", "/")
+        async def test_get_route():
+            resp = await self.client.request("GET", "/")
             assert resp.status == 200
-            text = yield from resp.text()
+            text = await resp.text()
             assert _hello_world_str == text
 
         self.loop.run_until_complete(test_get_route())
 
 
 def test_get_route(loop, test_client):
-    @asyncio.coroutine
-    def test_get_route():
-        resp = yield from test_client.request("GET", "/")
+    async def test_get_route():
+        resp = await test_client.request("GET", "/")
         assert resp.status == 200
-        text = yield from resp.text()
+        text = await resp.text()
         assert _hello_world_str == text
 
     loop.run_until_complete(test_get_route())
 
 
-@asyncio.coroutine
-def test_client_websocket(loop, test_client):
-    resp = yield from test_client.ws_connect("/websocket")
+async def test_client_websocket(loop, test_client):
+    resp = await test_client.ws_connect("/websocket")
     resp.send_str("foo")
-    msg = yield from resp.receive()
+    msg = await resp.receive()
     assert msg.type == aiohttp.WSMsgType.TEXT
     assert "foo" in msg.data
     resp.send_str("close")
-    msg = yield from resp.receive()
+    msg = await resp.receive()
     assert msg.type == aiohttp.WSMsgType.CLOSE
 
 
-@asyncio.coroutine
-def test_client_cookie(loop, test_client):
+async def test_client_cookie(loop, test_client):
     assert not test_client.session.cookie_jar
-    yield from test_client.get("/cookie")
+    await test_client.get("/cookie")
     cookies = list(test_client.session.cookie_jar)
     assert cookies[0].key == 'cookie'
     assert cookies[0].value == 'val'
 
 
-@asyncio.coroutine
 @pytest.mark.parametrize("method", [
     "get", "post", "options", "post", "put", "patch", "delete"
 ])
-@asyncio.coroutine
-def test_test_client_methods(method, loop, test_client):
-    resp = yield from getattr(test_client, method)("/")
+async def test_test_client_methods(method, loop, test_client):
+    resp = await getattr(test_client, method)("/")
     assert resp.status == 200
-    text = yield from resp.text()
+    text = await resp.text()
     assert _hello_world_str == text
 
 
-@asyncio.coroutine
-def test_test_client_head(loop, test_client):
-    resp = yield from test_client.head("/")
+async def test_test_client_head(loop, test_client):
+    resp = await test_client.head("/")
     assert resp.status == 200
 
 
@@ -287,10 +270,9 @@ def test_test_client_props(loop):
 def test_test_server_context_manager(loop):
     app = _create_example_app()
     with _TestServer(app, loop=loop) as server:
-        @asyncio.coroutine
-        def go():
+        async def go():
             client = aiohttp.ClientSession(loop=loop)
-            resp = yield from client.head(server.make_url('/'))
+            resp = await client.head(server.make_url('/'))
             assert resp.status == 200
             resp.close()
             client.close()
