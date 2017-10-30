4e0ad7db1b915419566d572e855f954a5b605f02
hellysmile <hellysmile@gmail.com>
2017-10-30 20:46:25 +0200

    Revert getpeercert

--- aiohttp/connector.py
+++ aiohttp/connector.py
@@ -828,8 +828,14 @@ class TCPConnector(BaseConnector):
             has_cert = transp.get_extra_info('sslcontext')
             if has_cert and fingerprint:
                 sock = transp.get_extra_info('socket')
+                if not hasattr(sock, 'getpeercert'):
+                    # Workaround for asyncio 3.5.0
+                    # Starting from 3.5.1 version
+                    # there is 'ssl_object' extra info in transport
+                    sock = transp._ssl_protocol._sslpipe.ssl_object
                 # gives DER-encoded cert as a sequence of bytes (or None)
                 cert = sock.getpeercert(binary_form=True)
+                assert cert
                 got = hashfunc(cert).digest()
                 expected = fingerprint
                 if got != expected:
