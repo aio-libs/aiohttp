251b321299cc9b342625e474e097d16319eba23f
Andrew Svetlov <andrew.svetlov@gmail.com>
2017-11-01 11:56:17 +0200

    Move TOC to separate RST file

--- docs/conf.py
+++ docs/conf.py
@@ -86,7 +86,7 @@ source_suffix = '.rst'
 # source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = 'toc'
 
 # General information about the project.
 project = 'aiohttp'
