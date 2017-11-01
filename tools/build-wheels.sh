db28afa026aa6a82fcd6e5d13e0060334a2cf357
Andrew Svetlov <andrew.svetlov@gmail.com>
2017-11-01 18:39:41 +0200

    Don't build python 3.4 binary wheels

--- tools/build-wheels.sh
+++ tools/build-wheels.sh
@@ -6,7 +6,7 @@ fi
 set -euo pipefail
 # ref: https://coderwall.com/p/fkfaqq/safer-bash-scripts-with-set-euxo-pipefail
 
-PYTHON_VERSIONS="cp34-cp34m cp35-cp35m cp36-cp36m"
+PYTHON_VERSIONS="cp35-cp35m cp36-cp36m"
 
 # Avoid creation of __pycache__/*.py[c|o]
 export PYTHONDONTWRITEBYTECODE=1
