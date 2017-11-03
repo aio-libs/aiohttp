04f59e7907b5b97f4ac6ae1c06781b9858691f82
Andrew Svetlov <andrew.svetlov@gmail.com>
2017-11-03 11:51:35 +0300

    Fix #2409: Get rid of warnings from tests

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -97,9 +97,10 @@ def test_run_wsgi(worker, loop):
     assert loop.is_closed()
 
 
-def test_handle_quit(worker):
+def test_handle_quit(worker, loop):
     with mock.patch('asyncio.ensure_future') as m_ensure_future:
         worker.loop = mock.Mock()
+        worker.close = mock.Mock()
         worker.handle_quit(object(), object())
         assert not worker.alive
         assert worker.exit_code == 0
@@ -185,7 +186,7 @@ def test__get_valid_log_format_exc(worker):
     assert '%(name)s' in str(exc)
 
 
-async def test__run_ok(worker, loop):
+async def test__run_ok_tcp(worker, loop):
     skip_if_no_dict(loop)
 
     worker.ppid = 1
