8b28ac9180ec02eeff9e49176f31896afc67dcc4
Christian Barra <barrachri@gmail.com>
2017-11-17 11:19:41 +0100

    WIP fix#2525

--- aiohttp/test_utils.py
+++ aiohttp/test_utils.py
@@ -15,7 +15,7 @@ from multidict import CIMultiDict
 from yarl import URL
 
 import aiohttp
-from aiohttp.client import _RequestContextManager
+from aiohttp.client import _RequestContextManager, _WSRequestContextManager
 
 from . import ClientSession, hdrs
 from .helpers import sentinel
@@ -278,12 +278,17 @@ class TestClient:
             self.request(hdrs.METH_DELETE, path, *args, **kwargs)
         )
 
-    async def ws_connect(self, path, *args, **kwargs):
+    def ws_connect(self, path, *args, **kwargs):
         """Initiate websocket connection.
 
         The api corresponds to aiohttp.ClientSession.ws_connect.
 
         """
+        return _WSRequestContextManager(
+            self._ws_connect(path, *args, **kwargs)
+        )
+
+    async def _ws_connect(self, path, *args, **kwargs):
         ws = await self._session.ws_connect(
             self.make_url(path), *args, **kwargs)
         self._websockets.append(ws)
