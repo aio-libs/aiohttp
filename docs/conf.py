11c420244f657ad4de4479e9776d9e48b3655120
Andrew Svetlov <andrew.svetlov@gmail.com>
2018-01-01 21:46:08 +0200

    Mention 2018 year

--- docs/conf.py
+++ docs/conf.py
@@ -93,7 +93,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'aiohttp'
-copyright = '2013-2017, Aiohttp contributors'
+copyright = '2013-2018, Aiohttp contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
