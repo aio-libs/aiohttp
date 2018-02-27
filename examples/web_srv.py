1e9b75afc7d927d6b2c5c66a3009be3e62324725
Alexander Shorin <kxepal@gmail.com>
2018-02-27 10:40:52 +0300

    Update examples for async write usage (#2770)
    This follows #2483

--- examples/web_srv.py
+++ examples/web_srv.py
@@ -17,7 +17,7 @@ async def intro(request):
     resp.content_length = len(binary)
     resp.content_type = 'text/plain'
     await resp.prepare(request)
-    resp.write(binary)
+    await resp.write(binary)
     return resp
 
 
@@ -39,7 +39,7 @@ async def hello(request):
     resp.content_length = len(answer)
     resp.content_type = 'text/plain'
     await resp.prepare(request)
-    resp.write(answer)
+    await resp.write(answer)
     await resp.write_eof()
     return resp
 
