bc5edc8a9efc4a460f30b306fa3fb6154717db51
Andrew Svetlov <andrew.svetlov@gmail.com>
2018-07-24 14:09:21 +0300

    Drop autodoc from Sphinx config (#3156)

--- docs/conf.py
+++ docs/conf.py
@@ -33,13 +33,6 @@ with codecs.open(_version_path, 'r', 'latin1') as fp:
         raise RuntimeError('Unable to determine version.')
 
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, os.path.abspath('.'))
-
-
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -49,7 +42,6 @@ sys.path.insert(0, os.path.abspath('.'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.intersphinx',
     'sphinxcontrib.asyncio',
