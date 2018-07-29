20f5496f709179f813b24b00be24babcf8f6d78b
Andrew Svetlov <andrew.svetlov@gmail.com>
2018-07-29 23:23:16 +0300

    Replace inline types with a stub for frozenlist

--- aiohttp/frozenlist.py
+++ aiohttp/frozenlist.py
@@ -1,32 +1,27 @@
+from collections.abc import MutableSequence
 from functools import total_ordering
-from typing import (Generic, Iterable, Iterator, List, MutableSequence,
-                    Optional, TypeVar, Union)
 
 from .helpers import NO_EXTENSIONS
 
 
-_T = TypeVar('_T')
-_Arg = Union[List[_T], Iterable[_T]]
-
-
 @total_ordering
-class FrozenList(MutableSequence[_T], Generic[_T]):
+class FrozenList(MutableSequence):
 
     __slots__ = ('_frozen', '_items')
 
-    def __init__(self, items: Optional[_Arg]=None) -> None:
-        self._frozen = False  # type: bool
+    def __init__(self, items=None):
+        self._frozen = False
         if items is not None:
             items = list(items)
         else:
             items = []
-        self._items = items  # type: List[_T]
+        self._items = items
 
     @property
-    def frozen(self) -> bool:
+    def frozen(self):
         return self._frozen
 
-    def freeze(self) -> None:
+    def freeze(self):
         self._frozen = True
 
     def __getitem__(self, index):
@@ -42,27 +37,27 @@ class FrozenList(MutableSequence[_T], Generic[_T]):
             raise RuntimeError("Cannot modify frozen list.")
         del self._items[index]
 
-    def __len__(self) -> int:
+    def __len__(self):
         return self._items.__len__()
 
-    def __iter__(self) -> Iterator[_T]:
+    def __iter__(self):
         return self._items.__iter__()
 
-    def __reversed__(self) -> Iterator[_T]:
+    def __reversed__(self):
         return self._items.__reversed__()
 
-    def __eq__(self, other) -> bool:
+    def __eq__(self, other):
         return list(self) == other
 
-    def __le__(self, other) -> bool:
+    def __le__(self, other):
         return list(self) <= other
 
-    def insert(self, pos: int, item: _T) -> None:
+    def insert(self, pos, item):
         if self._frozen:
             raise RuntimeError("Cannot modify frozen list.")
         self._items.insert(pos, item)
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return '<FrozenList(frozen={}, {!r})>'.format(self._frozen,
                                                       self._items)
 
