a3ee1f427ba074057ff21a864d6276b8c775b7cb
Andrew Svetlov <andrew.svetlov@gmail.com>
2018-10-26 14:21:27 +0200

    Annotate client_proto.py

--- aiohttp/client_proto.py
+++ aiohttp/client_proto.py
@@ -1,15 +1,17 @@
 import asyncio
 from contextlib import suppress
-from typing import Optional
+from typing import Any, Optional, Tuple
 
 from .base_protocol import BaseProtocol
 from .client_exceptions import (ClientOSError, ClientPayloadError,
                                 ServerDisconnectedError, ServerTimeoutError)
-from .http import HttpResponseParser
-from .streams import EMPTY_PAYLOAD, DataQueue
+from .helpers import BaseTimerContext
+from .http import HttpResponseParser, RawResponseMessage
+from .streams import EMPTY_PAYLOAD, DataQueue, StreamReader
 
 
-class ResponseHandler(BaseProtocol, DataQueue):
+class ResponseHandler(BaseProtocol,
+                      DataQueue[Tuple[RawResponseMessage, StreamReader]]):
     """Helper class to adapt between Protocol and StreamReader."""
 
     def __init__(self, *,
@@ -29,17 +31,17 @@ class ResponseHandler(BaseProtocol, DataQueue):
 
         self._tail = b''
         self._upgraded = False
-        self._parser = None
+        self._parser = None  # type: Optional[HttpResponseParser]
 
-        self._read_timeout = None
-        self._read_timeout_handle = None
+        self._read_timeout = None  # type: Optional[float]
+        self._read_timeout_handle = None  # type: Optional[asyncio.TimerHandle]
 
     @property
-    def upgraded(self):
+    def upgraded(self) -> bool:
         return self._upgraded
 
     @property
-    def should_close(self):
+    def should_close(self) -> bool:
         if (self._payload is not None and
                 not self._payload.is_eof() or self._upgraded):
             return True
@@ -47,37 +49,38 @@ class ResponseHandler(BaseProtocol, DataQueue):
         return (self._should_close or self._upgraded or
                 self.exception() is not None or
                 self._payload_parser is not None or
-                len(self) or self._tail)
+                len(self) > 0 or bool(self._tail))
 
-    def force_close(self):
+    def force_close(self) -> None:
         self._should_close = True
 
-    def close(self):
+    def close(self) -> None:
         transport = self.transport
         if transport is not None:
             transport.close()
             self.transport = None
             self._payload = None
             self._drop_timeout()
-        return transport
 
     def is_connected(self) -> bool:
         return self.transport is not None
 
-    def connection_lost(self, exc):
+    def connection_lost(self, exc: Optional[BaseException]) -> None:
         self._drop_timeout()
 
         if self._payload_parser is not None:
             with suppress(Exception):
                 self._payload_parser.feed_eof()
 
-        try:
-            uncompleted = self._parser.feed_eof()
-        except Exception:
-            uncompleted = None
-            if self._payload is not None:
-                self._payload.set_exception(
-                    ClientPayloadError('Response payload is not completed'))
+        if self._parser is not None:
+            try:
+                uncompleted = self._parser.feed_eof()
+            except Exception:
+                uncompleted = None
+                if self._payload is not None:
+                    self._payload.set_exception(
+                        ClientPayloadError(
+                            'Response payload is not completed'))
 
         if not self.is_eof():
             if isinstance(exc, OSError):
@@ -113,7 +116,12 @@ class ResponseHandler(BaseProtocol, DataQueue):
         self._drop_timeout()
         super().set_exception(exc)
 
-    def set_parser(self, parser, payload):
+    def set_parser(self, parser: Any, payload: Any) -> None:
+        # TODO: actual types are:
+        #   parser: WebSocketReader
+        #   payload: FlowControlDataQueue
+        # but they are not generi enough
+        # Need an ABC for both types
         self._payload = payload
         self._payload_parser = parser
 
@@ -123,11 +131,11 @@ class ResponseHandler(BaseProtocol, DataQueue):
             data, self._tail = self._tail, b''
             self.data_received(data)
 
-    def set_response_params(self, *, timer=None,
-                            skip_payload=False,
-                            read_until_eof=False,
-                            auto_decompress=True,
-                            read_timeout=None):
+    def set_response_params(self, *, timer: BaseTimerContext=None,
+                            skip_payload: bool=False,
+                            read_until_eof: bool=False,
+                            auto_decompress: bool=True,
+                            read_timeout: Optional[float]=None) -> None:
         self._skip_payload = skip_payload
 
         self._read_timeout = read_timeout
@@ -143,29 +151,29 @@ class ResponseHandler(BaseProtocol, DataQueue):
             data, self._tail = self._tail, b''
             self.data_received(data)
 
-    def _drop_timeout(self):
+    def _drop_timeout(self) -> None:
         if self._read_timeout_handle is not None:
             self._read_timeout_handle.cancel()
             self._read_timeout_handle = None
 
-    def _reschedule_timeout(self):
+    def _reschedule_timeout(self) -> None:
         timeout = self._read_timeout
         if self._read_timeout_handle is not None:
             self._read_timeout_handle.cancel()
 
         if timeout:
-            self._read_timeout_handle = self._loop.call_later(
+            self._read_timeout_handle = self._loop.call_later(  # type: ignore
                 timeout, self._on_read_timeout)
         else:
             self._read_timeout_handle = None
 
-    def _on_read_timeout(self):
+    def _on_read_timeout(self) -> None:
         exc = ServerTimeoutError("Timeout on reading data from socket")
         self.set_exception(exc)
         if self._payload is not None:
             self._payload.set_exception(exc)
 
-    def data_received(self, data):
+    def data_received(self, data: bytes) -> None:
         if not data:
             return
 
@@ -207,7 +215,7 @@ class ResponseHandler(BaseProtocol, DataQueue):
                     self._payload = payload
 
                     if self._skip_payload or message.code in (204, 304):
-                        self.feed_data((message, EMPTY_PAYLOAD), 0)
+                        self.feed_data((message, EMPTY_PAYLOAD), 0)  # type: ignore  # noqa
                     else:
                         self.feed_data((message, payload), 0)
                 if payload is not None:
