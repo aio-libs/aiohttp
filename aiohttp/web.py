6b1169d85265fdfa964962690c97d4dda6952523
Andrew Svetlov <andrew.svetlov@gmail.com>
2018-12-28 16:40:25 +0200

    Convert web.py

--- aiohttp/web.py
+++ aiohttp/web.py
@@ -7,129 +7,184 @@ from collections.abc import Iterable
 from importlib import import_module
 from typing import Any, Awaitable, Callable, List, Optional, Type, Union, cast
 
-from . import (web_app, web_exceptions, web_fileresponse, web_middlewares,
-               web_protocol, web_request, web_response, web_routedef,
-               web_runner, web_server, web_urldispatcher, web_ws)
 from .abc import AbstractAccessLogger
 from .helpers import all_tasks
 from .log import access_logger
-from .web_app import CleanupError  # noqa
-from .web_app import Application
-from .web_exceptions import HTTPAccepted  # noqa
-from .web_exceptions import HTTPBadGateway  # noqa
-from .web_exceptions import HTTPBadRequest  # noqa
-from .web_exceptions import HTTPClientError  # noqa
-from .web_exceptions import HTTPConflict  # noqa
-from .web_exceptions import HTTPCreated  # noqa
-from .web_exceptions import HTTPError  # noqa
-from .web_exceptions import HTTPException  # noqa
-from .web_exceptions import HTTPExpectationFailed  # noqa
-from .web_exceptions import HTTPFailedDependency  # noqa
-from .web_exceptions import HTTPForbidden  # noqa
-from .web_exceptions import HTTPFound  # noqa
-from .web_exceptions import HTTPGatewayTimeout  # noqa
-from .web_exceptions import HTTPGone  # noqa
-from .web_exceptions import HTTPInsufficientStorage  # noqa
-from .web_exceptions import HTTPInternalServerError  # noqa
-from .web_exceptions import HTTPLengthRequired  # noqa
-from .web_exceptions import HTTPMethodNotAllowed  # noqa
-from .web_exceptions import HTTPMisdirectedRequest  # noqa
-from .web_exceptions import HTTPMovedPermanently  # noqa
-from .web_exceptions import HTTPMultipleChoices  # noqa
-from .web_exceptions import HTTPNetworkAuthenticationRequired  # noqa
-from .web_exceptions import HTTPNoContent  # noqa
-from .web_exceptions import HTTPNonAuthoritativeInformation  # noqa
-from .web_exceptions import HTTPNotAcceptable  # noqa
-from .web_exceptions import HTTPNotExtended  # noqa
-from .web_exceptions import HTTPNotFound  # noqa
-from .web_exceptions import HTTPNotImplemented  # noqa
-from .web_exceptions import HTTPNotModified  # noqa
-from .web_exceptions import HTTPOk  # noqa
-from .web_exceptions import HTTPPartialContent  # noqa
-from .web_exceptions import HTTPPaymentRequired  # noqa
-from .web_exceptions import HTTPPermanentRedirect  # noqa
-from .web_exceptions import HTTPPreconditionFailed  # noqa
-from .web_exceptions import HTTPPreconditionRequired  # noqa
-from .web_exceptions import HTTPProxyAuthenticationRequired  # noqa
-from .web_exceptions import HTTPRedirection  # noqa
-from .web_exceptions import HTTPRequestEntityTooLarge  # noqa
-from .web_exceptions import HTTPRequestHeaderFieldsTooLarge  # noqa
-from .web_exceptions import HTTPRequestRangeNotSatisfiable  # noqa
-from .web_exceptions import HTTPRequestTimeout  # noqa
-from .web_exceptions import HTTPRequestURITooLong  # noqa
-from .web_exceptions import HTTPResetContent  # noqa
-from .web_exceptions import HTTPSeeOther  # noqa
-from .web_exceptions import HTTPServerError  # noqa
-from .web_exceptions import HTTPServiceUnavailable  # noqa
-from .web_exceptions import HTTPSuccessful  # noqa
-from .web_exceptions import HTTPTemporaryRedirect  # noqa
-from .web_exceptions import HTTPTooManyRequests  # noqa
-from .web_exceptions import HTTPUnauthorized  # noqa
-from .web_exceptions import HTTPUnavailableForLegalReasons  # noqa
-from .web_exceptions import HTTPUnprocessableEntity  # noqa
-from .web_exceptions import HTTPUnsupportedMediaType  # noqa
-from .web_exceptions import HTTPUpgradeRequired  # noqa
-from .web_exceptions import HTTPUseProxy  # noqa
-from .web_exceptions import HTTPVariantAlsoNegotiates  # noqa
-from .web_exceptions import HTTPVersionNotSupported  # noqa
-from .web_fileresponse import FileResponse  # noqa
+from .web_app import Application, CleanupError
+from .web_exceptions import (HTTPAccepted, HTTPBadGateway, HTTPBadRequest,
+                             HTTPClientError, HTTPConflict, HTTPCreated,
+                             HTTPError, HTTPException, HTTPExpectationFailed,
+                             HTTPFailedDependency, HTTPForbidden, HTTPFound,
+                             HTTPGatewayTimeout, HTTPGone,
+                             HTTPInsufficientStorage, HTTPInternalServerError,
+                             HTTPLengthRequired, HTTPMethodNotAllowed,
+                             HTTPMisdirectedRequest, HTTPMovedPermanently,
+                             HTTPMultipleChoices,
+                             HTTPNetworkAuthenticationRequired, HTTPNoContent,
+                             HTTPNonAuthoritativeInformation,
+                             HTTPNotAcceptable, HTTPNotExtended, HTTPNotFound,
+                             HTTPNotImplemented, HTTPNotModified, HTTPOk,
+                             HTTPPartialContent, HTTPPaymentRequired,
+                             HTTPPermanentRedirect, HTTPPreconditionFailed,
+                             HTTPPreconditionRequired,
+                             HTTPProxyAuthenticationRequired, HTTPRedirection,
+                             HTTPRequestEntityTooLarge,
+                             HTTPRequestHeaderFieldsTooLarge,
+                             HTTPRequestRangeNotSatisfiable,
+                             HTTPRequestTimeout, HTTPRequestURITooLong,
+                             HTTPResetContent, HTTPSeeOther, HTTPServerError,
+                             HTTPServiceUnavailable, HTTPSuccessful,
+                             HTTPTemporaryRedirect, HTTPTooManyRequests,
+                             HTTPUnauthorized, HTTPUnavailableForLegalReasons,
+                             HTTPUnprocessableEntity, HTTPUnsupportedMediaType,
+                             HTTPUpgradeRequired, HTTPUseProxy,
+                             HTTPVariantAlsoNegotiates,
+                             HTTPVersionNotSupported)
+from .web_fileresponse import FileResponse
 from .web_log import AccessLogger
-from .web_middlewares import middleware, normalize_path_middleware  # noqa
-from .web_protocol import PayloadAccessError  # noqa
-from .web_protocol import RequestHandler  # noqa
-from .web_protocol import RequestPayloadError  # noqa
-from .web_request import BaseRequest, FileField, Request  # noqa
-from .web_response import ContentCoding  # noqa
-from .web_response import Response  # noqa
-from .web_response import StreamResponse  # noqa
-from .web_response import json_response  # noqa
-from .web_routedef import AbstractRouteDef  # noqa
-from .web_routedef import RouteDef  # noqa
-from .web_routedef import RouteTableDef  # noqa
-from .web_routedef import StaticDef  # noqa
-from .web_routedef import delete  # noqa
-from .web_routedef import get  # noqa
-from .web_routedef import head  # noqa
-from .web_routedef import options  # noqa
-from .web_routedef import patch  # noqa
-from .web_routedef import post  # noqa
-from .web_routedef import put  # noqa
-from .web_routedef import route  # noqa
-from .web_routedef import static  # noqa
-from .web_routedef import view  # noqa
-from .web_runner import (AppRunner, BaseRunner, BaseSite, GracefulExit,  # noqa
+from .web_middlewares import middleware, normalize_path_middleware
+from .web_protocol import (PayloadAccessError, RequestHandler,
+                           RequestPayloadError)
+from .web_request import BaseRequest, FileField, Request
+from .web_response import (ContentCoding, Response, StreamResponse,
+                           json_response)
+from .web_routedef import (AbstractRouteDef, RouteDef, RouteTableDef,
+                           StaticDef, delete, get, head, options, patch, post,
+                           put, route, static, view)
+from .web_runner import (AppRunner, BaseRunner, BaseSite, GracefulExit,
                          ServerRunner, SockSite, TCPSite, UnixSite)
-from .web_server import Server  # noqa
-from .web_urldispatcher import AbstractResource  # noqa
-from .web_urldispatcher import AbstractRoute  # noqa
-from .web_urldispatcher import DynamicResource  # noqa
-from .web_urldispatcher import PlainResource  # noqa
-from .web_urldispatcher import Resource  # noqa
-from .web_urldispatcher import ResourceRoute  # noqa
-from .web_urldispatcher import StaticResource  # noqa
-from .web_urldispatcher import UrlDispatcher  # noqa
-from .web_urldispatcher import UrlMappingMatchInfo  # noqa
-from .web_urldispatcher import View  # noqa
-from .web_ws import WebSocketReady  # noqa
-from .web_ws import WebSocketResponse  # noqa
-from .web_ws import WSMsgType  # noqa
+from .web_server import Server
+from .web_urldispatcher import (AbstractResource, AbstractRoute,
+                                DynamicResource, PlainResource, Resource,
+                                ResourceRoute, StaticResource, UrlDispatcher,
+                                UrlMappingMatchInfo, View)
+from .web_ws import WebSocketReady, WebSocketResponse, WSMsgType
 
 
-__all__ = (web_protocol.__all__ +
-           web_app.__all__ +
-           web_fileresponse.__all__ +
-           web_request.__all__ +
-           web_response.__all__ +
-           web_routedef.__all__ +
-           web_exceptions.__all__ +
-           web_urldispatcher.__all__ +
-           web_ws.__all__ +
-           web_server.__all__ +
-           web_runner.__all__ +
-           web_middlewares.__all__ +
-           ('run_app', 'BaseSite', 'TCPSite', 'UnixSite',
-            'SockSite', 'BaseRunner',
-            'AppRunner', 'ServerRunner', 'GracefulExit'))
+__all__ = (
+    # web_app
+    'Application',
+    'CleanupError',
+    # web_exceptions
+    'HTTPAccepted',
+    'HTTPBadGateway',
+    'HTTPBadRequest',
+    'HTTPClientError',
+    'HTTPConflict',
+    'HTTPCreated',
+    'HTTPError',
+    'HTTPException',
+    'HTTPExpectationFailed',
+    'HTTPFailedDependency',
+    'HTTPForbidden',
+    'HTTPFound',
+    'HTTPGatewayTimeout',
+    'HTTPGone',
+    'HTTPInsufficientStorage',
+    'HTTPInternalServerError',
+    'HTTPLengthRequired',
+    'HTTPMethodNotAllowed',
+    'HTTPMisdirectedRequest',
+    'HTTPMovedPermanently',
+    'HTTPMultipleChoices',
+    'HTTPNetworkAuthenticationRequired',
+    'HTTPNoContent',
+    'HTTPNonAuthoritativeInformation',
+    'HTTPNotAcceptable',
+    'HTTPNotExtended',
+    'HTTPNotFound',
+    'HTTPNotImplemented',
+    'HTTPNotModified',
+    'HTTPOk',
+    'HTTPPartialContent',
+    'HTTPPaymentRequired',
+    'HTTPPermanentRedirect',
+    'HTTPPreconditionFailed',
+    'HTTPPreconditionRequired',
+    'HTTPProxyAuthenticationRequired',
+    'HTTPRedirection',
+    'HTTPRequestEntityTooLarge',
+    'HTTPRequestHeaderFieldsTooLarge',
+    'HTTPRequestRangeNotSatisfiable',
+    'HTTPRequestTimeout',
+    'HTTPRequestURITooLong',
+    'HTTPResetContent',
+    'HTTPSeeOther',
+    'HTTPServerError',
+    'HTTPServiceUnavailable',
+    'HTTPSuccessful',
+    'HTTPTemporaryRedirect',
+    'HTTPTooManyRequests',
+    'HTTPUnauthorized',
+    'HTTPUnavailableForLegalReasons',
+    'HTTPUnprocessableEntity',
+    'HTTPUnsupportedMediaType',
+    'HTTPUpgradeRequired',
+    'HTTPUseProxy',
+    'HTTPVariantAlsoNegotiates',
+    'HTTPVersionNotSupported',
+    # web_fileresponse
+    'FileResponse',
+    # web_middlewares
+    'middleware',
+    'normalize_path_middleware',
+    # web_protocol
+    'PayloadAccessError',
+    'RequestHandler',
+    'RequestPayloadError',
+    # web_request
+    'BaseRequest',
+    'FileField',
+    'Request',
+    # web_response
+    'ContentCoding',
+    'Response',
+    'StreamResponse',
+    'json_response',
+    # web_routedef
+    'AbstractRouteDef',
+    'RouteDef',
+    'RouteTableDef',
+    'StaticDef',
+    'delete',
+    'get',
+    'head',
+    'options',
+    'patch',
+    'post',
+    'put',
+    'route',
+    'static',
+    'view',
+    # web_runner
+    'AppRunner',
+    'BaseRunner',
+    'BaseSite',
+    'GracefulExit',
+    'ServerRunner',
+    'SockSite',
+    'TCPSite',
+    'UnixSite',
+    # web_server
+    'Server',
+    # web_urldispatcher
+    'AbstractResource',
+    'AbstractRoute',
+    'DynamicResource',
+    'PlainResource',
+    'Resource',
+    'ResourceRoute',
+    'StaticResource',
+    'UrlDispatcher',
+    'UrlMappingMatchInfo',
+    'View',
+    # web_ws
+    'WebSocketReady',
+    'WebSocketResponse',
+    'WSMsgType',
+    # web
+    'run_app',
+)
 
 
 try:
