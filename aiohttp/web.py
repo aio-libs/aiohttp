aio-libs/aiohttp

commit dd30b2af1fd0d643e02ef5023f1d8a68a75b5301

Contextvars support (#3446)

aiohttp/web.py
@@ -11,6 +11,7 @@ from . import (web_app, web_exceptions, web_fileresponse, web_middlewares,
                web_protocol, web_request, web_response, web_routedef,
                web_runner, web_server, web_urldispatcher, web_ws)
 from .abc import AbstractAccessLogger
+from .helpers import all_tasks
 from .log import access_logger
 from .web_app import *  # noqa
 from .web_app import Application
@@ -52,42 +53,33 @@ except ImportError:  # pragma: no cover
     SSLContext = Any  # type: ignore
 
 
-def run_app(app: Union[Application, Awaitable[Application]], *,
-            host: Optional[str]=None,
-            port: Optional[int]=None,
-            path: Optional[str]=None,
-            sock: Optional[socket.socket]=None,
-            shutdown_timeout: float=60.0,
-            ssl_context: Optional[SSLContext]=None,
-            print: Callable[..., None]=print,
-            backlog: int=128,
-            access_log_class: Type[AbstractAccessLogger]=AccessLogger,
-            access_log_format: str=AccessLogger.LOG_FORMAT,
-            access_log: logging.Logger=access_logger,
-            handle_signals: bool=True,
-            reuse_address: Optional[bool]=None,
-            reuse_port: Optional[bool]=None) -> None:
-    """Run an app locally"""
-    loop = asyncio.get_event_loop()
-
+async def _run_app(app: Union[Application, Awaitable[Application]], *,
+                   host: Optional[str]=None,
+                   port: Optional[int]=None,
+                   path: Optional[str]=None,
+                   sock: Optional[socket.socket]=None,
+                   shutdown_timeout: float=60.0,
+                   ssl_context: Optional[SSLContext]=None,
+                   print: Callable[..., None]=print,
+                   backlog: int=128,
+                   access_log_class: Type[AbstractAccessLogger]=AccessLogger,
+                   access_log_format: str=AccessLogger.LOG_FORMAT,
+                   access_log: logging.Logger=access_logger,
+                   handle_signals: bool=True,
+                   reuse_address: Optional[bool]=None,
+                   reuse_port: Optional[bool]=None) -> None:
+    # A internal functio to actually do all dirty job for application running
     if asyncio.iscoroutine(app):
-        app = loop.run_until_complete(app)  # type: ignore
+        app = await app  # type: ignore
 
     app = cast(Application, app)
-
-    # Configure if and only if in debugging mode and using the default logger
-    if loop.get_debug() and access_log.name == 'aiohttp.access':
-        if access_log.level == logging.NOTSET:
-            access_log.setLevel(logging.DEBUG)
-        if not access_log.hasHandlers():
-            access_log.addHandler(logging.StreamHandler())
 
     runner = AppRunner(app, handle_signals=handle_signals,
                        access_log_class=access_log_class,
                        access_log_format=access_log_format,
                        access_log=access_log)
 
-    loop.run_until_complete(runner.setup())
+    await runner.setup()
 
     sites = []  # type: List[BaseSite]
 
@@ -141,21 +133,88 @@ def run_app(app: Union[Application, Awaitable[Application]], *,
                                           ssl_context=ssl_context,
                                           backlog=backlog))
         for site in sites:
-            loop.run_until_complete(site.start())
-        try:
-            if print:  # pragma: no branch
-                names = sorted(str(s.name) for s in runner.sites)
-                print("======== Running on {} ========\n"
-                      "(Press CTRL+C to quit)".format(', '.join(names)))
-            loop.run_forever()
-        except (GracefulExit, KeyboardInterrupt):  # pragma: no cover
-            pass
+            await site.start()
+
+        if print:  # pragma: no branch
+            names = sorted(str(s.name) for s in runner.sites)
+            print("======== Running on {} ========\n"
+                  "(Press CTRL+C to quit)".format(', '.join(names)))
+        while True:
+            await asyncio.sleep(3600)  # sleep forever by 1 hour intervals
     finally:
-        loop.run_until_complete(runner.cleanup())
-    if sys.version_info >= (3, 6):  # don't use PY_36 to pass mypy
-        if hasattr(loop, 'shutdown_asyncgens'):
+        await runner.cleanup()
+
+
+def _cancel_all_tasks(loop: asyncio.AbstractEventLoop) -> None:
+    to_cancel = all_tasks(loop)
+    if not to_cancel:
+        return
+
+    for task in to_cancel:
+        task.cancel()
+
+    loop.run_until_complete(
+        asyncio.gather(*to_cancel, loop=loop, return_exceptions=True))
+
+    for task in to_cancel:
+        if task.cancelled():
+            continue
+        if task.exception() is not None:
+            loop.call_exception_handler({
+                'message': 'unhandled exception during asyncio.run() shutdown',
+                'exception': task.exception(),
+                'task': task,
+            })
+
+
+def run_app(app: Union[Application, Awaitable[Application]], *,
+            host: Optional[str]=None,
+            port: Optional[int]=None,
+            path: Optional[str]=None,
+            sock: Optional[socket.socket]=None,
+            shutdown_timeout: float=60.0,
+            ssl_context: Optional[SSLContext]=None,
+            print: Callable[..., None]=print,
+            backlog: int=128,
+            access_log_class: Type[AbstractAccessLogger]=AccessLogger,
+            access_log_format: str=AccessLogger.LOG_FORMAT,
+            access_log: logging.Logger=access_logger,
+            handle_signals: bool=True,
+            reuse_address: Optional[bool]=None,
+            reuse_port: Optional[bool]=None) -> None:
+    """Run an app locally"""
+    loop = asyncio.get_event_loop()
+
+    # Configure if and only if in debugging mode and using the default logger
+    if loop.get_debug() and access_log.name == 'aiohttp.access':
+        if access_log.level == logging.NOTSET:
+            access_log.setLevel(logging.DEBUG)
+        if not access_log.hasHandlers():
+            access_log.addHandler(logging.StreamHandler())
+
+    try:
+        loop.run_until_complete(_run_app(app,
+                                         host=host,
+                                         port=port,
+                                         path=path,
+                                         sock=sock,
+                                         shutdown_timeout=shutdown_timeout,
+                                         ssl_context=ssl_context,
+                                         print=print,
+                                         backlog=backlog,
+                                         access_log_class=access_log_class,
+                                         access_log_format=access_log_format,
+                                         access_log=access_log,
+                                         handle_signals=handle_signals,
+                                         reuse_address=reuse_address,
+                                         reuse_port=reuse_port))
+    except (GracefulExit, KeyboardInterrupt):  # pragma: no cover
+        pass
+    finally:
+        _cancel_all_tasks(loop)
+        if sys.version_info >= (3, 6):  # don't use PY_36 to pass mypy
             loop.run_until_complete(loop.shutdown_asyncgens())
-    loop.close()
+        loop.close()
 
 
 def main(argv: List[str]) -> None:

aiohttp/helpers.py
@@ -20,8 +20,8 @@ from math import ceil
 from pathlib import Path
 from types import TracebackType
 from typing import (Any, Callable, Dict, Iterable, Iterator, List,  # noqa
-                    Mapping, Optional, Pattern, Tuple, Type, TypeVar, Union,
-                    cast)
+                    Mapping, Optional, Pattern, Set, Tuple, Type, TypeVar,
+                    Union, cast)
 from urllib.parse import quote
 from urllib.request import getproxies
 
@@ -48,6 +48,12 @@ try:
     from typing import ContextManager
 except ImportError:
     from typing_extensions import ContextManager
+
+
+all_tasks = asyncio.Task.all_tasks
+
+if PY_37:
+    all_tasks = getattr(asyncio, 'all_tasks')  # use the trick to cheat mypy
 
 
 _T = TypeVar('_T')

tests/test_run_app.py
@@ -14,6 +14,7 @@ from uuid import uuid4
 import pytest
 
 from aiohttp import web
+from aiohttp.helpers import PY_37
 from aiohttp.test_utils import make_mocked_coro
 
 
@@ -621,3 +622,103 @@ def test_run_app_default_logger_setup_only_if_unconfigured(patched_loop):
     mock_logger.setLevel.assert_not_called()
     mock_logger.hasHandlers.assert_called_with()
     mock_logger.addHandler.assert_not_called()
+
+
+def test_run_app_cancels_all_pending_tasks(patched_loop):
+    app = web.Application()
+    task = None
+
+    async def on_startup(app):
+        nonlocal task
+        loop = asyncio.get_event_loop()
+        task = loop.create_task(asyncio.sleep(1000))
+
+    app.on_startup.append(on_startup)
+
+    web.run_app(app, print=stopper(patched_loop))
+    assert task.cancelled()
+
+
+def test_run_app_cancels_done_tasks(patched_loop):
+    app = web.Application()
+    task = None
+
+    async def coro():
+        return 123
+
+    async def on_startup(app):
+        nonlocal task
+        loop = asyncio.get_event_loop()
+        task = loop.create_task(coro())
+
+    app.on_startup.append(on_startup)
+
+    web.run_app(app, print=stopper(patched_loop))
+    assert task.done()
+
+
+def test_run_app_cancels_failed_tasks(patched_loop):
+    app = web.Application()
+    task = None
+
+    exc = RuntimeError("FAIL")
+
+    async def fail():
+        try:
+            await asyncio.sleep(1000)
+        except asyncio.CancelledError:
+            raise exc
+
+    async def on_startup(app):
+        nonlocal task
+        loop = asyncio.get_event_loop()
+        task = loop.create_task(fail())
+        await asyncio.sleep(0.01)
+
+    app.on_startup.append(on_startup)
+
+    exc_handler = mock.Mock()
+    patched_loop.set_exception_handler(exc_handler)
+    web.run_app(app, print=stopper(patched_loop))
+    assert task.done()
+
+    msg = {
+        'message': 'unhandled exception during asyncio.run() shutdown',
+        'exception': exc,
+        'task': task,
+    }
+    exc_handler.assert_called_with(patched_loop, msg)
+
+
+@pytest.mark.skipif(not PY_37,
+                    reason="contextvars support is required")
+def test_run_app_context_vars(patched_loop):
+    from contextvars import ContextVar
+
+    count = 0
+    VAR = ContextVar('VAR', default='default')
+
+    async def on_startup(app):
+        nonlocal count
+        assert 'init' == VAR.get()
+        VAR.set('on_startup')
+        count += 1
+
+    async def on_cleanup(app):
+        nonlocal count
+        assert 'on_startup' == VAR.get()
+        count += 1
+
+    async def init():
+        nonlocal count
+        assert 'default' == VAR.get()
+        VAR.set('init')
+        app = web.Application()
+
+        app.on_startup.append(on_startup)
+        app.on_cleanup.append(on_cleanup)
+        count += 1
+        return app
+
+    web.run_app(init(), print=stopper(patched_loop))
+    assert count == 3

commit 8d42315bed44416effd036de5f22e0b432d032ac

Fix dropped connections on graceful shutdown
This fixes a bug introduced in https://github.com/aio-libs/aiohttp/commit/dd30b2af1fd0d643e02ef5023f1d8a68a75b5301#diff-8e4ccf3aaf43100064462a4df10952e5R214

aiohttp/web.py
@@ -105,7 +105,7 @@ from .web_runner import (
     AppRunner,
     BaseRunner,
     BaseSite,
-    GracefulExit,
+    ForceExit,
     ServerRunner,
     SockSite,
     TCPSite,
@@ -225,7 +225,7 @@ __all__ = (
     'AppRunner',
     'BaseRunner',
     'BaseSite',
-    'GracefulExit',
+    'ForceExit',
     'ServerRunner',
     'SockSite',
     'TCPSite',
@@ -344,8 +344,8 @@ async def _run_app(app: Union[Application, Awaitable[Application]], *,
             names = sorted(str(s.name) for s in runner.sites)
             print("======== Running on {} ========\n"
                   "(Press CTRL+C to quit)".format(', '.join(names)))
-        while True:
-            await asyncio.sleep(3600)  # sleep forever by 1 hour intervals
+
+        await runner.wait_for_close()
     finally:
         await runner.cleanup()
 
@@ -413,7 +413,7 @@ def run_app(app: Union[Application, Awaitable[Application]], *,
                                          handle_signals=handle_signals,
                                          reuse_address=reuse_address,
                                          reuse_port=reuse_port))
-    except (GracefulExit, KeyboardInterrupt):  # pragma: no cover
+    except (ForceExit, KeyboardInterrupt):  # pragma: no cover
         pass
     finally:
         _cancel_all_tasks(loop)

aiohttp/web_runner.py
@@ -16,15 +16,15 @@ except ImportError:
 
 
 __all__ = ('BaseSite', 'TCPSite', 'UnixSite', 'SockSite', 'BaseRunner',
-           'AppRunner', 'ServerRunner', 'GracefulExit')
+           'AppRunner', 'ServerRunner', 'ForceExit')
 
 
-class GracefulExit(SystemExit):
+class ForceExit(SystemExit):
     code = 1
 
 
-def _raise_graceful_exit() -> None:
-    raise GracefulExit()
+def _force_exit() -> None:
+    raise ForceExit()
 
 
 class BaseSite(ABC):
@@ -161,13 +161,14 @@ class SockSite(BaseSite):
 
 
 class BaseRunner(ABC):
-    __slots__ = ('_handle_signals', '_kwargs', '_server', '_sites')
+    __slots__ = ('_handle_signals', '_kwargs', '_server', '_sites', '_close_event')
 
     def __init__(self, *, handle_signals: bool=False, **kwargs: Any) -> None:
         self._handle_signals = handle_signals
         self._kwargs = kwargs
         self._server = None  # type: Optional[Server]
         self._sites = []  # type: List[BaseSite]
+        self._close_event = asyncio.Event()
 
     @property
     def server(self) -> Optional[Server]:
@@ -194,8 +195,8 @@ class BaseRunner(ABC):
 
         if self._handle_signals:
             try:
-                loop.add_signal_handler(signal.SIGINT, _raise_graceful_exit)
-                loop.add_signal_handler(signal.SIGTERM, _raise_graceful_exit)
+                loop.add_signal_handler(signal.SIGINT, self.close)
+                loop.add_signal_handler(signal.SIGTERM, self.close)
             except NotImplementedError:  # pragma: no cover
                 # add_signal_handler is not implemented on Windows
                 pass
@@ -205,6 +206,9 @@ class BaseRunner(ABC):
     @abstractmethod
     async def shutdown(self) -> None:
         pass  # pragma: no cover
+
+    async def wait_for_close(self) -> None:
+        await self._close_event.wait()
 
     async def cleanup(self) -> None:
         loop = asyncio.get_event_loop()
@@ -228,6 +232,11 @@ class BaseRunner(ABC):
             except NotImplementedError:  # pragma: no cover
                 # remove_signal_handler is not implemented on Windows
                 pass
+
+    def close(self) -> None:
+        if self._close_event.is_set():
+            _force_exit()
+        self._close_event.set()
 
     @abstractmethod
     async def _make_server(self) -> Server: