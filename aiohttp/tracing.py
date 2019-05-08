aio-libs/aiohttp

commit dc438ca2b7cbb62cea100396c103bba3ac42ab4e

Fix type hint for tracing Signal sequence content (#3596)
* Fix type hint for tracing Signal sequence content

The Signal is instantiated with a TraceConfig object but does not store
it in its mutable sequence. Fixed hinting to reflect what is expected to
be added as signal handler.

aiohttp/tracing.py
@@ -1,5 +1,5 @@
 from types import SimpleNamespace
-from typing import TYPE_CHECKING, Awaitable, Callable, Type
+from typing import TYPE_CHECKING, Awaitable, Callable, Type, Union
 
 import attr
 from multidict import CIMultiDict  # noqa
@@ -11,7 +11,24 @@ from .signals import Signal
 if TYPE_CHECKING:  # pragma: no cover
     from .client import ClientSession  # noqa
 
-    _Signal = Signal[Callable[['TraceConfig'], Awaitable[None]]]
+    _SignalArgs = Union[
+        [ClientSession, SimpleNamespace, 'TraceRequestStartParams'],
+        [ClientSession, SimpleNamespace, 'TraceRequestEndParams'],
+        [ClientSession, SimpleNamespace, 'TraceRequestExceptionParams'],
+        [ClientSession, SimpleNamespace, 'TraceConnectionQueuedStartParams'],
+        [ClientSession, SimpleNamespace, 'TraceConnectionQueuedEndParams'],
+        [ClientSession, SimpleNamespace, 'TraceConnectionCreateStartParams'],
+        [ClientSession, SimpleNamespace, 'TraceConnectionCreateEndParams'],
+        [ClientSession, SimpleNamespace, 'TraceConnectionReuseconnParams'],
+        [ClientSession, SimpleNamespace, 'TraceDnsResolveHostStartParams'],
+        [ClientSession, SimpleNamespace, 'TraceDnsResolveHostEndParams'],
+        [ClientSession, SimpleNamespace, 'TraceDnsCacheHitParams'],
+        [ClientSession, SimpleNamespace, 'TraceDnsCacheMissParams'],
+        [ClientSession, SimpleNamespace, 'TraceRequestRedirectParams'],
+        [ClientSession, SimpleNamespace, 'TraceRequestChunkSentParams'],
+        [ClientSession, SimpleNamespace, 'TraceResponseChunkReceivedParams'],
+    ]
+    _Signal = Signal[Callable[_SignalArgs, Awaitable[None]]]
 else:
     _Signal = Signal
 

commit a3731a3e034ac1384038919d3e31f71e81639072

Post-fix for error introduced by dc438ca2 (#3596)

aiohttp/tracing.py
@@ -12,23 +12,24 @@ if TYPE_CHECKING:  # pragma: no cover
     from .client import ClientSession  # noqa
 
     _SignalArgs = Union[
-        [ClientSession, SimpleNamespace, 'TraceRequestStartParams'],
-        [ClientSession, SimpleNamespace, 'TraceRequestEndParams'],
-        [ClientSession, SimpleNamespace, 'TraceRequestExceptionParams'],
-        [ClientSession, SimpleNamespace, 'TraceConnectionQueuedStartParams'],
-        [ClientSession, SimpleNamespace, 'TraceConnectionQueuedEndParams'],
-        [ClientSession, SimpleNamespace, 'TraceConnectionCreateStartParams'],
-        [ClientSession, SimpleNamespace, 'TraceConnectionCreateEndParams'],
-        [ClientSession, SimpleNamespace, 'TraceConnectionReuseconnParams'],
-        [ClientSession, SimpleNamespace, 'TraceDnsResolveHostStartParams'],
-        [ClientSession, SimpleNamespace, 'TraceDnsResolveHostEndParams'],
-        [ClientSession, SimpleNamespace, 'TraceDnsCacheHitParams'],
-        [ClientSession, SimpleNamespace, 'TraceDnsCacheMissParams'],
-        [ClientSession, SimpleNamespace, 'TraceRequestRedirectParams'],
-        [ClientSession, SimpleNamespace, 'TraceRequestChunkSentParams'],
-        [ClientSession, SimpleNamespace, 'TraceResponseChunkReceivedParams'],
+        'TraceRequestStartParams',
+        'TraceRequestEndParams',
+        'TraceRequestExceptionParams',
+        'TraceConnectionQueuedStartParams',
+        'TraceConnectionQueuedEndParams',
+        'TraceConnectionCreateStartParams',
+        'TraceConnectionCreateEndParams',
+        'TraceConnectionReuseconnParams',
+        'TraceDnsResolveHostStartParams',
+        'TraceDnsResolveHostEndParams',
+        'TraceDnsCacheHitParams',
+        'TraceDnsCacheMissParams',
+        'TraceRequestRedirectParams',
+        'TraceRequestChunkSentParams',
+        'TraceResponseChunkReceivedParams',
     ]
-    _Signal = Signal[Callable[_SignalArgs, Awaitable[None]]]
+    _Signal = Signal[Callable[[ClientSession, SimpleNamespace, _SignalArgs],
+                              Awaitable[None]]]
 else:
     _Signal = Signal
 