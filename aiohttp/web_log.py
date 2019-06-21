59be3d87595523b886b895649cf80ec23ff562e5
NewUserHa <32261870+NewUserHa@users.noreply.github.com>
2019-06-21 22:02:08 +0800

    Update web_log.py

--- aiohttp/web_log.py
+++ aiohttp/web_log.py
@@ -150,7 +150,7 @@ class AccessLogger(AbstractAccessLogger):
     def _format_t(request: BaseRequest,
                   response: StreamResponse,
                   time: float) -> str:
-        tz = datetime.timezone(datetime.timedelta(seconds=-_time.timezone))
+        tz = datetime.timezone(datetime.timedelta(seconds=-timezone))
         now = datetime.datetime.now(tz)
         start_time = now - datetime.timedelta(seconds=time)
         return start_time.strftime('[%d/%b/%Y:%H:%M:%S %z]')
