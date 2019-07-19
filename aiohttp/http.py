1ccb7d75f85cfee71fa7713b1918cf8d385bc6c3
Dmitry Marakasov <amdmi3@amdmi3.ru>
2019-07-19 12:12:03 +0300

    Use correct style for re-exported imports (for 3.5 branch) (#3873)

--- aiohttp/http.py
+++ aiohttp/http.py
@@ -3,34 +3,28 @@ import sys
 from typing import Mapping, Tuple  # noqa
 
 from . import __version__
-from .http_exceptions import HttpProcessingError
-from .http_parser import (
-    HeadersParser,
-    HttpParser,
-    HttpRequestParser,
-    HttpResponseParser,
-    RawRequestMessage,
-    RawResponseMessage,
-)
-from .http_websocket import (
-    WS_CLOSED_MESSAGE,
-    WS_CLOSING_MESSAGE,
-    WS_KEY,
-    WebSocketError,
-    WebSocketReader,
-    WebSocketWriter,
-    WSCloseCode,
-    WSMessage,
-    WSMsgType,
-    ws_ext_gen,
-    ws_ext_parse,
-)
-from .http_writer import (
-    HttpVersion,
-    HttpVersion10,
-    HttpVersion11,
-    StreamWriter,
-)
+from .http_exceptions import HttpProcessingError as HttpProcessingError
+from .http_parser import HeadersParser as HeadersParser
+from .http_parser import HttpParser as HttpParser
+from .http_parser import HttpRequestParser as HttpRequestParser
+from .http_parser import HttpResponseParser as HttpResponseParser
+from .http_parser import RawRequestMessage as RawRequestMessage
+from .http_parser import RawResponseMessage as RawResponseMessage
+from .http_websocket import WS_CLOSED_MESSAGE as WS_CLOSED_MESSAGE
+from .http_websocket import WS_CLOSING_MESSAGE as WS_CLOSING_MESSAGE
+from .http_websocket import WS_KEY as WS_KEY
+from .http_websocket import WebSocketError as WebSocketError
+from .http_websocket import WebSocketReader as WebSocketReader
+from .http_websocket import WebSocketWriter as WebSocketWriter
+from .http_websocket import WSCloseCode as WSCloseCode
+from .http_websocket import WSMessage as WSMessage
+from .http_websocket import WSMsgType as WSMsgType
+from .http_websocket import ws_ext_gen as ws_ext_gen
+from .http_websocket import ws_ext_parse as ws_ext_parse
+from .http_writer import HttpVersion as HttpVersion
+from .http_writer import HttpVersion10 as HttpVersion10
+from .http_writer import HttpVersion11 as HttpVersion11
+from .http_writer import StreamWriter as StreamWriter
 
 __all__ = (
     'HttpProcessingError', 'RESPONSES', 'SERVER_SOFTWARE',
