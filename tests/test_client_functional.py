eed4d7883da781f402dbea92e00a80a2d4bfc2fa
Andrew Svetlov <andrew.svetlov@gmail.com>
2019-10-11 16:59:54 +0300

    Fix file mode

--- tests/test_client_functional.py
+++ tests/test_client_functional.py
@@ -1270,7 +1270,7 @@ async def test_POST_FILES_DEFLATE(aiohttp_client, fname) -> None:
     app.router.add_post('/', handler)
     client = await aiohttp_client(app)
 
-    with fname.open('r') as f:
+    with fname.open('rb') as f:
         resp = await client.post(
             '/',
             data={'some': f},
