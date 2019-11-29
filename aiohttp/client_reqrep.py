5654c69a872d38dcc9290fb3163551250ff9d7dc
Andrew Svetlov <andrew.svetlov@gmail.com>
2019-11-29 12:59:23 +0200

    Use .raw_host instead of slower .host in client API

--- aiohttp/client_reqrep.py
+++ aiohttp/client_reqrep.py
@@ -266,7 +266,7 @@ class ClientRequest:
 
     @property
     def host(self) -> str:
-        ret = self.url.host
+        ret = self.url.raw_host
         assert ret is not None
         return ret
 
@@ -283,7 +283,7 @@ class ClientRequest:
     def update_host(self, url: URL) -> None:
         """Update destination host, port and connection type (ssl)."""
         # get host/port
-        if not url.host:
+        if not url.raw_host:
             raise InvalidURL(url)
 
         # basic auth info
