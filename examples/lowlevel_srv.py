309ce67404e1a030711b771e8f0237c716002898
Anas <anas.el.amraoui@live.com>
2020-05-17 11:42:57 +0300

    Typed examples/lowlevel_srv.py

--- examples/lowlevel_srv.py
+++ examples/lowlevel_srv.py
@@ -1,13 +1,15 @@
 import asyncio
+from asyncio import AbstractEventLoop
 
 from aiohttp import web
+from aiohttp.web_request import BaseRequest
 
 
-async def handler(request):
+async def handler(request: BaseRequest) -> web.StreamResponse:
     return web.Response(text="OK")
 
 
-async def main(loop):
+async def main(loop: AbstractEventLoop) -> None:
     server = web.Server(handler)
     await loop.create_server(server, "127.0.0.1", 8080)
     print("======= Serving on http://127.0.0.1:8080/ ======")
