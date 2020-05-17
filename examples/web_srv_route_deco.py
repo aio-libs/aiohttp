4855888c675ed0b337b6538ef5adcd1689725379
Anas <anas.el.amraoui@live.com>
2020-05-17 12:10:19 +0300

    Typed examples/web_srv_router_deco.py

--- examples/web_srv_route_deco.py
+++ examples/web_srv_route_deco.py
@@ -6,12 +6,13 @@ with decorator definition for routes
 import textwrap
 
 from aiohttp import web
+from aiohttp.web_request import Request
 
 routes = web.RouteTableDef()
 
 
 @routes.get("/")
-async def intro(request):
+async def intro(request: Request) -> web.StreamResponse:
     txt = textwrap.dedent(
         """\
         Type {url}/hello/John  {url}/simple or {url}/change_body
@@ -28,12 +29,12 @@ async def intro(request):
 
 
 @routes.get("/simple")
-async def simple(request):
+async def simple(request: Request) -> web.StreamResponse:
     return web.Response(text="Simple answer")
 
 
 @routes.get("/change_body")
-async def change_body(request):
+async def change_body(request: Request) -> web.StreamResponse:
     resp = web.Response()
     resp.body = b"Body changed"
     resp.content_type = "text/plain"
@@ -41,7 +42,7 @@ async def change_body(request):
 
 
 @routes.get("/hello")
-async def hello(request):
+async def hello(request: Request) -> web.StreamResponse:
     resp = web.StreamResponse()
     name = request.match_info.get("name", "Anonymous")
     answer = ("Hello, " + name).encode("utf8")
@@ -53,7 +54,7 @@ async def hello(request):
     return resp
 
 
-def init():
+def init() -> web.Application:
     app = web.Application()
     app.router.add_routes(routes)
     return app
