aio-libs/aiohttp

commit 964921d4e97e7c84bcfda6772ed458549aea0b09

refactor connector keep-live timer

aiohttp/connector.py
@@ -7,7 +7,6 @@ import warnings
 from collections import defaultdict
 from hashlib import md5, sha1, sha256
 from itertools import chain
-from math import ceil
 from types import MappingProxyType
 
 from yarl import URL
@@ -112,8 +111,7 @@ class BaseConnector(object):
     _source_traceback = None
 
     def __init__(self, *, conn_timeout=None, keepalive_timeout=sentinel,
-                 force_close=False, limit=20,
-                 loop=None):
+                 force_close=False, limit=20, time_service=None, loop=None):
 
         if force_close:
             if keepalive_timeout is not None and \
@@ -122,7 +120,7 @@ class BaseConnector(object):
                                  'be set if force_close is True')
         else:
             if keepalive_timeout is sentinel:
-                keepalive_timeout = 30
+                keepalive_timeout = 15.0
 
         if loop is None:
             loop = asyncio.get_event_loop()
@@ -135,10 +133,16 @@ class BaseConnector(object):
         self._acquired = defaultdict(set)
         self._conn_timeout = conn_timeout
         self._keepalive_timeout = keepalive_timeout
-        self._cleanup_handle = None
         self._force_close = force_close
         self._limit = limit
         self._waiters = defaultdict(list)
+
+        if time_service is not None:
+            self._time_service_owner = False
+            self._time_service = time_service
+        else:
+            self._time_service_owner = True
+            self._time_service = helpers.TimeService(loop)
 
         self._loop = loop
         self._factory = functools.partial(
@@ -146,6 +150,11 @@ class BaseConnector(object):
             disconnect_error=ServerDisconnectedError)
 
         self.cookies = SimpleCookie()
+
+        self._cleanup_handle = None
+        if (keepalive_timeout is not sentinel and
+                keepalive_timeout is not None):
+            self._cleanup()
 
     def __del__(self, _warnings=warnings):
         if self._closed:
@@ -197,43 +206,29 @@ class BaseConnector(object):
         """Cleanup unused transports."""
         if self._cleanup_handle:
             self._cleanup_handle.cancel()
-            self._cleanup_handle = None
 
-        now = self._loop.time()
+        now = self._time_service.loop_time()
 
-        connections = {}
-        timeout = self._keepalive_timeout
+        if self._conns:
+            connections = {}
+            deadline = now - self._keepalive_timeout
+            for key, conns in self._conns.items():
+                alive = []
+                for transport, proto, use_time in conns:
+                    if transport is not None:
+                        if proto.is_connected():
+                            if use_time - deadline < 0:
+                                transport.close()
+                            else:
+                                alive.append((transport, proto, use_time))
 
-        for key, conns in self._conns.items():
-            alive = []
-            for transport, proto, t0 in conns:
-                if transport is not None:
-                    if proto and not proto.is_connected():
-                        transport = None
-                    else:
-                        delta = t0 + self._keepalive_timeout - now
-                        if delta < 0:
-                            transport.close()
-                            transport = None
-                        elif delta < timeout:
-                            timeout = delta
+                if alive:
+                    connections[key] = alive
 
-                if transport is not None:
-                    alive.append((transport, proto, t0))
-            if alive:
-                connections[key] = alive
+            self._conns = connections
 
-        if connections:
-            self._cleanup_handle = self._loop.call_at(
-                ceil(now + timeout), self._cleanup)
-
-        self._conns = connections
-
-    def _start_cleanup_task(self):
-        if self._cleanup_handle is None:
-            now = self._loop.time()
-            self._cleanup_handle = self._loop.call_at(
-                ceil(now + self._keepalive_timeout), self._cleanup)
+        self._cleanup_handle = self._time_service.call_later(
+            self._keepalive_timeout / 2.0, self._cleanup)
 
     def close(self):
         """Close all opened transports."""
@@ -246,6 +241,9 @@ class BaseConnector(object):
         try:
             if self._loop.is_closed():
                 return ret
+
+            if self._time_service_owner:
+                self._time_service.close()
 
             for key, data in self._conns.items():
                 for transport, proto, t0 in data:
@@ -330,6 +328,7 @@ class BaseConnector(object):
             conns = self._conns[key]
         except KeyError:
             return None, None
+
         t1 = self._loop.time()
         while conns:
             transport, proto, t0 = conns.pop()
@@ -342,6 +341,7 @@ class BaseConnector(object):
                         # The very last connection was reclaimed: drop the key
                         del self._conns[key]
                     return transport, proto
+
         # No more connections: drop the key
         del self._conns[key]
         return None, None
@@ -397,8 +397,6 @@ class BaseConnector(object):
                 conns = self._conns[key] = []
             conns.append((transport, protocol, self._loop.time()))
             reader.unset_parser()
-
-            self._start_cleanup_task()
 
     @asyncio.coroutine
     def _create_connection(self, req):

tests/test_connector.py
@@ -22,7 +22,8 @@ from aiohttp.test_utils import unused_port
 
 
 def test_del(loop):
-    conn = aiohttp.BaseConnector(loop=loop)
+    conn = aiohttp.BaseConnector(
+        loop=loop, time_service=unittest.mock.Mock())
     transp = unittest.mock.Mock()
     conn._conns['a'] = [(transp, 'proto', 123)]
     conns_impl = conn._conns
@@ -78,7 +79,6 @@ def test_del_with_closed_loop(loop):
     conn._conns['a'] = [(transp, 'proto', 123)]
 
     conns_impl = conn._conns
-    conn._start_cleanup_task()
     exc_handler = unittest.mock.Mock()
     loop.set_exception_handler(exc_handler)
     loop.close()
@@ -127,6 +127,36 @@ def test_close(loop):
     assert not conn._conns
     assert tr.close.called
     assert conn.closed
+
+
+def test_close_time_service_owned(loop):
+    tr = unittest.mock.Mock()
+
+    conn = aiohttp.BaseConnector(loop=loop)
+    assert not conn.closed
+    conn._conns[1] = [(tr, object(), object())]
+    ts = conn._time_service = unittest.mock.Mock()
+    conn.close()
+
+    assert not conn._conns
+    assert tr.close.called
+    assert conn.closed
+    assert ts.close.called
+
+
+def test_close_time_service_unowned(loop):
+    tr = unittest.mock.Mock()
+    ts = unittest.mock.Mock()
+
+    conn = aiohttp.BaseConnector(loop=loop, time_service=ts)
+    assert not conn.closed
+    conn._conns[1] = [(tr, object(), object())]
+    conn.close()
+
+    assert not conn._conns
+    assert tr.close.called
+    assert conn.closed
+    assert not ts.close.called
 
 
 def test_get(loop):
@@ -197,7 +227,6 @@ def test_release(loop):
     conn._release(key, req, tr, proto)
     assert conn._release_waiter.called
     assert conn._conns[1][0] == (tr, proto, 10)
-    assert conn._start_cleanup_task.called
     conn.close()
 
 
@@ -424,16 +453,11 @@ def test_connect_oserr(loop):
     assert ctx.value.strerror.endswith('[permission error]')
 
 
-def test_start_cleanup_task():
+def test_ctor_cleanup():
     loop = unittest.mock.Mock()
     loop.time.return_value = 1.5
     conn = aiohttp.BaseConnector(loop=loop, keepalive_timeout=10)
-    assert conn._cleanup_handle is None
-
-    conn._start_cleanup_task()
     assert conn._cleanup_handle is not None
-    loop.call_at.assert_called_with(
-        12, conn._cleanup)
 
 
 def test_cleanup():
@@ -446,15 +470,16 @@ def test_cleanup():
     testset[1][1][1].is_connected.return_value = False
 
     loop = unittest.mock.Mock()
-    loop.time.return_value = 300
-    conn = aiohttp.BaseConnector(loop=loop)
+    time_service = unittest.mock.Mock()
+    time_service.loop_time.return_value = 300
+    conn = aiohttp.BaseConnector(loop=loop, time_service=time_service)
     conn._conns = testset
     existing_handle = conn._cleanup_handle = unittest.mock.Mock()
 
     conn._cleanup()
     assert existing_handle.cancel.called
     assert conn._conns == {}
-    assert conn._cleanup_handle is None
+    assert conn._cleanup_handle is not None
 
 
 def test_cleanup2():
@@ -462,16 +487,17 @@ def test_cleanup2():
     testset[1][0][1].is_connected.return_value = True
 
     loop = unittest.mock.Mock()
-    loop.time.return_value = 300.1
+    time_service = unittest.mock.Mock()
+    time_service.loop_time.return_value = 300
 
-    conn = aiohttp.BaseConnector(loop=loop, keepalive_timeout=10)
+    conn = aiohttp.BaseConnector(
+        loop=loop, keepalive_timeout=10, time_service=time_service)
     conn._conns = testset
     conn._cleanup()
     assert conn._conns == testset
 
     assert conn._cleanup_handle is not None
-    loop.call_at.assert_called_with(
-        310, conn._cleanup)
+    time_service.call_later.assert_called_with(5, conn._cleanup)
     conn.close()
 
 
@@ -481,17 +507,18 @@ def test_cleanup3():
     testset[1][0][1].is_connected.return_value = True
 
     loop = unittest.mock.Mock()
-    loop.time.return_value = 308.5
+    time_service = unittest.mock.Mock()
+    time_service.loop_time.return_value = 308.5
 
-    conn = aiohttp.BaseConnector(loop=loop, keepalive_timeout=10)
+    conn = aiohttp.BaseConnector(
+        loop=loop, keepalive_timeout=10, time_service=time_service)
     conn._conns = testset
 
     conn._cleanup()
     assert conn._conns == {1: [testset[1][1]]}
 
     assert conn._cleanup_handle is not None
-    loop.call_at.assert_called_with(
-        316, conn._cleanup)
+    time_service.call_later.assert_called_with(5, conn._cleanup)
     conn.close()
 
 
@@ -594,8 +621,6 @@ def test_close_twice(loop):
 
 def test_close_cancels_cleanup_handle(loop):
     conn = aiohttp.BaseConnector(loop=loop)
-    conn._start_cleanup_task()
-
     assert conn._cleanup_handle is not None
     conn.close()
     assert conn._cleanup_handle is None

aiohttp/client.py
@@ -107,10 +107,13 @@ class ClientSession:
         self._request_class = request_class
         self._response_class = response_class
         self._ws_response_class = ws_response_class
-        self._time_service = (
-            time_service
-            if time_service is not None
-            else TimeService(self._loop))
+
+        if time_service is not None:
+            self._time_service_owner = False
+            self._time_service = time_service
+        else:
+            self._time_service_owner = True
+            self._time_service = TimeService(self._loop)
 
     def __del__(self, _warnings=warnings):
         if not self.closed:
@@ -486,6 +489,10 @@ class ClientSession:
         if not self.closed:
             self._connector.close()
             self._connector = None
+
+            if self._time_service_owner:
+                self._time_service.close()
+
         ret = helpers.create_future(self._loop)
         ret.set_result(None)
         return ret

aiohttp/client_reqrep.py
@@ -790,7 +790,7 @@ class ClientResponse(HeadersMixin):
 
         @asyncio.coroutine
         def __aexit__(self, exc_type, exc_val, exc_tb):
-            if exc_type is None:
-                yield from self.release()
-            else:
-                self.close()
+            # similar to _RequestContextManager, we do not need to check
+            # for exceptions, response object can closes connection
+            # is state is broken
+            yield from self.release()

aiohttp/helpers.py
@@ -610,7 +610,7 @@ class TimeService:
         self._cb = loop.call_at(self._loop_time + self._interval, self._on_cb)
         self._scheduled = []
 
-    def stop(self):
+    def close(self):
         if self._cb:
             self._cb.cancel()
 
@@ -673,6 +673,9 @@ class TimeService:
             self._strtime = s = self._format_date_time()
         return self._strtime
 
+    def loop_time(self):
+        return self._loop_time
+
     def call_later(self, delay, callback, *args):
         """Arrange for a callback to be called at a given time.
 
@@ -703,28 +706,31 @@ class TimeService:
 
         timeout - value in seconds or None to disable timeout logic
         """
-        return LowresTimeout(timeout, self, self._loop)
+        when = self._loop_time + timeout if timeout is not None else 0.0
+
+        ctx = _TimeServiceTimeoutContext(when, self._loop)
+
+        if timeout is not None:
+            heapq.heappush(self._scheduled, ctx)
+
+        return ctx
 
 
-class LowresTimeout:
+class _TimeServiceTimeoutContext(TimerHandle):
     """ Low resolution timeout context manager """
 
-    def __init__(self, timeout, time_service, loop):
-        self._loop = loop
-        self._timeout = timeout
-        self._time_service = time_service
+    def __init__(self, when, loop):
+        super().__init__(when, self.cancel, (), loop)
+
         self._task = None
         self._cancelled = False
-        self._cancel_handler = None
 
     def __enter__(self):
         self._task = asyncio.Task.current_task(loop=self._loop)
         if self._task is None:
+            self._cancelled = True
             raise RuntimeError('Timeout context manager should be used '
                                'inside a task')
-        if self._timeout is not None:
-            self._cancel_handler = self._time_service.call_later(
-                self._timeout, self._cancel_task)
 
         return self
 
@@ -732,14 +738,17 @@ class LowresTimeout:
         self._task = None
 
         if exc_type is asyncio.CancelledError and self._cancelled:
-            self._cancel_handler = None
             raise asyncio.TimeoutError from None
-        if self._timeout is not None:
-            self._cancel_handler.cancel()
-            self._cancel_handler = None
 
-    def _cancel_task(self):
-        self._cancelled = self._task.cancel()
+        self._cancelled = True
+
+    def cancel(self):
+        if not self._cancelled:
+            if self._task is not None:
+                self._task.cancel()
+                self._task = None
+
+            self._cancelled = True
 
 
 class HeadersMixin:

aiohttp/web_server.py
@@ -160,7 +160,7 @@ class Server:
         coros = [conn.shutdown(timeout) for conn in self._connections]
         yield from asyncio.gather(*coros, loop=self._loop)
         self._connections.clear()
-        self._time_service.stop()
+        self._time_service.close()
 
     finish_connections = shutdown
 

tests/test_helpers.py
@@ -421,7 +421,7 @@ class TestTimeService:
         assert time_service._strtime is None
 
     def test_stop(self, time_service):
-        time_service.stop()
+        time_service.close()
         assert time_service._cb is None
         assert time_service._loop is None
 
@@ -430,13 +430,13 @@ class TestTimeService:
             return x
 
         handle = time_service.call_later(10, cb, 'test')
-        time_service.stop()
+        time_service.close()
         assert handle._cancelled
         assert not time_service._scheduled
 
     def test_double_stopping(self, time_service):
-        time_service.stop()
-        time_service.stop()
+        time_service.close()
+        time_service.close()
         assert time_service._cb is None
         assert time_service._loop is None
 

commit fd9fd67e264245723c6a70cf145b1e5fc3a228c2

Fix keepalive connections not being closed in time
Refactoring in 964921d4e97e7c84bcfda6772ed458549aea0b09 introduced a
regression so that `_cleanup()` could be called only once or few times.
`_release()` expects `self._cleanup_handle` to be None to add new
`weakref_handle`. But when `_cleanup()` called and there are no
remaining connections, `self._cleanup_handle` will remain as
`<TimerHandle cancelled when=5853434>`, so `_release()` will not have a
chance to schedule `_cleanup()` call again.

Closes: #3296

aiohttp/connector.py
@@ -298,6 +298,8 @@ class BaseConnector:
         """Cleanup unused transports."""
         if self._cleanup_handle:
             self._cleanup_handle.cancel()
+            # _cleanup_handle should be unset, otherwise _release() will not recreate it ever!
+            self._cleanup_handle = None
 
         now = self._loop.time()
         timeout = self._keepalive_timeout

tests/test_connector.py
@@ -344,6 +344,7 @@ async def test_release(loop, key) -> None:
 
     conn._release(key, proto)
     assert conn._release_waiter.called
+    assert conn._cleanup_handle is not None
     assert conn._conns[key][0][0] == proto
     assert conn._conns[key][0][1] == pytest.approx(loop.time(), abs=0.1)
     assert not conn._cleanup_closed_transports
@@ -1122,7 +1123,7 @@ async def test_cleanup(key) -> None:
     conn._cleanup()
     assert existing_handle.cancel.called
     assert conn._conns == {}
-    assert conn._cleanup_handle is not None
+    assert conn._cleanup_handle is None
 
 
 async def test_cleanup_close_ssl_transport(loop, ssl_key) -> None: