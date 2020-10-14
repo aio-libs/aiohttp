52ff43634ca02ecfc33f88f8821ed584990ecc67
Andrew Svetlov <andrew.svetlov@gmail.com>
2020-10-14 14:17:02 +0300

    Update linter

--- aiohttp/__init__.py
+++ aiohttp/__init__.py
@@ -1,6 +1,6 @@
 __version__ = '4.0.0a1'
 
-from typing import Tuple  # noqa
+from typing import Tuple
 
 from . import hdrs as hdrs
 from .client import BaseConnector as BaseConnector
@@ -114,7 +114,7 @@ from .tracing import (
     TraceResponseChunkReceivedParams as TraceResponseChunkReceivedParams,
 )
 
-__all__ = (
+__all__: Tuple[str, ...] = (
     'hdrs',
     # client
     'BaseConnector',
@@ -215,7 +215,7 @@ __all__ = (
     'TraceRequestRedirectParams',
     'TraceRequestStartParams',
     'TraceResponseChunkReceivedParams',
-)  # type: Tuple[str, ...]
+)
 
 try:
     from .worker import GunicornUVLoopWebWorker, GunicornWebWorker  # noqa
