a9fb9f0fd5064843f535108952ef398bcc3a79da
Andrew Svetlov <andrew.svetlov@gmail.com>
2020-10-14 13:28:05 +0300

    Fix isort

--- aiohttp/payload.py
+++ aiohttp/payload.py
@@ -391,7 +391,7 @@ class JsonPayload(BytesPayload):
 
 
 if TYPE_CHECKING:  # pragma: no cover
-    from typing import AsyncIterator, AsyncIterable
+    from typing import AsyncIterable, AsyncIterator
 
     _AsyncIterator = AsyncIterator[bytes]
     _AsyncIterable = AsyncIterable[bytes]
