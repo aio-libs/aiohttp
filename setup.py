c4fe55b7b9643e6d3641bedd2c6ac818abf72335
Andrew Svetlov <andrew.svetlov@gmail.com>
2020-10-24 13:46:39 +0300

    Change string style

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ class ve_build_ext(build_ext):
 
 txt = (here / "aiohttp" / "__init__.py").read_text("utf-8")
 try:
-    version = re.findall(r"^__version__ = '([^']+)'\r?$", txt, re.M)[0]
+    version = re.findall(r'^__version__ = "([^"]+)"\r?$', txt, re.M)[0]
 except IndexError:
     raise RuntimeError("Unable to determine version.")
 
