6f7cd7750f3242a1944a66e0df119fb311c7dfd8
Dmitry Erlikh <derlih@gmail.com>
2020-11-23 18:06:08 +0100

    Fix

--- aiohttp/abc.py
+++ aiohttp/abc.py
@@ -135,6 +135,9 @@ else:
     IterableBase = Iterable
 
 
+ClearCookiePredicate = Callable[["Morsel[str]"], bool]
+
+
 class AbstractCookieJar(Sized, IterableBase):
     """Abstract Cookie Jar."""
 
@@ -142,8 +145,12 @@ class AbstractCookieJar(Sized, IterableBase):
         self._loop = get_running_loop(loop)
 
     @abstractmethod
-    def clear(self) -> None:
-        """Clear all cookies."""
+    def clear(self, predicate: Optional[ClearCookiePredicate] = None) -> None:
+        """Clear all cookies if no predicate is passed."""
+
+    @abstractmethod
+    def clear_domain(self, domain: str) -> None:
+        """Clear all cookies for domain and all subdomains."""
 
     @abstractmethod
     def update_cookies(self, cookies: LooseCookies, response_url: URL = URL()) -> None:
