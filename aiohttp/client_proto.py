a735015ef8487c40d410313cc4f3ffd2f2d21e39
Andrew Svetlov <andrew.svetlov@gmail.com>
2020-11-27 19:55:02 +0200

    [3.8] Refactor web error handling (#5270). (#5295)
    (cherry picked from commit e9fdf0a2eb1f7fbeb015f6b66fe9b266ae55e66c)
    
    Co-authored-by: Andrew Svetlov <andrew.svetlov@gmail.com>

--- aiohttp/client_proto.py
+++ aiohttp/client_proto.py
@@ -231,7 +231,7 @@ class ResponseHandler(BaseProtocol, DataQueue[Tuple[RawResponseMessage, StreamRe
                     self._payload = payload
 
                     if self._skip_payload or message.code in (204, 304):
-                        self.feed_data((message, EMPTY_PAYLOAD), 0)  # type: ignore
+                        self.feed_data((message, EMPTY_PAYLOAD), 0)
                     else:
                         self.feed_data((message, payload), 0)
                 if payload is not None:
