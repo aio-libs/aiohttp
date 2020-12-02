1bb6ab3eadc7ee199db20b7385358a365fd604f0
Sam Bull <aa6bs0@sambull.org>
2020-12-02 12:33:00 +0000

    Spelling

--- aiohttp/web_app.py
+++ aiohttp/web_app.py
@@ -321,7 +321,7 @@ class Application(MutableMapping[str, Any]):
         if self.on_cleanup.frozen:
             await self.on_cleanup.send(self)
         else:
-            # If an exception occurrs in startup, ensure cleanup contexts are completed.
+            # If an exception occurs in startup, ensure cleanup contexts are completed.
             await self._cleanup_ctx._on_cleanup(self)
 
     def _prepare_middleware(self) -> Iterator[_Middleware]:
