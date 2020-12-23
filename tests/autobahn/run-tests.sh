0fb98d973c4651f5812f12b3d500ea831c6c9cae
Dmitry Erlikh <derlih@gmail.com>
2020-12-23 17:34:03 +0100

    extract build to separate file

--- tests/autobahn/run-tests.sh
+++ tests/autobahn/run-tests.sh
@@ -2,4 +2,8 @@
 
 rm -rf $PWD/reports
 mkdir $PWD/reports
-USER_ID=$(id -u) docker-compose -f $PWD/client/docker-compose.yml up --build --abort-on-container-exit
+
+USER_ID=$(id -u) docker-compose -p aiohttp-autobahn -f $PWD/images-for-compose.yml build --parallel
+
+docker-compose -f $PWD/client/docker-compose.yml up --abort-on-container-exit
+docker-compose -f $PWD/client/docker-compose.yml down
