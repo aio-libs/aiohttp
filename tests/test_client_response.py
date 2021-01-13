fc29651867cbf5cdda290bf6e7caf2891fb499a8
Adam Horacek <adam.horacek@gmail.com>
2021-01-13 20:56:09 +0100

    unit test

--- tests/test_client_response.py
+++ tests/test_client_response.py
@@ -1243,3 +1243,22 @@ def test_response_links_empty(loop: Any, session: Any) -> None:
     )
     response._headers = CIMultiDict()
     assert response.links == {}
+
+
+def test_response_not_closed_after_get_ok() -> None:
+    response = ClientResponse(
+        "get",
+        URL("http://del-cl-resp.org"),
+        request_info=mock.Mock(),
+        writer=mock.Mock(),
+        continue100=None,
+        timer=TimerNoop(),
+        traces=[],
+        loop=mock.Mock(),
+        session=mock.Mock(),
+    )
+    response.status = 400
+    response.reason = "Bad Request"
+    response._closed = False
+    assert not response.ok
+    assert not response.closed
