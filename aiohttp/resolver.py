45bb69545dd8744666dab45a9d22cf74ccbbdba0
cryato <psadikov-p@mail.ru>
2021-01-17 13:00:38 +0300

    add address size checking

--- aiohttp/resolver.py
+++ aiohttp/resolver.py
@@ -37,7 +37,9 @@ class ThreadedResolver(AbstractResolver):
 
         hosts = []
         for family, _, proto, _, address in infos:
-            if family == socket.AF_INET6 and address[3]:  # type: ignore
+            if (
+                family == socket.AF_INET6 and len(address) >= 4 and address[3]
+            ):  # type: ignore
                 # This is essential for link-local IPv6 addresses.
                 # LL IPv6 is a VERY rare case. Strictly speaking, we should use
                 # getnameinfo() unconditionally, but performance makes sense.
