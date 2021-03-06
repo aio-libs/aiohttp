5a31a5475cdd7afb4bfa971b857b25af4fdd2b5a
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2021-03-06 20:22:31 +0100

    Prepare Sphinx setup for stricter nitpicky mode
    (cherry picked from commit a4a7a0ae7fafdde82626761284ecf7a60f2ad65f)

--- docs/conf.py
+++ docs/conf.py
@@ -345,3 +345,7 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 # texinfo_no_detailmenu = False
+
+
+# -------------------------------------------------------------------------
+# nitpicky = True
