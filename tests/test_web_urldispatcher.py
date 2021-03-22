1fd08e1e5f3bed3fcdc093fe666f1978cf5d1f62
Daniel King <dking@broadinstitute.org>
2021-03-22 18:02:54 -0400

    apply black changes

--- tests/test_web_urldispatcher.py
+++ tests/test_web_urldispatcher.py
@@ -466,6 +466,7 @@ async def test_for_issue_5250(aiohttp_client: Any, tmp_path: Any) -> None:
 
     async def get_foobar(request):
         return web.Response(body="success!")
+
     app.router.add_get("/foobar", get_foobar)
 
     client = await aiohttp_client(app)
