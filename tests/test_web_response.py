bb5624b24f97841e68842c1d722165f3a095ecc2
Slava <slovaricheg@gmail.com>
2021-03-24 14:17:53 +0200

    Implement ETag support (#4594) (#5562)
    This change adds an `etag` property to the response object and
    `if_match`, `if_none_match` properties to the request object.
    Also, it implements ETag support in static routes and fixes a
    few bugs found along the way.
    
    Refs:
    * https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.26
    * https://tools.ietf.org/html/rfc7232#section-2.3
    * https://tools.ietf.org/html/rfc7232#section-6
    
    PR #5298 by @greshilov
    Resolves https://github.com/aio-libs/aiohttp/issues/4594
    
    Co-Authored-By: Serhiy Storchaka <storchaka@gmail.com>
    Co-Authored-By: Andrew Svetlov <andrew.svetlov@gmail.com>
    
    Co-authored-by: Serhiy Storchaka <storchaka@gmail.com>
    Co-authored-by: Andrew Svetlov <andrew.svetlov@gmail.com>

--- tests/test_web_response.py
+++ tests/test_web_response.py
@@ -11,6 +11,7 @@ from multidict import CIMultiDict, CIMultiDictProxy
 from re_assert import Matches
 
 from aiohttp import HttpVersion, HttpVersion10, HttpVersion11, hdrs
+from aiohttp.helpers import ETag
 from aiohttp.payload import BytesPayload
 from aiohttp.test_utils import make_mocked_coro, make_mocked_request
 from aiohttp.web import ContentCoding, Response, StreamResponse, json_response
@@ -22,7 +23,7 @@ def make_request(
     headers=CIMultiDict(),
     version=HttpVersion11,
     on_response_prepare=None,
-    **kwargs
+    **kwargs,
 ):
     app = kwargs.pop("app", None) or mock.Mock()
     app._debug = False
@@ -252,6 +253,82 @@ def test_last_modified_reset() -> None:
     resp.last_modified = 0
     resp.last_modified = None
     assert resp.last_modified is None
+
+
+def test_etag_initial() -> None:
+    resp = StreamResponse()
+    assert resp.etag is None
+
+
+def test_etag_string() -> None:
+    resp = StreamResponse()
+    value = "0123-kotik"
+    resp.etag = value
+    assert resp.etag == ETag(value=value)
+    assert resp.headers[hdrs.ETAG] == f'"{value}"'
+
+
+@pytest.mark.parametrize(
+    ["etag", "expected_header"],
+    (
+        (ETag(value="0123-weak-kotik", is_weak=True), 'W/"0123-weak-kotik"'),
+        (ETag(value="0123-strong-kotik", is_weak=False), '"0123-strong-kotik"'),
+    ),
+)
+def test_etag_class(etag, expected_header) -> None:
+    resp = StreamResponse()
+    resp.etag = etag
+    assert resp.etag == etag
+    assert resp.headers[hdrs.ETAG] == expected_header
+
+
+def test_etag_any() -> None:
+    resp = StreamResponse()
+    resp.etag = "*"
+    assert resp.etag == ETag(value="*")
+    assert resp.headers[hdrs.ETAG] == "*"
+
+
+@pytest.mark.parametrize(
+    "invalid_value",
+    (
+        '"invalid"',
+        "повинен бути ascii",
+        ETag(value='"invalid"', is_weak=True),
+        ETag(value="bad ©®"),
+    ),
+)
+def test_etag_invalid_value_set(invalid_value) -> None:
+    resp = StreamResponse()
+    with pytest.raises(ValueError, match="is not a valid etag"):
+        resp.etag = invalid_value
+
+
+@pytest.mark.parametrize(
+    "header",
+    (
+        "forgotten quotes",
+        '"∀ x ∉ ascii"',
+    ),
+)
+def test_etag_invalid_value_get(header) -> None:
+    resp = StreamResponse()
+    resp.headers["ETag"] = header
+    assert resp.etag is None
+
+
+@pytest.mark.parametrize("invalid", (123, ETag(value=123, is_weak=True)))
+def test_etag_invalid_value_class(invalid) -> None:
+    resp = StreamResponse()
+    with pytest.raises(ValueError, match="Unsupported etag type"):
+        resp.etag = invalid
+
+
+def test_etag_reset() -> None:
+    resp = StreamResponse()
+    resp.etag = "*"
+    resp.etag = None
+    assert resp.etag is None
 
 
 async def test_start() -> None:
