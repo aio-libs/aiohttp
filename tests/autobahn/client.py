19067ccd64b828753eef93e61b81cc8a96fff702
Will Fatherley <wefatherley@gmail.com>
2021-04-20 17:20:00 -0400

    removes deprecated logic from autobahn client and server tests

--- tests/autobahn/client.py
+++ tests/autobahn/client.py
@@ -6,31 +6,32 @@ import aiohttp
 
 
 async def client(loop, url, name):
-    ws = await aiohttp.ws_connect(url + "/getCaseCount")
-    num_tests = int((await ws.receive()).data)
-    print("running %d cases" % num_tests)
-    await ws.close()
+    async with aiohttp.ClientSession() as session:
+        async with session.ws_connect(url + "/getCaseCount") as ws:
+            num_tests = int((await ws.receive()).data)
+            print("running %d cases" % num_tests)
+            await ws.close()
 
-    for i in range(1, num_tests + 1):
-        print("running test case:", i)
-        text_url = url + "/runCase?case=%d&agent=%s" % (i, name)
-        ws = await aiohttp.ws_connect(text_url)
-        while True:
-            msg = await ws.receive()
+            for i in range(1, num_tests + 1):
+                print("running test case:", i)
+                text_url = url + "/runCase?case=%d&agent=%s" % (i, name)
+                ws = await session.ws_connect(text_url)
+                while True:
+                    msg = await ws.receive()
 
-            if msg.type == aiohttp.WSMsgType.TEXT:
-                await ws.send_str(msg.data)
-            elif msg.type == aiohttp.WSMsgType.BINARY:
-                await ws.send_bytes(msg.data)
-            elif msg.type == aiohttp.WSMsgType.CLOSE:
-                await ws.close()
-                break
-            else:
-                break
+                    if msg.type == aiohttp.WSMsgType.TEXT:
+                        await ws.send_str(msg.data)
+                    elif msg.type == aiohttp.WSMsgType.BINARY:
+                        await ws.send_bytes(msg.data)
+                    elif msg.type == aiohttp.WSMsgType.CLOSE:
+                        await ws.close()
+                        break
+                    else:
+                        break
 
-    url = url + "/updateReports?agent=%s" % name
-    ws = await aiohttp.ws_connect(url)
-    await ws.close()
+        url = url + "/updateReports?agent=%s" % name
+        ws = await session.ws_connect(url)
+        await ws.close()
 
 
 async def run(loop, url, name):
