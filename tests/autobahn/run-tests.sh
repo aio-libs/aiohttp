7ccc5f706fee83b933664af89200cc059d2345c2
Anes Abismail <anesabismail@gmail.com>
2021-05-14 23:46:49 +0100

    Fix: Rename images-for-compose.yml to docker-compose.yml

--- tests/autobahn/run-tests.sh
+++ tests/autobahn/run-tests.sh
@@ -3,7 +3,7 @@
 rm -rf $PWD/reports
 mkdir $PWD/reports
 
-USER_ID=$(id -u) docker-compose -p aiohttp-autobahn -f $PWD/images-for-compose.yml build --parallel
+docker-compose -p aiohttp-autobahn build
 
 docker-compose -f $PWD/client/docker-compose.yml up --abort-on-container-exit
 docker-compose -f $PWD/client/docker-compose.yml down
