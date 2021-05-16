6fb3efc4b5dd971e3f0e1080435fead757f15eaa
Will Fatherley <will@8point3one4.com>
2021-05-16 12:49:07 -0400

    Update client/server implementation in the autobahn tests (#5637)

--- tests/autobahn/server.py
+++ tests/autobahn/server.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
-import asyncio
 import logging
 
-from aiohttp import web
+from aiohttp import WSCloseCode, web
 
 
 async def wshandler(request):
@@ -30,29 +29,20 @@ async def wshandler(request):
     return ws
 
 
-async def main(loop):
-    app = web.Application()
-    app.router.add_route("GET", "/", wshandler)
-
-    handler = app._make_handler()
-    srv = await loop.create_server(handler, "127.0.0.1", 9001)
-    print("Server started at http://127.0.0.1:9001")
-    return app, srv, handler
-
-
-async def finish(app, srv, handler):
-    srv.close()
-    await handler.shutdown()
-    await srv.wait_closed()
+async def on_shutdown(app):
+    for ws in set(app["websockets"]):
+        await ws.close(code=WSCloseCode.GOING_AWAY, message="Server shutdown")
 
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
     logging.basicConfig(
         level=logging.DEBUG, format="%(asctime)s %(levelname)s %(message)s"
     )
-    app, srv, handler = loop.run_until_complete(main(loop))
+
+    app = web.Application()
+    app.router.add_route("GET", "/", wshandler)
+    app.on_shutdown.append(on_shutdown)
     try:
-        loop.run_forever()
+        web.run_app(app, host="127.0.0.1", port=9001)
     except KeyboardInterrupt:
-        loop.run_until_complete(finish(app, srv, handler))
+        print("Server stopped at http://127.0.0.1:9001")
