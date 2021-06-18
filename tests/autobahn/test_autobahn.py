3ccc9d8b611e88946cac4a66210587680b8680e0
Anes Abismail <anesabismail@gmail.com>
2021-06-18 20:07:56 +0100

    Fix docker compose file paths

--- tests/autobahn/test_autobahn.py
+++ tests/autobahn/test_autobahn.py
@@ -15,18 +15,18 @@ def create_report_directory():
 
 @pytest.fixture(scope="session", autouse=True)
 def build_aiohttp_docker_image():
-    docker = DockerClient(compose_files=["docker-compose.yml"])
+    docker = DockerClient(compose_files=["tests/autobahn/docker-compose.yml"])
     yield docker.compose.build()
     docker.compose.rm()
 
 
 def test_client():
-    docker = DockerClient(compose_files=["client/docker-compose.yml"])
+    docker = DockerClient(compose_files=["tests/autobahn/client/docker-compose.yml"])
     docker.compose.up(abort_on_container_exit=True)
     docker.compose.down()
 
 
 def test_server():
-    docker = DockerClient(compose_files=["server/docker-compose.yml"])
+    docker = DockerClient(compose_files=["tests/autobahn/server/docker-compose.yml"])
     docker.compose.up(abort_on_container_exit=True)
     docker.compose.down()
