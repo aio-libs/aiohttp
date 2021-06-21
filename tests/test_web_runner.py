fa46667b39ab81554c989071fea41ca80963c880
Sam Bull <aa6bs0@sambull.org>
2021-06-21 23:05:14 +0100

    Backport #5572: Use new loop for web.run_app(). (#5820)
    * Use new loop for web.run_app().
    
    * Skip test on 3.6

--- tests/test_web_runner.py
+++ tests/test_web_runner.py
@@ -1,6 +1,7 @@
 import asyncio
 import platform
 import signal
+import sys
 from unittest.mock import patch
 
 import pytest
@@ -162,3 +163,28 @@ async def test_tcpsite_default_host(make_runner):
     assert server is runner.server
     assert host is None
     assert port == 8080
+
+
+@pytest.mark.skipif(sys.version_info < (3, 7), reason="Requires asyncio.run()")
+def test_run_after_asyncio_run() -> None:
+    async def nothing():
+        pass
+
+    def spy():
+        spy.called = True
+
+    spy.called = False
+
+    async def shutdown():
+        spy()
+        raise web.GracefulExit()
+
+    # asyncio.run() creates a new loop and closes it.
+    asyncio.run(nothing())
+
+    app = web.Application()
+    # create_task() will delay the function until app is run.
+    app.on_startup.append(lambda a: asyncio.create_task(shutdown()))
+
+    web.run_app(app)
+    assert spy.called, "run_app() should work after asyncio.run()."
