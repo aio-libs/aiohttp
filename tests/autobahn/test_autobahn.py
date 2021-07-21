23536f4054b997077e78b2ddb5495f11468c0649
Anes Abismail <anesabismail@gmail.com>
2021-07-21 20:43:23 +0100

    Use docker instead of docker-compose

--- tests/autobahn/test_autobahn.py
+++ tests/autobahn/test_autobahn.py
@@ -1,94 +1,76 @@
 import json
 import shutil
-import socket
 import subprocess
 import sys
-import time
 from pathlib import Path
-from typing import Any, Dict, List
+from typing import Any, Dict, Generator, List
 
 import pytest
-
-HOST = "localhost"
-PORT = 9001
+from python_on_whales import docker
 
 
 @pytest.fixture(scope="module", autouse=True)
 def create_report_directory(request: Any) -> None:
-    path = Path(f"{request.fspath.dirname}/reports")
+    path = Path(request.fspath.dirname) / "reports"
     if path.is_dir():
         shutil.rmtree(path)
     path.mkdir()
 
 
 @pytest.fixture(scope="session", autouse=True)
-def setup_venv() -> None:
-    if not Path("tests/autobahn/autobahntestsuite-env").exists():
-        print("Creating Python 2.7 environment and installing autobahntestsuite")
-        subprocess.check_call(
-            ["virtualenv", "-p", "python2.7", "tests/autobahn/autobahntestsuite-env"]
-        )
-        subprocess.check_call(
-            [
-                "tests/autobahn/autobahntestsuite-env/bin/pip",
-                "install",
-                "autobahntestsuite>=0.8.0",
-            ]
-        )
-
-
-def wait_for_server(host: str, port: int) -> None:
-    while True:
-        sock = socket.socket()
-        try:
-            sock.connect((HOST, PORT))
-        except ConnectionRefusedError:
-            time.sleep(0.01)
-        else:
-            return
-        finally:
-            sock.close()
+def build_autobahn_testsuite() -> Generator[None, None, None]:
+    docker.build(
+        file="tests/autobahn/Dockerfile.autobahn",
+        tags=["autobahn-testsuite"],
+        context_path=".",
+    )
+    try:
+        yield
+    finally:
+        docker.image.remove(x="autobahn-testsuite")
 
 
 def get_failed_tests(report_path: str, name: str) -> List[Dict[str, Any]]:
-    report_path = Path(report_path)
-    result_summary = json.loads((report_path / "index.json").read_text())[name]
+    path = Path(report_path)
+    result_summary = json.loads((path / "index.json").read_text())[name]
     failed_messages = []
     PASS = {"OK", "INFORMATIONAL"}
     entry_fields = {"case", "description", "expectation", "expected", "received"}
     for results in result_summary.values():
         if results["behavior"] in PASS and results["behaviorClose"] in PASS:
             continue
-        report = json.loads((report_path / results["reportfile"]).read_text())
+        report = json.loads((path / results["reportfile"]).read_text())
         failed_messages.append({field: report[field] for field in entry_fields})
     return failed_messages
 
 
-def test_client() -> None:
-    print("Starting autobahntestsuite server")
-    server = subprocess.Popen(
-        [
-            "tests/autobahn/autobahntestsuite-env/bin/wstest",
-            "-m",
-            "fuzzingserver",
-            "-s",
-            "tests/autobahn/client/fuzzingserver.json",
-        ]
-    )
-    print("Waiting for server to start")
-    wait_for_server(HOST, PORT)
+@pytest.mark.skipif(sys.platform != "linux", reason="run only on Linux")
+def test_client(request: Any) -> None:
     try:
-        print("Running wsproto test client")
+        print("Starting autobahn-testsuite server")
+        autobahn_container = docker.run(
+            detach=True,
+            image="autobahn-testsuite",
+            name="autobahn",
+            publish=[(9001, 9001)],
+            remove=True,
+            volumes=[
+                (f"{request.fspath.dirname}/client", "/config"),
+                (f"{request.fspath.dirname}/reports", "/reports"),
+            ],
+        )
+        print("Running aiohttp test client")
         client = subprocess.Popen(
-            [sys.executable] + ["tests/autobahn/client/client.py"]
+            ["wait-for-it", "-s", "localhost:9001", "--"]
+            + [sys.executable]
+            + ["tests/autobahn/client/client.py"]
         )
         client.wait()
     finally:
-        print("Stopping server")
+        print("Stopping client and server")
         client.terminate()
         client.wait()
-        server.terminate()
-        server.wait()
+        autobahn_container.stop()
 
     failed_messages = get_failed_tests("tests/autobahn/reports/clients", "aiohttp")
 
@@ -101,29 +83,37 @@ def test_client() -> None:
     )
 
 
-def test_server() -> None:
-    print("Starting wsproto test server")
-    server = subprocess.Popen([sys.executable] + ["tests/autobahn/server/server.py"])
+@pytest.mark.skipif(sys.platform != "linux", reason="run only on Linux")
+def test_server(request: Any) -> None:
     try:
-        print("Waiting for server to start")
-        wait_for_server(HOST, PORT)
-        print("Starting autobahntestsuite client")
-        client = subprocess.Popen(
-            [
-                "tests/autobahn/autobahntestsuite-env/bin/wstest",
-                "-m",
-                "fuzzingclient",
-                "-s",
-                "tests/autobahn/server/fuzzingclient.json",
-                "-o",
-                "tests/autobahn/reports/servers",
-            ]
+        print("Starting aiohttp test server")
+        server = subprocess.Popen(
+            [sys.executable] + ["tests/autobahn/server/server.py"]
         )
-        client.wait()
+        print("Starting autobahn-testsuite client")
+        docker.run(
+            image="autobahn-testsuite",
+            name="autobahn",
+            remove=True,
+            volumes=[
+                (f"{request.fspath.dirname}/server", "/config"),
+                (f"{request.fspath.dirname}/reports", "/reports"),
+            ],
+            networks=["host"],
+            command=[
+                "wait-for-it",
+                "-s",
+                "localhost:9001",
+                "--",
+                "wstest",
+                "--mode",
+                "fuzzingclient",
+                "--spec",
+                "/config/fuzzingclient.json",
+            ],
+        )
     finally:
-        print("Stopping server")
-        client.terminate()
-        client.wait()
+        print("Stopping client and server")
         server.terminate()
         server.wait()
 
