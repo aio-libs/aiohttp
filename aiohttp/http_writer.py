ce3dc76d0ff3f13537edf922ba85350199dd51a9
franekmagiera <framagie@gmail.com>
2021-08-17 00:04:18 +0200

    Refactor headers serialization back to the broken down version and add the second CRLF sign after the headers

--- aiohttp/http_writer.py
+++ aiohttp/http_writer.py
@@ -181,11 +181,9 @@ def _safe_header(string: str) -> str:
 
 
 def _py_serialize_headers(status_line: str, headers: "CIMultiDict[str]") -> bytes:
-    headers_strings = [
-        _safe_header(k) + ": " + _safe_header(v) + "\r\n" for k, v in headers.items()
-    ]
-    line = status_line + "\r\n" + "".join(headers_strings)
-    return line.encode("utf-8") + b"\r\n"
+    headers_gen = (_safe_header(k) + ": " + _safe_header(v) for k, v in headers.items())
+    line = status_line + "\r\n" + "\r\n".join(headers_gen) + "\r\n\r\n"
+    return line.encode("utf-8")
 
 
 _serialize_headers = _py_serialize_headers
