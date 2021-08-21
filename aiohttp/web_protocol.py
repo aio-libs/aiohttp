6c2814c5cf86b2fe5433a23dc114ae186aec257a
Sam Bull <aa6bs0@sambull.org>
2021-08-21 13:50:20 +0100

    Remove remaining attrs (#5960)

--- aiohttp/web_protocol.py
+++ aiohttp/web_protocol.py
@@ -1,5 +1,6 @@
 import asyncio
 import asyncio.streams
+import dataclasses
 import traceback
 from collections import deque
 from contextlib import suppress
@@ -20,7 +21,6 @@ from typing import (
     cast,
 )
 
-import attr
 import yarl
 
 from .abc import AbstractAccessLogger, AbstractAsyncAccessLogger, AbstractStreamWriter
@@ -105,7 +105,7 @@ class AccessLoggerWrapper(AbstractAsyncAccessLogger):
         self.access_logger.log(request, response, self._loop.time() - request_start)
 
 
-@attr.s(auto_attribs=True, frozen=True, slots=True)
+@dataclasses.dataclass(frozen=True)
 class _ErrInfo:
     status: int
     exc: BaseException
