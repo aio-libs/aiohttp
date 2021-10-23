a4b7505172a9f84022f14c5976f108bc847a7475
Sam Bull <aa6bs0@sambull.org>
2021-10-23 17:33:50 +0100

    Don't cancel tasks when entering a timer context (#5853)

--- aiohttp/helpers.py
+++ aiohttp/helpers.py
@@ -654,7 +654,6 @@ class TimerContext(BaseTimerContext):
             )
 
         if self._cancelled:
-            task.cancel()
             raise asyncio.TimeoutError from None
 
         self._tasks.append(task)
