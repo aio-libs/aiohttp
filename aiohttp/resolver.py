148bdf6a5f5fb8fd4edd23ef21733cef27217055
秋葉 <ambiguous404@gmail.com>
2021-10-24 20:05:03 +0800

    Fix: fix the code about handling `getaddrinfo` when disable ipv6 in (#5906)
    CPython and enable ipv6 in system.
    Related to #5901; `getaddrinfo` will return an `(int, bytes)` tuple, if
    CPython could not handle the address family. It will cause a index out
    of range error in aiohttp.

--- aiohttp/resolver.py
+++ aiohttp/resolver.py
@@ -38,7 +38,9 @@ class ThreadedResolver(AbstractResolver):
 
         hosts = []
         for family, _, proto, _, address in infos:
-            if family == socket.AF_INET6 and address[3]:  # type: ignore[misc]
+            if family == socket.AF_INET6:
+                if not (socket.has_ipv6 and address[3]):  # type: ignore[misc]
+                    continue
                 # This is essential for link-local IPv6 addresses.
                 # LL IPv6 is a VERY rare case. Strictly speaking, we should use
                 # getnameinfo() unconditionally, but performance makes sense.
