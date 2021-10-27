6c42d31a14eb6fe28d68c569678b51f0ea036d28
Andrew Svetlov <andrew.svetlov@gmail.com>
2021-10-27 16:26:00 +0300

    cherry-pick https://github.com/aio-libs/aiohttp/pull/5364

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if sys.version_info < (3, 6):
 
 here = pathlib.Path(__file__).parent
 
-if (here / ".git").exists() and not (here / "vendor/http-parser/README.md").exists():
+if (here / ".git").exists() and not (here / "vendor/llhttp/README.md").exists():
     print("Install submodules when building from git clone", file=sys.stderr)
     print("Hint:", file=sys.stderr)
     print("  git submodule update --init", file=sys.stderr)
@@ -26,10 +26,13 @@ extensions = [
         "aiohttp._http_parser",
         [
             "aiohttp/_http_parser.c",
-            "vendor/http-parser/http_parser.c",
             "aiohttp/_find_header.c",
+            "vendor/llhttp/build/c/llhttp.c",
+            "vendor/llhttp/src/native/api.c",
+            "vendor/llhttp/src/native/http.c",
         ],
-        define_macros=[("HTTP_PARSER_STRICT", 0)],
+        define_macros=[("LLHTTP_STRICT_MODE", 0)],
+        include_dirs=["vendor/llhttp/build"],
     ),
     Extension("aiohttp._helpers", ["aiohttp/_helpers.c"]),
     Extension("aiohttp._http_writer", ["aiohttp/_http_writer.c"]),
