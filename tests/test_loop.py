5c4f1055dba0f2cc7cdf419a994919fb03855d97
Han Qiao <qiao.han@aicadium.ai>
2021-10-28 21:43:53 +0800

    Switch to ThreadedChildWatcher and test (#5877) (#5919)
    * Switch to ThreadedChildWatcher and test (#5877)
    
    Co-authored-by: Sviatoslav Sydorenko <wk.cvs.github@sydorenko.org.ua>
    (cherry picked from commit 33a38b8c358011fc8bc9198cd62a2a50b69bbc14)
    
    * Fix event loop access for py310
    
    Co-authored-by: Andrew Svetlov <andrew.svetlov@gmail.com>

--- tests/test_loop.py
+++ tests/test_loop.py
@@ -5,14 +5,15 @@ import threading
 import pytest
 
 from aiohttp import web
-from aiohttp.test_utils import AioHTTPTestCase
+from aiohttp.helpers import PY_38
+from aiohttp.test_utils import AioHTTPTestCase, loop_context
 
 
 @pytest.mark.skipif(
     platform.system() == "Windows", reason="the test is not valid for Windows"
 )
 async def test_subprocess_co(loop) -> None:
-    assert isinstance(threading.current_thread(), threading._MainThread)
+    assert PY_38 or isinstance(threading.current_thread(), threading._MainThread)
     proc = await asyncio.create_subprocess_shell(
         "exit 0",
         stdin=asyncio.subprocess.DEVNULL,
@@ -40,3 +41,24 @@ class TestCase(AioHTTPTestCase):
 
 def test_default_loop(loop) -> None:
     assert asyncio.get_event_loop_policy().get_event_loop() is loop
+
+
+@pytest.mark.xfail(not PY_38, reason="ThreadedChildWatcher is only available in 3.8+")
+def test_setup_loop_non_main_thread() -> None:
+    child_exc = None
+
+    def target() -> None:
+        try:
+            with loop_context() as loop:
+                assert asyncio.get_event_loop_policy().get_event_loop() is loop
+                loop.run_until_complete(test_subprocess_co(loop))
+        except Exception as exc:
+            nonlocal child_exc
+            child_exc = exc
+
+    # Ensures setup_test_loop can be called by pytest-xdist in non-main thread.
+    t = threading.Thread(target=target)
+    t.start()
+    t.join()
+
+    assert child_exc is None
