441c10eaf7ac9ded20aed376715b53dbac9cb3f1
Andrew Svetlov <andrew.svetlov@gmail.com>
2021-10-29 19:20:20 +0300

    http-parser -> llhttp (#6144)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Dmitry Erlikh <derlih@gmail.com>

--- tests/test_http_parser.py
+++ tests/test_http_parser.py
@@ -2,7 +2,7 @@
 # Tests for aiohttp/protocol.py
 
 import asyncio
-from typing import Any
+from typing import Any, List
 from unittest import mock
 from urllib.parse import quote
 
@@ -13,6 +13,7 @@ from yarl import URL
 import aiohttp
 from aiohttp import http_exceptions, streams
 from aiohttp.http_parser import (
+    NO_EXTENSIONS,
     DeflateBuffer,
     HttpPayloadParser,
     HttpRequestParserPy,
@@ -42,7 +43,14 @@ def protocol():
     return mock.Mock()
 
 
-@pytest.fixture(params=REQUEST_PARSERS)
+def _gen_ids(parsers: List[Any]) -> List[str]:
+    return [
+        "py-parser" if parser.__module__ == "aiohttp.http_parser" else "c-parser"
+        for parser in parsers
+    ]
+
+
+@pytest.fixture(params=REQUEST_PARSERS, ids=_gen_ids(REQUEST_PARSERS))
 def parser(loop: Any, protocol: Any, request: Any):
     # Parser implementations
     return request.param(
@@ -55,13 +63,13 @@ def parser(loop: Any, protocol: Any, request: Any):
     )
 
 
-@pytest.fixture(params=REQUEST_PARSERS)
+@pytest.fixture(params=REQUEST_PARSERS, ids=_gen_ids(REQUEST_PARSERS))
 def request_cls(request: Any):
     # Request Parser class
     return request.param
 
 
-@pytest.fixture(params=RESPONSE_PARSERS)
+@pytest.fixture(params=RESPONSE_PARSERS, ids=_gen_ids(RESPONSE_PARSERS))
 def response(loop: Any, protocol: Any, request: Any):
     # Parser implementations
     return request.param(
@@ -74,7 +82,7 @@ def response(loop: Any, protocol: Any, request: Any):
     )
 
 
-@pytest.fixture(params=RESPONSE_PARSERS)
+@pytest.fixture(params=RESPONSE_PARSERS, ids=_gen_ids(RESPONSE_PARSERS))
 def response_cls(request: Any):
     # Parser implementations
     return request.param
@@ -83,6 +91,14 @@ def response_cls(request: Any):
 @pytest.fixture
 def stream():
     return mock.Mock()
+
+
+@pytest.mark.skipif(NO_EXTENSIONS, reason="Extentions available but not imported")
+def test_c_parser_loaded():
+    assert "HttpRequestParserC" in dir(aiohttp.http_parser)
+    assert "HttpResponseParserC" in dir(aiohttp.http_parser)
+    assert "RawRequestMessageC" in dir(aiohttp.http_parser)
+    assert "RawResponseMessageC" in dir(aiohttp.http_parser)
 
 
 def test_parse_headers(parser: Any) -> None:
