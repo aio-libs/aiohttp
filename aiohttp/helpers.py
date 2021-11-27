1c9aba92447b433cbeb3a1f01d64e8e3dfbdd2da
Andrew Svetlov <andrew.svetlov@gmail.com>
2021-11-27 12:02:36 +0200

    Fix typing

--- aiohttp/helpers.py
+++ aiohttp/helpers.py
@@ -47,7 +47,7 @@ from urllib.request import getproxies, proxy_bypass
 
 import async_timeout
 import attr
-from multidict import MultiDict, MultiDictProxy
+from multidict import MultiDict, MultiDictProxy, MultiMapping
 from yarl import URL
 
 from . import hdrs
@@ -747,11 +747,13 @@ class HeadersMixin:
 
     ATTRS = frozenset(["_content_type", "_content_dict", "_stored_content_type"])
 
+    _headers: MultiMapping[str]
+
     _content_type = None  # type: Optional[str]
     _content_dict = None  # type: Optional[Dict[str, str]]
-    _stored_content_type = sentinel
+    _stored_content_type: Union[str, None, _SENTINEL] = sentinel
 
-    def _parse_content_type(self, raw: str) -> None:
+    def _parse_content_type(self, raw: Optional[str]) -> None:
         self._stored_content_type = raw
         if raw is None:
             # default value according to RFC 2616
@@ -763,7 +765,7 @@ class HeadersMixin:
     @property
     def content_type(self) -> str:
         """The value of content part for Content-Type HTTP header."""
-        raw = self._headers.get(hdrs.CONTENT_TYPE)  # type: ignore[attr-defined]
+        raw = self._headers.get(hdrs.CONTENT_TYPE)
         if self._stored_content_type != raw:
             self._parse_content_type(raw)
         return self._content_type  # type: ignore[return-value]
@@ -771,7 +773,7 @@ class HeadersMixin:
     @property
     def charset(self) -> Optional[str]:
         """The value of charset part for Content-Type HTTP header."""
-        raw = self._headers.get(hdrs.CONTENT_TYPE)  # type: ignore[attr-defined]
+        raw = self._headers.get(hdrs.CONTENT_TYPE)
         if self._stored_content_type != raw:
             self._parse_content_type(raw)
         return self._content_dict.get("charset")  # type: ignore[union-attr]
@@ -779,9 +781,7 @@ class HeadersMixin:
     @property
     def content_length(self) -> Optional[int]:
         """The value of Content-Length HTTP header."""
-        content_length = self._headers.get(  # type: ignore[attr-defined]
-            hdrs.CONTENT_LENGTH
-        )
+        content_length = self._headers.get(hdrs.CONTENT_LENGTH)
 
         if content_length is not None:
             return int(content_length)
