875a5822df9c30def84cc3f64a08ccdcc82b5ad5
Sam Bull <aa6bs0@sambull.org>
2021-12-23 12:52:50 +0000

    Update web_urldispatcher.py

--- aiohttp/web_urldispatcher.py
+++ aiohttp/web_urldispatcher.py
@@ -19,6 +19,7 @@ from typing import (
     Iterator,
     List,
     Mapping,
+    NoReturn,
     Optional,
     Pattern,
     Set,
@@ -928,10 +929,8 @@ class View(AbstractView):
     async def _iter(self) -> StreamResponse:
         if self.request.method not in hdrs.METH_ALL:
             self._raise_allowed_methods()
-        method: Callable[[], Awaitable[StreamResponse]]
-        method = getattr(  # type: ignore[assignment]
-            self, self.request.method.lower(), None
-        )
+        method: Optional[Callable[[], Awaitable[StreamResponse]]]
+        method = getattr(self, self.request.method.lower(), None)
         if method is None:
             self._raise_allowed_methods()
         resp = await method()
@@ -940,7 +939,7 @@ class View(AbstractView):
     def __await__(self) -> Generator[Any, None, StreamResponse]:
         return self._iter().__await__()
 
-    def _raise_allowed_methods(self) -> None:
+    def _raise_allowed_methods(self) -> NoReturn:
         allowed_methods = {m for m in hdrs.METH_ALL if hasattr(self, m.lower())}
         raise HTTPMethodNotAllowed(self.request.method, allowed_methods)
 
