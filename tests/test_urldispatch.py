9a70f6fa5c51c6bed2dc8774ab2e9e9e3dc8cbbe
Alexander Malev <malev@somedev.ru>
2021-12-29 01:14:27 +0300

    add tests

--- tests/test_urldispatch.py
+++ tests/test_urldispatch.py
@@ -19,6 +19,7 @@ from aiohttp.web_urldispatcher import (
     AbstractResource,
     Domain,
     DynamicResource,
+    GroupPlainResource,
     MaskDomain,
     PlainResource,
     ResourceRoute,
@@ -1292,3 +1293,21 @@ async def test_prefixed_subapp_root_route(app: Any) -> None:
     assert match_info.route.handler is handler
     match_info = await app.router.resolve(make_mocked_request("GET", "/s"))
     assert "<MatchInfoError 404: Not Found>" == repr(match_info)
+
+
+def test_group_plain_resource():
+    grp = GroupPlainResource()
+    assert "<GroupPlainResource count=0>" == repr(grp)
+    assert grp.canonical.startswith('group_')
+    resource = grp.add_resource('/')
+    assert grp.resources() == [resource]
+    assert grp._match('/') is not None
+    assert grp._match('/no') is None
+    assert "<GroupPlainResource count=1>" == repr(grp)
+
+
+def test_append_routes_in_one_resource(app: Any):
+    handler = make_handler()
+    app.router.add_get('/a/{param}/', handler)
+    app.router.add_put('/a/{param}/', handler)
+    assert len(list(app.router.resources())) == 1
