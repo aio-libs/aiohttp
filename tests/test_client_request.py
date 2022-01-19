165b996316290cc9ca9ab32a3be2cd7288288c90
Andrew Svetlov <andrew.svetlov@gmail.com>
2022-01-19 18:05:49 +0200

    Add HTTP method validation

--- tests/test_client_request.py
+++ tests/test_client_request.py
@@ -88,6 +88,11 @@ def test_method3(make_request: Any) -> None:
     assert req.method == "HEAD"
 
 
+def test_method3_invalid(make_request: Any) -> None:
+    with pytest.raises(ValueError):
+        make_request("METHOD WITH\nWHITESPACES", "http://python.org/")
+
+
 def test_version_1_0(make_request: Any) -> None:
     req = make_request("get", "http://python.org/", version="1.0")
     assert req.version == (1, 0)
