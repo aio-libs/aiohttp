a350ced7c7888acbde7db6ab59be5711f8b50f8d
Sam Bull <aa6bs0@sambull.org>
2022-02-10 20:04:39 +0000

    Update test_imports.py

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -12,7 +12,8 @@ def test___all__(pytester: pytest.Pytester) -> None:
         """
     )
     result = pytester.runpytest("-vv")
-    assert not result.errlines
+    if sys.platform == "win32":
+        assert not result.outlines
     result.assert_outcomes(passed=0, errors=0)
 
 
