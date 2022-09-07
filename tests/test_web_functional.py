b530bc94bdb7d2579815b0d2ec902ae53ac0b6ed
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2022-09-08 01:59:38 +0200

    Release all the `BytesIO` file handles in tests
    (cherry picked from commits dd11075a2beb07acd631cdbe9f77ca94c5ddaf14
    and d859a7dbb42bb9075dbc449dd5fa162262492236)
    
    (cherry picked from commit 54a4dce452a44bfca532a31eec88151c52bf2760)

--- tests/test_web_functional.py
+++ tests/test_web_functional.py
@@ -1568,15 +1568,17 @@ async def test_post_max_client_size(aiohttp_client) -> None:
     app.router.add_post("/", handler)
     client = await aiohttp_client(app)
 
-    data = {"long_string": 1024 * "x", "file": io.BytesIO(b"test")}
-    resp = await client.post("/", data=data)
+    with io.BytesIO(b"test") as file_handle:
+        data = {"long_string": 1024 * "x", "file": file_handle}
+        resp = await client.post("/", data=data)
 
-    assert 413 == resp.status
-    resp_text = await resp.text()
-    assert (
-        "Maximum request body size 10 exceeded, " "actual body size 1024" in resp_text
-    )
-    data["file"].close()
+        assert 413 == resp.status
+        resp_text = await resp.text()
+        assert (
+            "Maximum request body size 10 exceeded, "
+            "actual body size 1024" in resp_text
+        )
+        data["file"].close()
 
 
 async def test_post_max_client_size_for_file(aiohttp_client) -> None:
@@ -1588,8 +1590,9 @@ async def test_post_max_client_size_for_file(aiohttp_client) -> None:
     app.router.add_post("/", handler)
     client = await aiohttp_client(app)
 
-    data = {"file": io.BytesIO(b"test")}
-    resp = await client.post("/", data=data)
+    with io.BytesIO(b"test") as file_handle:
+        data = {"file": file_handle}
+        resp = await client.post("/", data=data)
 
     assert 413 == resp.status
 
@@ -1604,15 +1607,16 @@ async def test_response_with_bodypart(aiohttp_client) -> None:
     app.router.add_post("/", handler)
     client = await aiohttp_client(app)
 
-    data = {"file": io.BytesIO(b"test")}
-    resp = await client.post("/", data=data)
+    with io.BytesIO(b"test") as file_handle:
+        data = {"file": file_handle}
+        resp = await client.post("/", data=data)
 
-    assert 200 == resp.status
-    body = await resp.read()
-    assert body == b"test"
+        assert 200 == resp.status
+        body = await resp.read()
+        assert body == b"test"
 
-    disp = multipart.parse_content_disposition(resp.headers["content-disposition"])
-    assert disp == ("attachment", {"name": "file", "filename": "file"})
+        disp = multipart.parse_content_disposition(resp.headers["content-disposition"])
+        assert disp == ("attachment", {"name": "file", "filename": "file"})
 
 
 async def test_response_with_bodypart_named(aiohttp_client, tmpdir) -> None:
