3daa2c4216a2c13c39e5f7d1cefb704f85300f65
Ivan Lakovic <laky55555@gmail.com>
2022-10-20 23:00:36 +0100

    Remove unused monkeypatch parameter

--- tests/test_connector.py
+++ tests/test_connector.py
@@ -2188,7 +2188,7 @@ class TestDNSCacheTable:
         monkeypatch.setattr("aiohttp.connector.monotonic", lambda: 3)
         assert dns_cache_table.expired("localhost")
 
-    def test_never_expire(self, monkeypatch: pytest.MonkeyPatch) -> None:
+    def test_never_expire(self) -> None:
         dns_cache_table = _DNSCacheTable(ttl=None)
         dns_cache_table.add("localhost", ["127.0.0.1"])
         assert not dns_cache_table.expired("localhost")
