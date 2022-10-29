f5ae2a3ba0568c152ede03bb72e35c8c4d4aa555
Sam Bull <aa6bs0@sambull.org>
2022-10-29 17:03:42 +0100

    Update test_imports.py

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,3 +1,4 @@
+import os
 import platform
 import sys
 
@@ -37,6 +38,7 @@ def test_import_time(pytester: pytest.Pytester) -> None:
     from time to time, but this should provide an early warning if something is
     added that significantly increases import time.
     """
+    os.environ["PYTHONPATH"] = "aiohttp/"
     r = pytester.run(sys.executable, "-We", "-c", "import aiohttp", timeout=0.45)
 
     assert not r.stdout.str()
