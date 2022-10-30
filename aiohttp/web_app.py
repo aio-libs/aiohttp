4ee0570ae36a9917b509fee70b6bfa26cd994426
Sam Bull <aa6bs0@sambull.org>
2022-10-30 16:41:03 +0000

    Update web_app.py

--- aiohttp/web_app.py
+++ aiohttp/web_app.py
@@ -374,21 +374,25 @@ class Application(MutableMapping[Union[str, AppKey[Any]], Any]):
         match_info.add_app(self)
         match_info.freeze()
 
+        resp = None
         request._match_info = match_info
         expect = request.headers.get(hdrs.EXPECT)
         if expect:
-            await match_info.expect_handler(request)
+            resp = await match_info.expect_handler(request)
             await request.writer.drain()
 
-        handler = match_info.handler
+        if resp is None:
+            handler = match_info.handler
 
-        if self._run_middlewares:
-            for app in match_info.apps[::-1]:
-                assert app.pre_frozen, "middleware handlers are not ready"
-                for m in app._middlewares_handlers:
-                    handler = update_wrapper(partial(m, handler=handler), handler)
+            if self._run_middlewares:
+                for app in match_info.apps[::-1]:
+                    assert app.pre_frozen, "middleware handlers are not ready"
+                    for m in app._middlewares_handlers:
+                        handler = update_wrapper(partial(m, handler=handler), handler)
 
-        return await handler(request)
+            resp = await handler(request)
+
+        return resp
 
     def __call__(self) -> "Application":
         """gunicorn compatibility"""
