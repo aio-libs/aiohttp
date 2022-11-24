75bdecb847f172b9ff3b98d0800c55d51ffda53a
Sam Bull <aa6bs0@sambull.org>
2022-11-24 19:14:52 +0000

    Update test_imports.py

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -59,4 +59,4 @@ def test_import_time(pytester: pytest.Pytester) -> None:
         else:
             os.environ["PYTHONPATH"] = old_path
 
-    assert best_time_ms < 160
+    assert best_time_ms < 200
