5409871e9d3c463de63a47a7a99eb966699cd290
Sam Bull <aa6bs0@sambull.org>
2023-02-28 18:21:11 +0000

    Update test_cookiejar.py

--- tests/test_cookiejar.py
+++ tests/test_cookiejar.py
@@ -773,7 +773,7 @@ async def test_cookie_jar_clear_domain():
 
 
 @pytest.mark.skipif(sys.version_info < (3, 8), reason="Requires latest protocol")
-def test_pickle_format(cookies_to_send) -> None:
+async def test_pickle_format(cookies_to_send) -> None:
     """Test if cookiejar pickle format breaks.
 
     If this test fails, it may indicate that saved cookiejars will stop working.
