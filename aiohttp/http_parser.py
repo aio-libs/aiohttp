3ff81dc9c9ce20efd3bf54cf52adaf438c483a92
Mykola Mokhnach <mokhnach@gmail.com>
2023-03-07 20:56:51 +0100

    refactor: Extract zlib-related logic into a single module (#7223)
    <!-- Thank you for your contribution! -->
    
    ## What do these changes do?
    
    Addresses issue https://github.com/aio-libs/aiohttp/issues/7192
    Refactors the logic to have the zlib-related stuff concentrated into a
    single module
    
    ## Are there changes in behavior for the user?
    
    No
    
    ## Related issue number
    
    https://github.com/aio-libs/aiohttp/issues/7192
    
    ## Checklist
    
    - [x] I think the code is well written
    - [x] Unit tests for the changes exist
    - [ ] Documentation reflects the changes
    - [ ] If you provide code modification, please add yourself to
    `CONTRIBUTORS.txt`
      * The format is &lt;Name&gt; &lt;Surname&gt;.
      * Please keep alphabetical order, the file is sorted by names.
    - [ ] Add a new news fragment into the `CHANGES` folder
      * name it `<issue_id>.<type>` for example (588.bugfix)
    * if you don't have an `issue_id` change it to the pr id after creating
    the pr
      * ensure type is one of the following:
        * `.feature`: Signifying a new feature.
        * `.bugfix`: Signifying a bug fix.
        * `.doc`: Signifying a documentation improvement.
        * `.removal`: Signifying a deprecation or removal of public API.
    * `.misc`: A ticket has been closed, but it is not of interest to users.
    * Make sure to use full sentences with correct case and punctuation, for
    example: "Fix issue with non-ascii contents in doctest text files."
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Sam Bull <aa6bs0@sambull.org>

--- aiohttp/http_parser.py
+++ aiohttp/http_parser.py
@@ -3,11 +3,9 @@ import asyncio
 import collections
 import re
 import string
-import zlib
 from contextlib import suppress
 from enum import IntEnum
 from typing import (
-    Any,
     Generic,
     List,
     NamedTuple,
@@ -18,7 +16,6 @@ from typing import (
     Type,
     TypeVar,
     Union,
-    cast,
 )
 
 from multidict import CIMultiDict, CIMultiDictProxy, istr
@@ -27,6 +24,7 @@ from yarl import URL
 
 from . import hdrs
 from .base_protocol import BaseProtocol
+from .compression_utils import HAS_BROTLI, BrotliDecompressor, ZLibDecompressor
 from .helpers import NO_EXTENSIONS, BaseTimerContext
 from .http_exceptions import (
     BadHttpMessage,
@@ -41,14 +39,6 @@ from .http_writer import HttpVersion, HttpVersion10
 from .log import internal_logger
 from .streams import EMPTY_PAYLOAD, StreamReader
 from .typedefs import RawHeaders
-
-try:
-    import brotli
-
-    HAS_BROTLI = True
-except ImportError:  # pragma: no cover
-    HAS_BROTLI = False
-
 
 __all__ = (
     "HeadersParser",
@@ -859,34 +849,16 @@ class DeflateBuffer:
         self.encoding = encoding
         self._started_decoding = False
 
+        self.decompressor: Union[BrotliDecompressor, ZLibDecompressor]
         if encoding == "br":
             if not HAS_BROTLI:  # pragma: no cover
                 raise ContentEncodingError(
                     "Can not decode content-encoding: brotli (br). "
                     "Please install `Brotli`"
                 )
-
-            class BrotliDecoder:
-                # Supports both 'brotlipy' and 'Brotli' packages
-                # since they share an import name. The top branches
-                # are for 'brotlipy' and bottom branches for 'Brotli'
-                def __init__(self) -> None:
-                    self._obj = brotli.Decompressor()
-
-                def decompress(self, data: bytes) -> bytes:
-                    if hasattr(self._obj, "decompress"):
-                        return cast(bytes, self._obj.decompress(data))
-                    return cast(bytes, self._obj.process(data))
-
-                def flush(self) -> bytes:
-                    if hasattr(self._obj, "flush"):
-                        return cast(bytes, self._obj.flush())
-                    return b""
-
-            self.decompressor: Any = BrotliDecoder()
+            self.decompressor = BrotliDecompressor()
         else:
-            zlib_mode = 16 + zlib.MAX_WBITS if encoding == "gzip" else zlib.MAX_WBITS
-            self.decompressor = zlib.decompressobj(wbits=zlib_mode)
+            self.decompressor = ZLibDecompressor(encoding=encoding)
 
     def set_exception(self, exc: BaseException) -> None:
         self.out.set_exception(exc)
@@ -907,10 +879,12 @@ class DeflateBuffer:
         ):
             # Change the decoder to decompress incorrectly compressed data
             # Actually we should issue a warning about non-RFC-compliant data.
-            self.decompressor = zlib.decompressobj(wbits=-zlib.MAX_WBITS)
+            self.decompressor = ZLibDecompressor(
+                encoding=self.encoding, suppress_deflate_header=True
+            )
 
         try:
-            chunk = self.decompressor.decompress(chunk)
+            chunk = self.decompressor.decompress_sync(chunk)
         except Exception:
             raise ContentEncodingError(
                 "Can not decode content-encoding: %s" % self.encoding
@@ -926,7 +900,7 @@ class DeflateBuffer:
 
         if chunk or self.size > 0:
             self.out.feed_data(chunk, len(chunk))
-            if self.encoding == "deflate" and not self.decompressor.eof:
+            if self.encoding == "deflate" and not self.decompressor.eof:  # type: ignore
                 raise ContentEncodingError("deflate")
 
         self.out.feed_eof()
