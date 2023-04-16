12fdaac7cc21eef468770f5b1781172bbe56d3b5
J. Nick Koston <nick@koston.org>
2023-04-16 05:41:59 -1000

    Reduce access log overhead when logging is disabled (#7240)
    <!-- Thank you for your contribution! -->
    
    ## What do these changes do?
    
    If the logger was disabled the log message would be formatted and thrown
    away. Avoid formatting the log message when logging is not enabled at
    info level.
    
    In testing this represented up to 12% of the run time of serving a
    static file.
    
    ## Are there changes in behavior for the user?
    
    No
    
    ## Related issue number
    
    <!-- Are there any issues opened that will be resolved by merging this
    change? -->
    
    ## Checklist
    
    - [x] I think the code is well written
    - [x] Unit tests for the changes exist
    - [ ] Documentation reflects the changes
    - [ ] If you provide code modification, please add yourself to
    `CONTRIBUTORS.txt`
      * The format is &lt;Name&gt; &lt;Surname&gt;.
      * Please keep alphabetical order, the file is sorted by names.
    - [x] Add a new news fragment into the `CHANGES` folder
      * name it `<issue_id>.<type>` for example (588.bugfix)
    * if you don't have an `issue_id` change it to the pr id after creating
    the pr
      * ensure type is one of the following:
        * `.feature`: Signifying a new feature.
        * `.bugfix`: Signifying a bug fix.
        * `.doc`: Signifying a documentation improvement.
        * `.removal`: Signifying a deprecation or removal of public API.
    * `.misc`: A ticket has been closed, but it is not of interest to users.
    * Make sure to use full sentences with correct case and punctuation, for
    example: "Fix issue with non-ascii contents in doctest text files."
    
    ---------
    
    Co-authored-by: Sviatoslav Sydorenko <wk.cvs.github@sydorenko.org.ua>
    Co-authored-by: Sam Bull <aa6bs0@sambull.org>

--- tests/test_web_log.py
+++ tests/test_web_log.py
@@ -1,5 +1,6 @@
 # type: ignore
 import datetime
+import logging
 import platform
 import sys
 from typing import Any
@@ -251,3 +252,14 @@ async def test_contextvars_logger(aiohttp_server: Any, aiohttp_client: Any):
     resp = await client.get("/")
     assert 200 == resp.status
     assert msg == "contextvars: uuid"
+
+
+def test_logger_does_nothing_when_disabled(caplog: pytest.LogCaptureFixture) -> None:
+    """Test that the logger does nothing when the log level is disabled."""
+    mock_logger = logging.getLogger("test.aiohttp.log")
+    mock_logger.setLevel(logging.INFO)
+    access_logger = AccessLogger(mock_logger, "%b")
+    access_logger.log(
+        mock.Mock(name="mock_request"), mock.Mock(name="mock_response"), 42
+    )
+    assert "mock_response" in caplog.text
