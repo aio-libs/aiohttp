0755fed5fdbe88047e4ae2c9f84649ff571b0de6
Sam Bull <git@sambull.org>
2023-07-02 16:19:01 +0100

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -179,7 +179,8 @@ def unix_sockname(tmp_path: Any, tmp_path_factory: Any):
 
 @pytest.fixture
 def selector_loop() -> None:
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    policy = asyncio.WindowsSelectorEventLoopPolicy()
+    asyncio.set_event_loop_policy(policy)
 
     with loop_context(policy.new_event_loop) as _loop:
         asyncio.set_event_loop(_loop)
