cb7e1e5d4e2e93e1690f9250ccd50d0164eb72bb
Jan Gosmann <jan@hyper-world.de>
2023-07-22 15:42:25 +0200

    Fix PermissionError when loading .netrc (#7237) (#7378) (#7395)
    ## What do these changes do?
    
    If no NETRC environment variable is provided and the .netrc path cannot
    be accessed due to missing permission, a PermissionError was raised
    instead of returning None. See issue #7237. This PR fixes the issue.
    
    If the changes look good, I can also prepare backports.
    
    ## Are there changes in behavior for the user?
    
    If the .netrc cannot be accessed due to a permission problem (and the
    `NETRC` environment variable is unset), no `PermissionError` will be
    raised. Instead it will be silently ignored.
    
    ## Related issue number
    
    Fixes #7237
    
    Backport of #7378
    
    (cherry picked from commit 4d84a5061396af966fcd380a71e64b6faf576edf)
    
    ## Checklist
    
    - [x] I think the code is well written
    - [x] Unit tests for the changes exist
    - [x] Documentation reflects the changes
    - [x] If you provide code modification, please add yourself to
    `CONTRIBUTORS.txt`
      * The format is &lt;Name&gt; &lt;Surname&gt;.
      * Please keep alphabetical order, the file is sorted by names.
    - [x] Add a new news fragment into the `CHANGES` folder
      * name it `<issue_id>.<type>` for example (588.bugfix)
    * if you don't have an `issue_id` change it to the pr id after creating
    the pr
      * ensure type is one of the following:
        * `.feature`: Signifying a new feature.
        * `.bugfix`: Signifying a bug fix.
        * `.doc`: Signifying a documentation improvement.
        * `.removal`: Signifying a deprecation or removal of public API.
    * `.misc`: A ticket has been closed, but it is not of interest to users.
    * Make sure to use full sentences with correct case and punctuation, for
    example: "Fix issue with non-ascii contents in doctest text files."

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -5,6 +5,7 @@ import gc
 import platform
 import tempfile
 from math import isclose, modf
+from pathlib import Path
 from unittest import mock
 from urllib.request import getproxies_environment
 
@@ -178,7 +179,6 @@ def test_basic_auth_from_not_url() -> None:
 
 
 class ReifyMixin:
-
     reify = NotImplemented
 
     def test_reify(self) -> None:
@@ -763,3 +763,23 @@ class TestChainMapProxy:
 )
 def test_parse_http_date(value, expected):
     assert parse_http_date(value) == expected
+
+
+@pytest.fixture
+def protected_dir(tmp_path: Path):
+    protected_dir = tmp_path / "protected"
+    protected_dir.mkdir()
+    try:
+        protected_dir.chmod(0o600)
+        yield protected_dir
+    finally:
+        protected_dir.rmdir()
+
+
+def test_netrc_from_home_does_not_raise_if_access_denied(
+    protected_dir: Path, monkeypatch: pytest.MonkeyPatch
+):
+    monkeypatch.setattr(Path, "home", lambda: protected_dir)
+    monkeypatch.delenv("NETRC", raising=False)
+
+    helpers.netrc_from_env()
