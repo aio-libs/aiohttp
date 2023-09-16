abd5eebc18b81a50572c6399da7cd92b6486c28f
Michał Górny <mgorny@gentoo.org>
2023-09-16 18:49:39 +0200

    Support brotlicffi alternatively to brotli
    Support the brotlicffi implementation as an alternative to brotli.
    This is necessary since the current version of brotli (as of 1.1.0)
    does not work on PyPy.  This uses the approach of preferring brotlicffi
    over brotli in imports, and listing brotli or brotlicffi depending
    on the Python implementation in dependencies, as recommended
    in brotlicffi documentation:
    https://pypi.org/project/brotlicffi/1.1.0.0/#using-brotlicffi-in-projects

--- tests/test_web_functional.py
+++ tests/test_web_functional.py
@@ -8,7 +8,6 @@ import zlib
 from typing import Any, Optional
 from unittest import mock
 
-import brotli
 import pytest
 from multidict import CIMultiDictProxy, MultiDict
 from yarl import URL
@@ -18,6 +17,11 @@ from aiohttp import FormData, HttpVersion10, HttpVersion11, TraceConfig, multipa
 from aiohttp.hdrs import CONTENT_LENGTH, CONTENT_TYPE, TRANSFER_ENCODING
 from aiohttp.test_utils import make_mocked_coro
 from aiohttp.typedefs import Handler
+
+try:
+    import brotlicffi as brotli
+except ImportError:
+    import brotli
 
 try:
     import ssl
