ba39f05db3eed72665117b8dd646e716b9ea61e1
Ben Kallus <benjamin.p.kallus.gr@dartmouth.edu>
2023-10-17 14:37:36 -0400

    remove unused exception

--- tests/test_http_parser.py
+++ tests/test_http_parser.py
@@ -664,7 +664,7 @@ def test_http_request_bad_status_line(parser: Any) -> None:
 
 def test_http_request_bad_status_line_whitespace(parser: Any) -> None:
     text = b"GET\npathn\nHTTP/1.1\r\n\r\n"
-    with pytest.raises(http_exceptions.BadStatusLine) as exc_info:
+    with pytest.raises(http_exceptions.BadStatusLine):
         parser.feed_data(text)
 
 
