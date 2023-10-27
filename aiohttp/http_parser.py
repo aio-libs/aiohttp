434f39c5f5bba457b53ac87d86fb59688893d6f9
J. Nick Koston <nick@koston.org>
2023-10-27 16:33:34 -0500

    keep it the same as much as possible

--- aiohttp/http_parser.py
+++ aiohttp/http_parser.py
@@ -350,7 +350,8 @@ class HttpParser(abc.ABC, Generic[_MsgT]):
                         )
                         if not code_indicates_empty_body and (
                             (length is not None and length > 0)
-                            or (msg.chunked and not msg.upgrade)
+                            or msg.chunked
+                            and not msg.upgrade
                         ):
                             payload = StreamReader(
                                 self.protocol,
