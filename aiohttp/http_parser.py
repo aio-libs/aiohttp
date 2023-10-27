6cae8460fe620ba1d02f519c267e30b8c708d7ce
J. Nick Koston <nick@koston.org>
2023-10-27 15:16:48 -0500

    adjust

--- aiohttp/http_parser.py
+++ aiohttp/http_parser.py
@@ -350,10 +350,8 @@ class HttpParser(abc.ABC, Generic[_MsgT]):
                         )
                         if (
                             not code_indicates_empty_body
-                            and (length is not None and length > 0)
-                            or msg.chunked
-                            and not msg.upgrade
-                        ):
+                            and ((length is not None and length > 0) or msg.chunked)
+                        ) and not msg.upgrade:
                             payload = StreamReader(
                                 self.protocol,
                                 timer=self.timer,
