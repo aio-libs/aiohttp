a2e4976e9fd1f10ba6b0b80f06bf77c613a870fa
J. Nick Koston <nick@koston.org>
2023-10-28 22:10:11 -0700

    explict test for 101/204/304

--- tests/test_web_response.py
+++ tests/test_web_response.py
@@ -637,11 +637,11 @@ async def test_rm_transfer_encoding_rfc_9112_6_3_http_11(status: int) -> None:
     assert hdrs.TRANSFER_ENCODING not in resp.headers
 
 
-@pytest.mark.parametrize("status", (204, 304))
-async def test_rm_content_length_204_304_responses(status: int) -> None:
-    """Remove content length for 204 and 304 responses.
+@pytest.mark.parametrize("status", (101, 204, 304))
+async def test_rm_content_length_101_204_304_responses(status: int) -> None:
+    """Remove content length for 101, 204, and 304 responses.
 
-    Content-Length is forbidden for 204
+    Content-Length is forbidden for 1xx and 204
     https://datatracker.ietf.org/doc/html/rfc7230#section-3.3.2
 
     Content-Length is discouraged for 304.
