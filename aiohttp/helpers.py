8e75ea63ae3ace53611d1dbf86b25ce67d76ebf0
Sam Bull <git@sambull.org>
2023-10-30 16:57:31 +0000

    Update helpers.py

--- aiohttp/helpers.py
+++ aiohttp/helpers.py
@@ -1074,6 +1074,5 @@ def method_must_be_empty_body(method: str) -> bool:
 
 def status_code_must_be_empty_body(code: int) -> bool:
     """Check if a status code must return an empty body."""
-    # 204, 304, 1xx should not have a body per
     # https://datatracker.ietf.org/doc/html/rfc9112#section-6.3-2.1
     return code in {204, 304} or 100 <= code < 200
