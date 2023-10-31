7a09009bb2e35b44c01879ce7401417a56c9668e
Sam Bull <git@sambull.org>
2023-10-31 14:59:32 +0000

    Update spec links

--- aiohttp/helpers.py
+++ aiohttp/helpers.py
@@ -1067,7 +1067,6 @@ def parse_http_date(date_str: Optional[str]) -> Optional[datetime.datetime]:
 
 def must_be_empty_body(method: str, code: int) -> bool:
     """Check if a request must return an empty body."""
-    # https://datatracker.ietf.org/doc/html/rfc9112#section-6.3
     return status_code_must_be_empty_body(code) or method_must_be_empty_body(method)
 
 
@@ -1089,10 +1088,6 @@ def should_remove_content_length(method: str, code: int) -> bool:
 
     This should always be a subset of must_be_empty_body
     """
-    # forbidden on 1xx/204/CONNECT per:
-    # https://datatracker.ietf.org/doc/html/rfc7230#section-3.3.2
-    # discouraged on 304 per
-    # https://datatracker.ietf.org/doc/html/rfc7232#section-4.1
-    # HEAD requests should still include the content-length header
-    # per https://datatracker.ietf.org/doc/html/rfc2616#section-14.13
+    # https://www.rfc-editor.org/rfc/rfc9110.html#section-8.6-7
+    # https://www.rfc-editor.org/rfc/rfc9110.html#section-8.6-8
     return code in (204, 304) or 100 <= code < 200 or method == hdrs.METH_CONNECT
