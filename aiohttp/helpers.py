5100372ed71248b84fa6b9f0953c818bc01724c9
J. Nick Koston <nick@koston.org>
2023-11-02 14:58:15 -0500

    Ensure empty body response for 1xx/204/304 per RFC 9112 sec 6.3 (#7756)
    <!-- Thank you for your contribution! -->
    
    `Transfer-Encoding` and `Content-Length` will be removed from the server
    response when sending a 1xx (Informational), 204 (No Content), or 304
    (Not Modified) status since no body is expected for these status codes
    
    - `Content-Length` forbidden on 1xx/204 per
    https://datatracker.ietf.org/doc/html/rfc7230#section-3.3.2 and
    discouraged on 304 per
    https://datatracker.ietf.org/doc/html/rfc7232#section-4.1
    
    We need to ensure `Content-Length` is kept for `HEAD` to ensure
    keep-alive works as expected and is allowed per
    https://datatracker.ietf.org/doc/html/rfc2616#section-14.13
    
    - `Transfer-Encoding` removed per
    https://datatracker.ietf.org/doc/html/rfc9112#section-6.1
    > any recipient on the response chain (including the origin server) can
    remove transfer codings when they are not needed.
    
    `aiohttp` would incorrectly send an body of `0\r\n\r\n` when `chunked`
    was set for `Transfer-Encoding` with the above status code.
    
    This change ensures `aiohttp` does not send these invalid bodies.
    
    The `Transfer-Encoding` and `Content-Length` headers will be removed
    when sending a 1xx (Informational), 204 (No Content), or 304 (Not
    Modified) except for `HEAD` responses since these status since no body
    is expected for these status codes.
    
    An invalid body of `0\r\n\r\n` will no longer be sent with these status
    codes.
    
    <!-- Are there any issues opened that will be resolved by merging this
    change? -->
    
    - [x] I think the code is well written
    - [x] Unit tests for the changes exist
    - [ ] Documentation reflects the changes
    - [ ] If you provide code modification, please add yourself to
    `CONTRIBUTORS.txt`
      * The format is &lt;Name&gt; &lt;Surname&gt;.
      * Please keep alphabetical order, the file is sorted by names.
    - [x] Add a new news fragment into the `CHANGES` folder
      * name it `<issue_id>.<type>` for example (588.bugfix)
    * if you don't have an `issue_id` change it to the pr id after creating
    the pr
      * ensure type is one of the following:
        * `.feature`: Signifying a new feature.
        * `.bugfix`: Signifying a bug fix.
        * `.doc`: Signifying a documentation improvement.
        * `.removal`: Signifying a deprecation or removal of public API.
    * `.misc`: A ticket has been closed, but it is not of interest to users.
    * Make sure to use full sentences with correct case and punctuation, for
    example: "Fix issue with non-ascii contents in doctest text files."
    
    TODO:
    
    - [x] figure out why client closes connection on HEAD requests with
    content length
    - [x] figure out why client functional tests fail with
    AIOHTTP_NO_EXTENSIONS=1 `test_keepalive_after_empty_body_status` and
    `test_keepalive_after_empty_body_status_stream_response` -- They do not
    fail anymore after the fixed in
    https://github.com/aio-libs/aiohttp/pull/7755
    - [x] timeline
    - [x] more functional testing
    
    ---------
    
    Co-authored-by: Sam Bull <git@sambull.org>
    (cherry picked from commit f63cf18fec3e8e6608c7fe7bff9f46a0976f6c25)

--- aiohttp/helpers.py
+++ aiohttp/helpers.py
@@ -970,6 +970,15 @@ def parse_http_date(date_str: Optional[str]) -> Optional[datetime.datetime]:
     return None
 
 
+def must_be_empty_body(method: str, code: int) -> bool:
+    """Check if a request must return an empty body."""
+    return (
+        status_code_must_be_empty_body(code)
+        or method_must_be_empty_body(method)
+        or (200 <= code < 300 and method.upper() == hdrs.METH_CONNECT)
+    )
+
+
 def method_must_be_empty_body(method: str) -> bool:
     """Check if a method must return an empty body."""
     # https://datatracker.ietf.org/doc/html/rfc9112#section-6.3-2.1
@@ -981,3 +990,17 @@ def status_code_must_be_empty_body(code: int) -> bool:
     """Check if a status code must return an empty body."""
     # https://datatracker.ietf.org/doc/html/rfc9112#section-6.3-2.1
     return code in {204, 304} or 100 <= code < 200
+
+
+def should_remove_content_length(method: str, code: int) -> bool:
+    """Check if a Content-Length header should be removed.
+
+    This should always be a subset of must_be_empty_body
+    """
+    # https://www.rfc-editor.org/rfc/rfc9110.html#section-8.6-8
+    # https://www.rfc-editor.org/rfc/rfc9110.html#section-15.4.5-4
+    return (
+        code in {204, 304}
+        or 100 <= code < 200
+        or (200 <= code < 300 and method.upper() == hdrs.METH_CONNECT)
+    )
