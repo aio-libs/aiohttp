ba6e30b2cb4d45efe6e210d79d2a89c710a0d16c
J. Nick Koston <nick@koston.org>
2023-11-14 13:07:25 -0600

    fix resources registered at /

--- aiohttp/web_urldispatcher.py
+++ aiohttp/web_urldispatcher.py
@@ -1094,8 +1094,8 @@ class UrlDispatcher(AbstractRouter, Mapping[str, AbstractResource]):
         """Return a key to index the resource in the resource index."""
         canonical = resource.canonical
         if "{" in canonical:  # strip at the first { to allow for variables
-            return canonical.partition("{")[0].rstrip("/") or "/"
-        return canonical
+            return canonical.partition("{")[0].rstrip("/")
+        return canonical or "/"
 
     def index_resource(self, resource: AbstractResource) -> None:
         """Add a resource to the resource index."""
