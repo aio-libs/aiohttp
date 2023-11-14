de684c8ef1ccc5c25283c456d9adf0fa33c85d84
Rongrong <i@rong.moe>
2023-11-14 19:27:30 +0800

    Remove check after setting Windows-specific TIME_MAX

--- aiohttp/cookiejar.py
+++ aiohttp/cookiejar.py
@@ -66,8 +66,6 @@ class CookieJar(AbstractCookieJar):
         # Hit the maximum representable time on Windows
         # https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/localtime-localtime32-localtime64
         MAX_TIME = calendar.timegm((3000, 12, 31, 23, 59, 59, -1, -1, -1))
-        # The below line should never raise an exception
-        time.gmtime(MAX_TIME)
     except OverflowError:
         # #4515: datetime.max may not be representable on 32-bit platforms
         MAX_TIME = 2**31 - 1
