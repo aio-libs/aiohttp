cabbdc5345a1e3183564cb804572371d9613ab0e
Sam Bull <git@sambull.org>
2023-11-25 21:28:40 +0000

    Fix flaky websocket test

--- tests/test_client_ws_functional.py
+++ tests/test_client_ws_functional.py
@@ -8,6 +8,7 @@ import pytest
 import aiohttp
 from aiohttp import hdrs, web
 from aiohttp.client_ws import ClientWSTimeout
+from aiohttp.http import WSCloseCode
 
 if sys.version_info >= (3, 11):
     import asyncio as async_timeout
@@ -645,10 +646,9 @@ async def test_heartbeat_no_pong(aiohttp_client: Any) -> None:
     client = await aiohttp_client(app)
     resp = await client.ws_connect("/", heartbeat=0.05)
 
-    await resp.receive()
-    await resp.receive()
-
+    await asyncio.sleep(0.1)
     assert ping_received
+    assert resp.close_code is WSCloseCode.ABNORMAL_CLOSURE
 
 
 async def test_send_recv_compress(aiohttp_client: Any) -> None:
