566f3467bff9356efd257cbe817658676eb335f5
Sam Bull <git@sambull.org>
2023-11-26 15:00:28 +0000

    Fix regression with connection upgrade (#7879) (#7907)
    Fixes #7867.
    
    (cherry picked from commit 48b15583305e692ce997ec6f5a6a2f88f23ace71)

--- tests/test_client_functional.py
+++ tests/test_client_functional.py
@@ -173,6 +173,25 @@ async def test_keepalive_response_released(aiohttp_client: Any) -> None:
     assert 1 == len(client._session.connector._conns)
 
 
+async def test_upgrade_connection_not_released_after_read(aiohttp_client) -> None:
+    async def handler(request: web.Request) -> web.Response:
+        body = await request.read()
+        assert b"" == body
+        return web.Response(
+            status=101, headers={"Connection": "Upgrade", "Upgrade": "tcp"}
+        )
+
+    app = web.Application()
+    app.router.add_route("GET", "/", handler)
+
+    client = await aiohttp_client(app)
+
+    resp = await client.get("/")
+    await resp.read()
+    assert resp.connection is not None
+    assert not resp.closed
+
+
 async def test_keepalive_server_force_close_connection(aiohttp_client) -> None:
     async def handler(request):
         body = await request.read()
