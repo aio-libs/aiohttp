6950c06469c487b4478e39c369009422a4f997fd
J. Nick Koston <nick@koston.org>
2023-12-10 17:33:57 -1000

    Revert "Update tests/conftest.py"
    This reverts commit 71406c2391acbcec0cd3b87698704d4d287e0950.

--- tests/conftest.py
+++ tests/conftest.py
@@ -208,3 +208,11 @@ def netrc_contents(
     monkeypatch.setenv("NETRC", str(netrc_file_path))
 
     return netrc_file_path
+
+
+@pytest.fixture
+def start_connection():
+    with mock.patch(
+        "aiohttp.connector.aiohappyeyeballs.start_connection"
+    ) as start_connection_mock:
+        yield start_connection_mock
