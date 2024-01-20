64d82635c933d26a835253cdfea52d87b33fdc0a
Sam Bull <git@sambull.org>
2024-01-20 14:49:49 +0000

    Update web_protocol.py

--- aiohttp/web_protocol.py
+++ aiohttp/web_protocol.py
@@ -589,6 +589,9 @@ class RequestHandler(BaseProtocol):
             except Exception as exc:
                 self.log_exception("Unhandled exception", exc_info=exc)
                 self.force_close()
+            except BaseException:
+                self.force_close()
+                raise
             finally:
                 if self.transport is None and resp is not None:
                     self.log_debug("Ignored premature client disconnection.")
