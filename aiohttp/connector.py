34349453c6aedee44a5e635f9e77b21a365c462d
Sam Bull <git@sambull.org>
2024-01-28 22:15:44 +0000

    Merge 3.10

--- aiohttp/connector.py
+++ aiohttp/connector.py
@@ -757,7 +757,7 @@ class TCPConnector(BaseConnector):
         timeout_ceil_threshold: float = 5,
         happy_eyeballs_delay: Optional[float] = 0.25,
         interleave: Optional[int] = None,
-    ) -> None:
+    ):
         super().__init__(
             keepalive_timeout=keepalive_timeout,
             force_close=force_close,
