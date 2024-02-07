6e68cfeba3ebc5e9492cd2b501539b9e10717fd5
Alex Macdonald <alex@alexmac.cc>
2024-02-06 21:37:07 -0800

    PR feedback

--- tests/test_urldispatch.py
+++ tests/test_urldispatch.py
@@ -1146,14 +1146,16 @@ def test_subapp_iter(app: Any) -> None:
     assert list(resource) == [r1, r2]
 
 
-def test_invalid_route_name(router: Any) -> None:
+@pytest.mark.parametrize(
+    "route_name",
+    [
+        "invalid name",
+        "class",
+    ],
+)
+def test_invalid_route_name(router: Any, route_name: str) -> None:
     with pytest.raises(ValueError):
-        router.add_get("/", make_handler(), name="invalid name")
-
-
-def test_invalid_route_name_identifier(router) -> None:
-    with pytest.raises(ValueError):
-        router.add_get("/", make_handler(), name="class")  # identifier
+        router.add_get("/", make_handler(), name=route_name)
 
 
 def test_frozen_router(router: Any) -> None:
