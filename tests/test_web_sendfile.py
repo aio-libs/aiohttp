f70f58823c028e5034cfc3545cdd335bdc8b0b8f
Steve Repsher <steverep@users.noreply.github.com>
2024-02-14 07:34:25 -0500

    [3.10] Add server capability to check for Brotli compressed static files (#8063)
    Currently server only checks if static routes have a `.gz` extension and
    serves them with `gzip` encoding. These changes do the same for `.br`
    files with `br` encoding. Brotli is prioritized over gzip if both exist
    and are supported by the client, as it should almost always be a smaller
    content length.
    
    I considered making a check for which is smaller if both exist, but
    figured it wouldn't be worth the extra file system call in the vast
    majority of cases (at least not for typical web formats). Users should
    simply use gzip if it's smaller than Brotli for any file.
    
    Resolves #8062
    
    (cherry picked from commit dfc92967d10eb83a8d726c02c3de90da15f8335f)
    
    Co-authored-by: Steve Repsher <steverep@users.noreply.github.com>
    Co-authored-by: J. Nick Koston <nick@koston.org>
    Co-authored-by: Sviatoslav Sydorenko <sviat@redhat.com>

--- tests/test_web_sendfile.py
+++ tests/test_web_sendfile.py
@@ -20,7 +20,7 @@ def test_using_gzip_if_header_present_and_file_available(loop) -> None:
 
     filepath = mock.create_autospec(Path, spec_set=True)
     filepath.name = "logo.png"
-    filepath.with_name.return_value = gz_filepath
+    filepath.with_suffix.return_value = gz_filepath
 
     file_sender = FileResponse(filepath)
     file_sender._path = filepath
@@ -41,7 +41,7 @@ def test_gzip_if_header_not_present_and_file_available(loop) -> None:
 
     filepath = mock.create_autospec(Path, spec_set=True)
     filepath.name = "logo.png"
-    filepath.with_name.return_value = gz_filepath
+    filepath.with_suffix.return_value = gz_filepath
     filepath.stat.return_value.st_size = 1024
     filepath.stat.return_value.st_mtime_ns = 1603733507222449291
 
@@ -63,7 +63,7 @@ def test_gzip_if_header_not_present_and_file_not_available(loop) -> None:
 
     filepath = mock.create_autospec(Path, spec_set=True)
     filepath.name = "logo.png"
-    filepath.with_name.return_value = gz_filepath
+    filepath.with_suffix.return_value = gz_filepath
     filepath.stat.return_value.st_size = 1024
     filepath.stat.return_value.st_mtime_ns = 1603733507222449291
 
@@ -87,7 +87,7 @@ def test_gzip_if_header_present_and_file_not_available(loop) -> None:
 
     filepath = mock.create_autospec(Path, spec_set=True)
     filepath.name = "logo.png"
-    filepath.with_name.return_value = gz_filepath
+    filepath.with_suffix.return_value = gz_filepath
     filepath.stat.return_value.st_size = 1024
     filepath.stat.return_value.st_mtime_ns = 1603733507222449291
 
