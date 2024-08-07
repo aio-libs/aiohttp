27feaceb7d1e0b2fcd9209d2505548c50366fed1
Sam Bull <git@sambull.org>
2024-08-07 18:12:15 +0100

    Update web_request.py

--- aiohttp/web_request.py
+++ aiohttp/web_request.py
@@ -814,6 +814,18 @@ class BaseRequest(MutableMapping[str, Any], HeadersMixin):
     def _cancel(self, exc: BaseException) -> None:
         set_exception(self._payload, exc)
 
+    def _finish(self) -> None:
+        if self._post is None or self.content_type != "multipart/form-data":
+            return
+
+        # NOTE: Release file descriptors for the
+        # NOTE: `tempfile.Temporaryfile`-created `_io.BufferedRandom`
+        # NOTE: instances of files sent within multipart request body
+        # NOTE: via HTTP POST request.
+        for file_name, file_field_object in self._post.items():
+            if isinstance(file_field_object, FileField):
+                file_field_object.file.close()
+
 
 class Request(BaseRequest):
     __slots__ = ("_match_info",)
