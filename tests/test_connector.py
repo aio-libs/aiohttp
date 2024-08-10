bf7f7e210e104836ee56d003bc8caf1343b243fb
J. Nick Koston <nick@koston.org>
2024-08-10 09:13:34 -0500

    only has overload for Exception, not BaseException

--- tests/test_connector.py
+++ tests/test_connector.py
@@ -1806,9 +1806,9 @@ async def test_ssl_context_creation_raises(exception: BaseException) -> None:
 
     with mock.patch.object(
         conn, "_make_ssl_context", side_effect=exception
-    ), pytest.raises(
+    ), pytest.raises(  # type: ignore[call-overload]
         exception
-    ):  # type: ignore[call-overload]
+    ):
         await conn._make_or_get_ssl_context(True)
 
     assert isinstance(await conn._make_or_get_ssl_context(True), ssl.SSLContext)
