15c68dbd721082ba12a101aa2dff888ff9e1da84
J. Nick Koston <nick@koston.org>
2024-08-10 08:23:23 -0500

    tweaks

--- aiohttp/connector.py
+++ aiohttp/connector.py
@@ -1016,8 +1016,8 @@ class TCPConnector(BaseConnector):
                 result = await self._loop.run_in_executor(None, self._make_ssl_context)
             # BaseException is used since we might get CancelledError
             except BaseException as ex:
-                set_exception(future, ex)
                 self._made_ssl_context.pop(verified)
+                set_exception(future, ex)
             else:
                 set_result(future, result)
             return result
