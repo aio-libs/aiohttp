64795f7a6cd1eca763e9e202b4a96d9541a85038
Sam Bull <git@sambull.org>
2024-09-08 23:33:26 +0100

    Update client_proto.py

--- aiohttp/client_proto.py
+++ aiohttp/client_proto.py
@@ -275,21 +275,18 @@ class ResponseHandler(BaseProtocol, DataQueue[Tuple[RawResponseMessage, StreamRe
                 # parse http messages
                 try:
                     messages, upgraded, tail = self._parser.feed_data(data)
-                except BaseException as underlying_exc:
+                except HttpProcessingError as underlying_exc:
                     if self.transport is not None:
                         # connection.release() could be called BEFORE
                         # data_received(), the transport is already
                         # closed in this case
                         self.transport.close()
                     # should_close is True after the call
-                    if isinstance(underlying_exc, HttpProcessingError):
-                        exc = HttpProcessingError(
-                            code=underlying_exc.code,
-                            message=underlying_exc.message,
-                            headers=underlying_exc.headers,
-                        )
-                    else:
-                        exc = HttpProcessingError()
+                    exc = HttpProcessingError(
+                        code=underlying_exc.code,
+                        message=underlying_exc.message,
+                        headers=underlying_exc.headers,
+                    )
                     self.set_exception(exc, underlying_exc)
                     return
 
