2c9f99e193b4ccd7f8a742c16c2eaa54cdc95721
Luke Petre <lpetre@gmail.com>
2024-09-09 03:31:34 +0100

    Use Query typedef from yarl for params (#8564)
    ## What do these changes do?
    
    The type signature for params does not match the functionality provided
    by yarl. Update the type signature to expose the full feature set of
    yarl.
    
    ## Are there changes in behavior for the user?
    
    No changes to the user, expect that type checking will now match the
    runtime functionality.
    
    ## Is it a substantial burden for the maintainers to support this?
    
    The only burden I see is that the type signature in yarl is not exported
    in the package, which means these two files should be kept in sync. I
    would recommend yarl be updated to export the type signature and aiohttp
    could use that directly.
    
    ## Related issue number
    Fixes #8563
    
    ## Checklist
    
    - [x] I think the code is well written
    - [ ] Unit tests for the changes exist
    - [ ] Documentation reflects the changes
    - [ ] If you provide code modification, please add yourself to
    `CONTRIBUTORS.txt`
      * The format is &lt;Name&gt; &lt;Surname&gt;.
      * Please keep alphabetical order, the file is sorted by names.
    - [ ] Add a new news fragment into the `CHANGES/` folder
      * name it `<issue_or_pr_num>.<type>.rst` (e.g. `588.bugfix.rst`)
      * if you don't have an issue number, change it to the pull request
        number after creating the PR
        * `.bugfix`: A bug fix for something the maintainers deemed an
          improper undesired behavior that got corrected to match
          pre-agreed expectations.
        * `.feature`: A new behavior, public APIs. That sort of stuff.
        * `.deprecation`: A declaration of future API removals and breaking
          changes in behavior.
        * `.breaking`: When something public is removed in a breaking way.
          Could be deprecated in an earlier release.
        * `.doc`: Notable updates to the documentation structure or build
          process.
        * `.packaging`: Notes for downstreams about unobvious side effects
          and tooling. Changes in the test invocation considerations and
          runtime assumptions.
        * `.contrib`: Stuff that affects the contributor experience. e.g.
          Running tests, building the docs, setting up the development
          environment.
        * `.misc`: Changes that are hard to assign to any of the above
          categories.
      * Make sure to use full sentences with correct case and punctuation,
        for example:
        ```rst
        Fixed issue with non-ascii contents in doctest text files
        -- by :user:`contributor-gh-handle`.
        ```
    
        Use the past tense or the present tense a non-imperative mood,
        referring to what's changed compared to the last released version
        of this project.
    
    ---------
    
    Co-authored-by: J. Nick Koston <nick@koston.org>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Sam Bull <aa6bs0@sambull.org>

--- aiohttp/client_reqrep.py
+++ aiohttp/client_reqrep.py
@@ -69,6 +69,7 @@ from .typedefs import (
     JSONDecoder,
     LooseCookies,
     LooseHeaders,
+    Query,
     RawHeaders,
 )
 
@@ -196,7 +197,7 @@ class ClientRequest:
         method: str,
         url: URL,
         *,
-        params: Optional[Mapping[str, str]] = None,
+        params: Query = None,
         headers: Optional[LooseHeaders] = None,
         skip_auto_headers: Optional[Iterable[str]] = None,
         data: Any = None,
