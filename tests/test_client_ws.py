aca1cba6ad0651fde3583ecacb02ac2fab92e6be
Sam Bull <git@sambull.org>
2024-09-18 15:58:10 +0100

    Update test_client_ws.py

--- tests/test_client_ws.py
+++ tests/test_client_ws.py
@@ -8,9 +8,7 @@ from unittest import mock
 import pytest
 
 import aiohttp
-from aiohttp import client, hdrs
-from aiohttp.client_exceptions import ServerDisconnectedError
-from aiohttp.client_ws import ClientWSTimeout
+from aiohttp import ClientConnectionResetError, ClientWSTimeout, ServerDisconnectedError, client, hdrs
 from aiohttp.http import WS_KEY
 from aiohttp.streams import EofStream
 from aiohttp.test_utils import make_mocked_coro
