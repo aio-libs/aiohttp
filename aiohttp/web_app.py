41fbaeac0c88c04abdef9f9e638b43bdd3900406
J. Nick Koston <nick@koston.org>
2024-09-22 13:12:22 -0500

    Small cleanups to Application._handle

--- aiohttp/web_app.py
+++ aiohttp/web_app.py
@@ -386,22 +386,19 @@ class Application(MutableMapping[Union[str, AppKey[Any]], Any]):
         match_info.add_app(self)
         match_info.freeze()
 
-        resp = None
         request._match_info = match_info
-        expect = request.headers.get(hdrs.EXPECT)
-        if expect:
+
+        if request.headers.get(hdrs.EXPECT):
             resp = await match_info.expect_handler(request)
             await request.writer.drain()
+            return resp
 
-        if resp is None:
-            handler = match_info.handler
+        handler = match_info.handler
 
-            if self._run_middlewares:
-                handler = _build_middlewares(handler, match_info.apps)
+        if self._run_middlewares:
+            handler = _build_middlewares(handler, match_info.apps)
 
-            resp = await handler(request)
-
-        return resp
+        return await handler(request)
 
     def __call__(self) -> "Application":
         """gunicorn compatibility"""
