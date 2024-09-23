195e3f2efdd3a8edf91b414e7f29cd461a73ce57
J. Nick Koston <nick@koston.org>
2024-09-23 09:54:02 -0500

    Revert "Revert "Switch to using `yarl.URL.absolute` over `yarl.URL.is_absolute()` (#9171)""
    This reverts commit 7a4c5273043a500b20f958e3d30e7dc196c9b669.

--- aiohttp/test_utils.py
+++ aiohttp/test_utils.py
@@ -164,7 +164,7 @@ class BaseTestServer(ABC, Generic[_Request]):
         assert self._root is not None
         url = URL(path)
         if not self.skip_url_asserts:
-            assert not url.is_absolute()
+            assert not url.absolute
             return self._root.join(url)
         else:
             return URL(str(self._root) + str(path))
