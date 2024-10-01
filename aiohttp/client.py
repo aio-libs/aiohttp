2c847f4bf09087ad7259e211d92d5a3ec6a5cba1
J. Nick Koston <nick@koston.org>
2024-10-01 13:58:30 -0500

    [PR #9372/a8555be backport][3.11] Only prepare proxy headers for a request if a proxy is set (#9377)

--- aiohttp/client.py
+++ aiohttp/client.py
@@ -515,7 +515,6 @@ class ClientSession:
 
         # Merge with default headers and transform to CIMultiDict
         headers = self._prepare_headers(headers)
-        proxy_headers = self._prepare_headers(proxy_headers)
 
         try:
             url = self._build_url(str_or_url)
@@ -531,7 +530,10 @@ class ClientSession:
             for i in skip_auto_headers:
                 skip_headers.add(istr(i))
 
-        if proxy is not None:
+        if proxy is None:
+            proxy_headers = None
+        else:
+            proxy_headers = self._prepare_headers(proxy_headers)
             try:
                 proxy = URL(proxy)
             except ValueError as e:
