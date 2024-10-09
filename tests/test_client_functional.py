9b8f85daedcd9e7f2dd7bbd31e92413929e51b28
Pierre-Louis Peeters <peetersp@dnb.com>
2024-10-09 16:25:04 +0200

    Make redirect URL auth take precedence over input auth in client

--- tests/test_client_functional.py
+++ tests/test_client_functional.py
@@ -2980,6 +2980,60 @@ async def test_creds_in_auth_and_redirect_url(
         assert resp.status == 200
 
 
+async def test_creds_redirect_url_creds_precedence(
+    create_server_for_url_and_handler: Callable[[URL, Handler], Awaitable[TestServer]],
+) -> None:
+    url_from = URL("http://example.com")
+    url_to = URL("http://user@example.com")
+
+    async def srv(request: web.Request) -> web.Response:
+        assert request.host == url_from.host
+
+        if request.headers.get(hdrs.AUTHORIZATION) == "Basic dXNlcjpwYXNz":
+            raise web.HTTPMovedPermanently(url_to)
+
+        return web.Response()
+
+    server = await create_server_for_url_and_handler(url_from, srv)
+
+    etc_hosts = {
+        (url_from.host, 80): server,
+    }
+
+    class FakeResolver(AbstractResolver):
+        async def resolve(
+            self,
+            host: str,
+            port: int = 0,
+            family: socket.AddressFamily = socket.AF_INET,
+        ) -> List[ResolveResult]:
+            server = etc_hosts[(host, port)]
+            assert server.port is not None
+
+            return [
+                {
+                    "hostname": host,
+                    "host": server.host,
+                    "port": server.port,
+                    "family": socket.AF_INET,
+                    "proto": 0,
+                    "flags": socket.AI_NUMERICHOST,
+                }
+            ]
+
+        async def close(self) -> None:
+            """Dummy"""
+
+    connector = aiohttp.TCPConnector(resolver=FakeResolver(), ssl=False)
+
+    async with aiohttp.ClientSession(connector=connector) as client:
+        resp = await client.get(url_from, auth=aiohttp.BasicAuth("user", "pass"))
+        assert len(resp.history) == 1
+        assert str(resp.url) == "http://example.com"
+        assert resp.request_info.headers.get("authorization") == "Basic dXNlcjo="
+        assert resp.status == 200
+
+
 @pytest.fixture
 def create_server_for_url_and_handler(
     aiohttp_server: AiohttpServer, tls_certificate_authority: trustme.CA
