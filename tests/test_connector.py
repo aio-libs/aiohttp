d7223d8efb3518f261062def45ced36013bc350a
J. Nick Koston <nick@koston.org>
2024-10-10 12:02:05 -0500

    Fix test to sleep twice since there is now one less iteration of the event loop needed as we got rid of a task

--- tests/test_connector.py
+++ tests/test_connector.py
@@ -1104,6 +1104,7 @@ def dns_response(loop):
     async def coro():
         # simulates a network operation
         await asyncio.sleep(0)
+        await asyncio.sleep(0)
         return ["127.0.0.1"]
 
     return coro
