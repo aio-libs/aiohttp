400f1a6b0a6c38ed1ed92ac0ca514c97d3d9a899
J. Nick Koston <nick@koston.org>
2024-11-01 11:35:54 -0500

    Add benchmark for serializing headers

---
+++ tests/test_benchmarks_http_writer.py
@@ -0,0 +1,37 @@
+"""codspeed benchmarks for http writer."""
+
+import asyncio
+
+from multidict import CIMultiDict
+from pytest_codspeed import BenchmarkFixture
+
+from aiohttp import hdrs
+from aiohttp.http_writer import _serialize_headers
+
+
+def test_serialize_headers(
+    loop: asyncio.AbstractEventLoop, benchmark: BenchmarkFixture
+) -> None:
+    """Benchmark 100 _serialize_headers."""
+    status_line = "HTTP/1.1 200 OK"
+    headers = CIMultiDict(
+        {
+            hdrs.CONTENT_TYPE: "text/plain",
+            hdrs.CONTENT_LENGTH: "100",
+            hdrs.CONNECTION: "keep-alive",
+            hdrs.DATE: "Mon, 23 May 2005 22:38:34 GMT",
+            hdrs.SERVER: "Test/1.0",
+            hdrs.CONTENT_ENCODING: "gzip",
+            hdrs.VARY: "Accept-Encoding",
+            hdrs.CACHE_CONTROL: "no-cache",
+            hdrs.PRAGMA: "no-cache",
+            hdrs.EXPIRES: "0",
+            hdrs.LAST_MODIFIED: "Mon, 23 May 2005 22:38:34 GMT",
+            hdrs.ETAG: "1234567890",
+        }
+    )
+
+    @benchmark
+    def _run() -> None:
+        for _ in range(100):
+            _serialize_headers(status_line, headers)
