16cdc6938b4d13be837984dc3439e6624d409d8b
J. Nick Koston <nick@koston.org>
2024-11-03 00:31:21 -0500

    adjust

--- tests/test_benchmarks_client_ws.py
+++ tests/test_benchmarks_client_ws.py
@@ -10,17 +10,18 @@ from aiohttp.pytest_plugin import AiohttpClient
 from aiohttp.test_utils import _WSRequestContextManager
 
 
-def test_one_hundred_round_trip_websocket_text_messages(
+def test_one_thousand_round_trip_websocket_text_messages(
     loop: asyncio.AbstractEventLoop,
     aiohttp_client: AiohttpClient,
     benchmark: BenchmarkFixture,
 ) -> None:
-    """Benchmark round trip of 100 WebSocket text messages."""
+    """Benchmark round trip of 1000 WebSocket text messages."""
+    message_count = 1000
 
     async def handler(request: web.Request) -> NoReturn:
         ws = web.WebSocketResponse()
         await ws.prepare(request)
-        for _ in range(100):
+        for _ in range(message_count):
             await ws.send_str("answer")
         await ws.close()
         return ws
@@ -31,7 +32,7 @@ def test_one_hundred_round_trip_websocket_text_messages(
     async def run_websocket_benchmark() -> _WSRequestContextManager:
         client = await aiohttp_client(app)
         resp = await client.ws_connect("/")
-        for _ in range(100):
+        for _ in range(message_count):
             await resp.receive()
 
     @benchmark
