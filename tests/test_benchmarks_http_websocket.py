4e2f2b96af95536e5b63c21ea5be0024103c846b
J. Nick Koston <nick@koston.org>
2024-11-03 00:45:01 -0500

    revert bad merge

--- tests/test_benchmarks_http_websocket.py
+++ tests/test_benchmarks_http_websocket.py
@@ -1,12 +1,10 @@
 """codspeed benchmarks for http websocket."""
 
 import asyncio
-from typing import Union
 
 from pytest_codspeed import BenchmarkFixture
 
 from aiohttp import DataQueue
-from aiohttp._websocket.helpers import MSG_SIZE
 from aiohttp.base_protocol import BaseProtocol
 from aiohttp.http_websocket import (
     WebSocketReader,
@@ -43,7 +41,7 @@ class MockTransport(asyncio.Transport):
         """Swallow is_closing."""
         return False
 
-    def write(self, data: Union[bytes, bytearray, memoryview]) -> None:
+    def write(self, data: bytes) -> None:
         """Swallow writes."""
 
 
@@ -59,20 +57,6 @@ def test_send_one_hundred_websocket_text_messages(
     """Benchmark sending 100 WebSocket text messages."""
     writer = WebSocketWriter(MockProtocol(loop=loop), MockTransport())
     raw_message = b"Hello, World!" * 100
-
-    async def _send_one_hundred_websocket_text_messages() -> None:
-        for _ in range(100):
-            await writer.send_frame(raw_message, WSMsgType.TEXT)
-
-    benchmark(loop.run_until_complete(_send_one_hundred_websocket_text_messages()))
-
-
-def test_send_one_hundred_large_websocket_text_messages(
-    loop: asyncio.AbstractEventLoop, benchmark: BenchmarkFixture
-) -> None:
-    """Benchmark sending 100 WebSocket text messages."""
-    writer = WebSocketWriter(MockProtocol(loop=loop), MockTransport())
-    raw_message = b"x" * MSG_SIZE * 4
 
     async def _send_one_hundred_websocket_text_messages() -> None:
         for _ in range(100):
