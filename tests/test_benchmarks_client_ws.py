36596a14179bb4dfe2a7c6d7022de661706067aa
J. Nick Koston <nick@koston.org>
2024-11-03 00:30:19 -0500

    adjust

--- tests/test_benchmarks_client_ws.py
+++ tests/test_benchmarks_client_ws.py
@@ -1,7 +1,6 @@
-"""codspeed benchmarks for http websocket."""
+"""codspeed benchmarks for websocket client."""
 
 import asyncio
-import threading
 from typing import NoReturn
 
 from pytest_codspeed import BenchmarkFixture
@@ -11,19 +10,14 @@ from aiohttp.pytest_plugin import AiohttpClient
 from aiohttp.test_utils import _WSRequestContextManager
 
 
-def test_read_one_hundred_round_trip_websocket_text_messages(
-    aiohttp_client: AiohttpClient, benchmark: BenchmarkFixture
+def test_one_hundred_round_trip_websocket_text_messages(
+    loop: asyncio.AbstractEventLoop,
+    aiohttp_client: AiohttpClient,
+    benchmark: BenchmarkFixture,
 ) -> None:
     """Benchmark round trip of 100 WebSocket text messages."""
-    loop = asyncio.new_event_loop()
 
-    def start_background_loop() -> None:
-        asyncio.set_event_loop(loop)
-        loop.run_forever()
-
-    async def background_server() -> _WSRequestContextManager:
-        nonlocal resp
-
+    async def run_websocket_benchmark() -> _WSRequestContextManager:
         async def handler(request: web.Request) -> NoReturn:
             ws = web.WebSocketResponse()
             await ws.prepare(request)
@@ -35,23 +29,10 @@ def test_read_one_hundred_round_trip_websocket_text_messages(
         app = web.Application()
         app.router.add_route("GET", "/", handler)
         client = await aiohttp_client(app)
-        return await client.ws_connect("/")
-
-    thread = threading.Thread(target=start_background_loop, daemon=True)
-
-    thread.start()
-    resp = asyncio.run_coroutine_threadsafe(background_server(), loop).result()
-
-    async def _receive_one_hundred_websocket_text_messages() -> None:
+        resp = await client.ws_connect("/")
         for _ in range(100):
             await resp.receive()
 
-    benchmark(
-        asyncio.run_coroutine_threadsafe(
-            _receive_one_hundred_websocket_text_messages(), loop
-        ).result
-    )
-    asyncio.run_coroutine_threadsafe(resp.close(), loop).result()
-    loop.stop()
-    thread.join()
-    loop.close()
+    @benchmark
+    def _run() -> None:
+        loop.run_until_complete(run_websocket_benchmark())
