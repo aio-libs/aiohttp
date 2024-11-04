1589609f2221248e8e25908a7cffa264180b162e
J. Nick Koston <nick@koston.org>
2024-11-04 11:00:59 -0600

    more cleanups

--- tests/test_connector.py
+++ tests/test_connector.py
@@ -7,7 +7,6 @@ import socket
 import ssl
 import sys
 import uuid
-from collections import deque
 from concurrent import futures
 from contextlib import closing, suppress
 from typing import (
@@ -502,7 +501,8 @@ async def test_release_waiter_release_first(
     w1, w2 = mock.Mock(), mock.Mock()
     w1.done.return_value = False
     w2.done.return_value = False
-    conn._waiters[key] = deque([w1, w2])
+    conn._waiters[key][w1] = None
+    conn._waiters[key][w2] = None
     conn._release_waiter()
     assert w1.set_result.called
     assert not w2.set_result.called
@@ -516,7 +516,8 @@ async def test_release_waiter_skip_done_waiter(
     w1, w2 = mock.Mock(), mock.Mock()
     w1.done.return_value = True
     w2.done.return_value = False
-    conn._waiters[key] = deque([w1, w2])
+    conn._waiters[key][w1] = None
+    conn._waiters[key][w2] = None
     conn._release_waiter()
     assert not w1.set_result.called
     assert w2.set_result.called
@@ -531,8 +532,8 @@ async def test_release_waiter_per_host(
     w1, w2 = mock.Mock(), mock.Mock()
     w1.done.return_value = False
     w2.done.return_value = False
-    conn._waiters[key] = deque([w1])
-    conn._waiters[key2] = deque([w2])
+    conn._waiters[key][w1] = None
+    conn._waiters[key2][w2] = None
     conn._release_waiter()
     assert (w1.set_result.called and not w2.set_result.called) or (
         not w1.set_result.called and w2.set_result.called
