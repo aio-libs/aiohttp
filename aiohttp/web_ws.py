6fe7ed1275a9e6ab2559fe57868a710ef51beed7
J. Nick Koston <nick@koston.org>
2024-11-03 21:05:56 -0600

    Attempt to improve WebSocket receive performance
    We have a lot of checks at the end of receive that are rarely hit

--- aiohttp/web_ws.py
+++ aiohttp/web_ws.py
@@ -604,6 +604,7 @@ class WebSocketResponse(StreamResponse):
                 continue
             elif msg.type is WSMsgType.PONG and self._autoping:
                 continue
+
             return msg
 
     async def receive_str(self, *, timeout: Optional[float] = None) -> str:
