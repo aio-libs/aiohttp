4cc98ade5c732303bb92e9da03a5b22dc1e4a218
J. Nick Koston <nick@koston.org>
2024-11-07 08:58:04 -0600

    safer

--- aiohttp/web_response.py
+++ aiohttp/web_response.py
@@ -420,7 +420,7 @@ class StreamResponse(BaseClass, HeadersMixin, CookieMixin):
             # https://datatracker.ietf.org/doc/html/rfc9112#section-6.1-13
             if hdrs.TRANSFER_ENCODING in headers:
                 del headers[hdrs.TRANSFER_ENCODING]
-        elif writer.length != 0:
+        elif (writer.length if self._length_check else self.content_length) != 0:
             # https://www.rfc-editor.org/rfc/rfc9110#section-8.3-5
             headers.setdefault(hdrs.CONTENT_TYPE, "application/octet-stream")
         headers.setdefault(hdrs.DATE, rfc822_formatted_time())
