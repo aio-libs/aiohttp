9b6feaf4e948389b798c8b76c2b48a501ef2c355
J. Nick Koston <nick@koston.org>
2024-11-06 20:52:15 -0600

    Add benchmark for sending client requests

--- tests/test_benchmarks_client_request.py
+++ tests/test_benchmarks_client_request.py
@@ -2,6 +2,7 @@
 
 import asyncio
 from http.cookies import Morsel
+from typing import Union
 
 from pytest_codspeed import BenchmarkFixture
 from yarl import URL
@@ -65,3 +66,43 @@ def test_create_client_request_with_headers(
             chunked=None,
             expect100=False,
         )
+
+
+def test_send_client_request_one_hundred(
+    loop: asyncio.AbstractEventLoop, benchmark: BenchmarkFixture
+) -> None:
+    url = URL("http://python.org")
+    req = ClientRequest("get", url, loop=loop)
+
+    class MockTransport(asyncio.Transport):
+        """Mock transport for testing that do no real I/O."""
+
+        def is_closing(self) -> bool:
+            """Swallow is_closing."""
+            return False
+
+        def write(self, data: Union[bytes, bytearray, memoryview]) -> None:
+            """Swallow writes."""
+
+    class MockProtocol(asyncio.BaseProtocol):
+
+        def __init__(self):
+            self.transport = MockTransport()
+
+    class MockConnection:
+        def __init__(self):
+            self.transport = None
+            self.protocol = MockProtocol()
+
+        def get_extra_info(self, key):
+            return None
+
+    conn = MockConnection()
+
+    async def send_requests() -> None:
+        for _ in range(100):
+            await req.send(conn)
+
+    @benchmark
+    def _run() -> None:
+        loop.run_until_complete(send_requests())
