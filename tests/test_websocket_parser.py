ea1c084d9f49e91feb2e92fea307260ad3f47376
J. Nick Koston <nick@koston.org>
2024-11-07 13:18:07 -0600

    [3.11] Fix WebSocket reader flow control size calculation for multi-byte data (#9686)

--- tests/test_websocket_parser.py
+++ tests/test_websocket_parser.py
@@ -613,9 +613,6 @@ def test_flow_control_binary(
     assert protocol._reading_paused is True
 
 
-@pytest.mark.xfail(
-    reason="Flow control is currently broken on master branch; see #9686"
-)
 def test_flow_control_multi_byte_text(
     protocol: BaseProtocol,
     out_low_limit: aiohttp.FlowControlDataQueue[WSMessage],
