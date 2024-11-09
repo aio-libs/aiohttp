fd09b483e59c28ca009221cf4794ea280e4ab7e4
J. Nick Koston <nick@koston.org>
2024-11-09 11:58:21 +0000

    [PR #9722/fbf555c backport][3.11] Simplify header and method lookups (#9724)

--- aiohttp/client.py
+++ aiohttp/client.py
@@ -92,10 +92,10 @@ from .cookiejar import CookieJar
 from .helpers import (
     _SENTINEL,
     DEBUG,
+    EMPTY_BODY_METHODS,
     BasicAuth,
     TimeoutHandle,
     get_env_proxy_for_url,
-    method_must_be_empty_body,
     sentinel,
     strip_auth_from_url,
 )
@@ -706,7 +706,7 @@ class ClientSession:
                     assert conn.protocol is not None
                     conn.protocol.set_response_params(
                         timer=timer,
-                        skip_payload=method_must_be_empty_body(method),
+                        skip_payload=method in EMPTY_BODY_METHODS,
                         read_until_eof=read_until_eof,
                         auto_decompress=auto_decompress,
                         read_timeout=real_timeout.sock_read,
