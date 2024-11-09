2603139dc268a60ee8c63eb2fbfa1490e227a382
J. Nick Koston <nick@koston.org>
2024-11-09 08:46:11 +0000

    Client middleware PoC

--- aiohttp/client.py
+++ aiohttp/client.py
@@ -451,6 +451,7 @@ class ClientSession:
         auto_decompress: Optional[bool] = None,
         max_line_size: Optional[int] = None,
         max_field_size: Optional[int] = None,
+        middleware: Optional["Middleware"] = None,
     ) -> ClientResponse:
         # NOTE: timeout clamps existing connect and read timeouts.  We cannot
         # set the default to None because we need to detect if the user wants
@@ -658,6 +659,8 @@ class ClientSession:
                         max_field_size=max_field_size,
                     )
 
+                    # TODO: pre-request middleware
+
                     try:
                         try:
                             resp = await req.send(conn)
@@ -762,6 +765,8 @@ class ClientSession:
                         params = {}
                         resp.release()
                         continue
+
+                    # TODO: post request middleware
 
                     break
 
