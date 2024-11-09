8ad1e2be53f5286c465ebcf6e57cd424db44b989
J. Nick Koston <nick@koston.org>
2024-11-09 18:43:34 +0000

    [PR #9741/4be6dc4 backport][3.11] Fix race getting an unused port when there are multiple test runners (#9744)

--- tests/test_test_utils.py
+++ tests/test_test_utils.py
@@ -375,9 +375,16 @@ async def test_client_context_manager_response(method, app, loop) -> None:
                 assert "Hello, world" in text
 
 
-async def test_custom_port(loop, app, aiohttp_unused_port) -> None:
-    port = aiohttp_unused_port()
-    client = _TestClient(TestServer(app, loop=loop, port=port), loop=loop)
+async def test_custom_port(
+    loop: asyncio.AbstractEventLoop,
+    app: web.Application,
+    unused_port_socket: socket,
+) -> None:
+    sock = unused_port_socket
+    port = sock.getsockname()[1]
+    client = _TestClient(
+        TestServer(app, port=port, socket_factory=lambda *args, **kwargs: sock)
+    )
     await client.start_server()
 
     assert client.server.port == port
