30d969d9b3e9b7b1b5091a03ba869d9163e6ec3a
Emmanuel Okedele <emmanuel@coefficient.ai>
2024-11-09 12:13:34 +0000

    Fixes issues with test_web_urldispatcher when it encounters OS AF_UNIX too long

--- tests/test_web_urldispatcher.py
+++ tests/test_web_urldispatcher.py
@@ -547,14 +547,15 @@ async def test_access_compressed_file_as_symlink(
 
 
 async def test_access_special_resource(
-    tmp_path_factory: pytest.TempPathFactory, aiohttp_client: AiohttpClient
+    unix_sockname: str, aiohttp_client: AiohttpClient
 ) -> None:
     """Test access to non-regular files is forbidden using a UNIX domain socket."""
     if not getattr(socket, "AF_UNIX", None):
         pytest.skip("UNIX domain sockets not supported")
 
-    tmp_path = tmp_path_factory.mktemp("special")
-    my_special = tmp_path / "sock"
+    my_special = pathlib.Path(unix_sockname)
+    tmp_path = my_special.parent
+    
     my_socket = socket.socket(socket.AF_UNIX)
     my_socket.bind(str(my_special))
     assert my_special.is_socket()
