b3e53571300e592a48ed9a6220eaf7170ab371a3
J. Nick Koston <nick@koston.org>
2024-11-11 01:42:12 +0000

    [PR #9784/dbad41c backport][3.10] Add benchmark for reading WebSocket payloads larger than 2**16 (#9785)

--- tests/test_benchmarks_http_websocket.py
+++ tests/test_benchmarks_http_websocket.py
@@ -9,11 +9,33 @@ from aiohttp import DataQueue
 from aiohttp.base_protocol import BaseProtocol
 from aiohttp.http_websocket import (
     MSG_SIZE,
+    PACK_LEN3,
     WebSocketReader,
     WebSocketWriter,
     WSMessage,
     WSMsgType,
 )
+
+
+def test_read_large_binary_websocket_messages(
+    loop: asyncio.AbstractEventLoop, benchmark: BenchmarkFixture
+) -> None:
+    """Read one hundred large binary websocket messages."""
+    queue: DataQueue[WSMessage] = DataQueue(loop=loop)
+    reader = WebSocketReader(queue, max_msg_size=2**18)
+
+    # PACK3 has a minimum message length of 2**16 bytes.
+    message = b"x" * ((2**16) + 1)
+    msg_length = len(message)
+    first_byte = 0x80 | 0 | WSMsgType.BINARY.value
+    header = PACK_LEN3(first_byte, 127, msg_length)
+    raw_message = header + message
+    feed_data = reader.feed_data
+
+    @benchmark
+    def _run() -> None:
+        for _ in range(100):
+            feed_data(raw_message)
 
 
 def test_read_one_hundred_websocket_text_messages(
