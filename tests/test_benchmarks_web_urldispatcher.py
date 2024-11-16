8b3794e5bf962d30b4a5035762557218df5b027d
Andrew Svetlov <andrew.svetlov@gmail.com>
2024-11-16 15:35:10 +0100

    Add benchmarks for github url resolving emulation (#9925)
    I think github API is a representative example for possible route table.

--- tests/test_benchmarks_web_urldispatcher.py
+++ tests/test_benchmarks_web_urldispatcher.py
@@ -1,7 +1,11 @@
 """codspeed benchmarks for the URL dispatcher."""
 
 import asyncio
+import json
 import pathlib
+import random
+import string
+from pathlib import Path
 from typing import NoReturn
 from unittest import mock
 
@@ -46,11 +50,12 @@ def test_resolve_root_route(
     app = web.Application()
     app.router.add_route("GET", "/", handler)
     app.freeze()
+    router = app.router
     request = _mock_request(method="GET", path="/")
 
     async def run_url_dispatcher_benchmark() -> None:
         for _ in range(resolve_count):
-            await app._router.resolve(request)
+            await router.resolve(request)
 
     @benchmark
     def _run() -> None:
@@ -67,11 +72,12 @@ def test_resolve_static_root_route(
     app = web.Application()
     app.router.add_static("/", pathlib.Path(aiohttp.__file__).parent)
     app.freeze()
+    router = app.router
     request = _mock_request(method="GET", path="/")
 
     async def run_url_dispatcher_benchmark() -> None:
         for _ in range(resolve_count):
-            await app._router.resolve(request)
+            await router.resolve(request)
 
     @benchmark
     def _run() -> None:
@@ -92,11 +98,12 @@ def test_resolve_single_fixed_url_with_many_routes(
     for count in range(250):
         app.router.add_route("GET", f"/api/server/dispatch/{count}/update", handler)
     app.freeze()
+    router = app.router
     request = _mock_request(method="GET", path="/api/server/dispatch/1/update")
 
     async def run_url_dispatcher_benchmark() -> None:
         for _ in range(resolve_count):
-            await app._router.resolve(request)
+            await router.resolve(request)
 
     @benchmark
     def _run() -> None:
@@ -116,6 +123,7 @@ def test_resolve_multiple_fixed_url_with_many_routes(
     for count in range(250):
         app.router.add_route("GET", f"/api/server/dispatch/{count}/update", handler)
     app.freeze()
+    router = app.router
 
     requests = [
         _mock_request(method="GET", path=f"/api/server/dispatch/{count}/update")
@@ -124,7 +132,7 @@ def test_resolve_multiple_fixed_url_with_many_routes(
 
     async def run_url_dispatcher_benchmark() -> None:
         for request in requests:
-            await app._router.resolve(request)
+            await router.resolve(request)
 
     @benchmark
     def _run() -> None:
@@ -152,12 +160,13 @@ def test_resolve_multiple_level_fixed_url_with_many_routes(
     for url in urls:
         app.router.add_route("GET", url, handler)
     app.freeze()
+    router = app.router
 
     requests = [_mock_request(method="GET", path=url) for url in urls]
 
     async def run_url_dispatcher_benchmark() -> None:
         for request in requests:
-            await app._router.resolve(request)
+            await router.resolve(request)
 
     @benchmark
     def _run() -> None:
@@ -178,6 +187,7 @@ def test_resolve_dynamic_resource_url_with_many_routes(
         app.router.add_route("GET", f"/api/server/other/{count}/update", handler)
     app.router.add_route("GET", "/api/server/dispatch/{customer}/update", handler)
     app.freeze()
+    router = app.router
 
     requests = [
         _mock_request(method="GET", path=f"/api/server/dispatch/{customer}/update")
@@ -186,7 +196,121 @@ def test_resolve_dynamic_resource_url_with_many_routes(
 
     async def run_url_dispatcher_benchmark() -> None:
         for request in requests:
-            await app._router.resolve(request)
+            await router.resolve(request)
+
+    @benchmark
+    def _run() -> None:
+        loop.run_until_complete(run_url_dispatcher_benchmark())
+
+
+def test_resolve_gitapi(
+    loop: asyncio.AbstractEventLoop,
+    benchmark: BenchmarkFixture,
+) -> None:
+    """Resolve DynamicResource for simulated github API.
+
+    The benchmark uses OpenAPI generated info for github.
+    To update the local data file please run the following command:
+    $ curl https://raw.githubusercontent.com/github/rest-api-description/refs/heads/main/descriptions/api.github.com/api.github.com.json | jq ".paths | keys" > github-urls.json
+    """
+
+    async def handler(request: web.Request) -> NoReturn:
+        assert False
+
+    here = Path(__file__).parent
+    with (here / "github-urls.json").open() as f:
+        urls = json.load(f)
+
+    app = web.Application()
+    for url in urls:
+        app.router.add_get(url, handler)
+    app.freeze()
+    router = app.router
+
+    # PR reviews API was selected absolutely voluntary.
+    # It is not any special but sits somewhere in the middle of the urls list.
+    # If anybody has better idea please suggest.
+
+    alnums = string.ascii_letters + string.digits
+
+    requests = []
+    for i in range(250):
+        owner = "".join(random.sample(alnums, 10))
+        repo = "".join(random.sample(alnums, 10))
+        pull_number = random.randint(0, 250)
+        requests.append(
+            _mock_request(
+                method="GET", path=f"/repos/{owner}/{repo}/pulls/{pull_number}/reviews"
+            )
+        )
+
+    async def run_url_dispatcher_benchmark() -> None:
+        for request in requests:
+            await router.resolve(request)
+
+    @benchmark
+    def _run() -> None:
+        loop.run_until_complete(run_url_dispatcher_benchmark())
+
+
+def test_resolve_gitapi_subapps(
+    loop: asyncio.AbstractEventLoop,
+    benchmark: BenchmarkFixture,
+) -> None:
+    """Resolve DynamicResource for simulated github API, grouped in subapps.
+
+    The benchmark uses OpenAPI generated info for github.
+    To update the local data file please run the following command:
+    $ curl https://raw.githubusercontent.com/github/rest-api-description/refs/heads/main/descriptions/api.github.com/api.github.com.json | jq ".paths | keys" > github-urls.json
+    """
+
+    async def handler(request: web.Request) -> NoReturn:
+        assert False
+
+    here = Path(__file__).parent
+    with (here / "github-urls.json").open() as f:
+        urls = json.load(f)
+
+    subapps = {
+        "gists": web.Application(),
+        "orgs": web.Application(),
+        "projects": web.Application(),
+        "repos": web.Application(),
+        "teams": web.Application(),
+        "user": web.Application(),
+        "users": web.Application(),
+    }
+
+    app = web.Application()
+    for url in urls:
+        parts = url.split("/")
+        subapp = subapps.get(parts[1], app)
+        subapp.router.add_get(url, handler)
+    for key, subapp in subapps.items():
+        app.add_subapp("/" + key, subapp)
+    app.freeze()
+    router = app.router
+
+    # PR reviews API was selected absolutely voluntary.
+    # It is not any special but sits somewhere in the middle of the urls list.
+    # If anybody has better idea please suggest.
+
+    alnums = string.ascii_letters + string.digits
+
+    requests = []
+    for i in range(250):
+        owner = "".join(random.sample(alnums, 10))
+        repo = "".join(random.sample(alnums, 10))
+        pull_number = random.randint(0, 250)
+        requests.append(
+            _mock_request(
+                method="GET", path=f"/repos/{owner}/{repo}/pulls/{pull_number}/reviews"
+            )
+        )
+
+    async def run_url_dispatcher_benchmark() -> None:
+        for request in requests:
+            await router.resolve(request)
 
     @benchmark
     def _run() -> None:
