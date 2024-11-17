4c1a2fdf1227c6620f773bc5ad07140bb6d4faf7
J. Nick Koston <nick@koston.org>
2024-11-17 12:33:21 -0600

    Add benchmarks for creating web responses

--- tests/test_benchmarks_web_response.py
+++ tests/test_benchmarks_web_response.py
@@ -14,13 +14,10 @@ def test_simple_web_response(
     """Benchmark creating 100 simple web.Response."""
     response_count = 100
 
-    async def run_web_response_benchmark() -> None:
-        for _ in range(response_count):
-            web.Response()
-
     @benchmark
     def _run() -> None:
-        loop.run_until_complete(run_web_response_benchmark())
+        for _ in range(response_count):
+            web.Response()
 
 
 def test_web_response_with_headers(
@@ -35,13 +32,10 @@ def test_web_response_with_headers(
         "Date": "Sun, 01 Aug 2021 12:00:00 GMT",
     }
 
-    async def run_web_response_benchmark() -> None:
-        for _ in range(response_count):
-            web.Response(headers=headers)
-
     @benchmark
     def _run() -> None:
-        loop.run_until_complete(run_web_response_benchmark())
+        for _ in range(response_count):
+            web.Response(headers=headers)
 
 
 def test_web_response_with_bytes_body(
@@ -51,13 +45,10 @@ def test_web_response_with_bytes_body(
     """Benchmark creating 100 web.Response with bytes."""
     response_count = 100
 
-    async def run_web_response_benchmark() -> None:
-        for _ in range(response_count):
-            web.Response(body=b"Hello, World!")
-
     @benchmark
     def _run() -> None:
-        loop.run_until_complete(run_web_response_benchmark())
+        for _ in range(response_count):
+            web.Response(body=b"Hello, World!")
 
 
 def test_web_response_with_text_body(
@@ -67,13 +58,10 @@ def test_web_response_with_text_body(
     """Benchmark creating 100 web.Response with text."""
     response_count = 100
 
-    async def run_web_response_benchmark() -> None:
-        for _ in range(response_count):
-            web.Response(text="Hello, World!")
-
     @benchmark
     def _run() -> None:
-        loop.run_until_complete(run_web_response_benchmark())
+        for _ in range(response_count):
+            web.Response(text="Hello, World!")
 
 
 def test_simple_web_stream_response(
@@ -83,10 +71,7 @@ def test_simple_web_stream_response(
     """Benchmark creating 100 simple web.StreamResponse."""
     response_count = 100
 
-    async def run_web_response_benchmark() -> None:
-        for _ in range(response_count):
-            web.StreamResponse()
-
     @benchmark
     def _run() -> None:
-        loop.run_until_complete(run_web_response_benchmark())
+        for _ in range(response_count):
+            web.StreamResponse()
