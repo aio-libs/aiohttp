fad44f6bc96e4f54a5b8e7e30d1e9e01a021bec9
J. Nick Koston <nick@koston.org>
2024-12-01 15:08:30 -0600

    Fix request count check for BadHttpMethod (#10076)

--- aiohttp/web_server.py
+++ aiohttp/web_server.py
@@ -76,6 +76,8 @@ class Server(Generic[_Request]):
         self._loop = asyncio.get_running_loop()
         self._connections: Dict[RequestHandler[_Request], asyncio.Transport] = {}
         self._kwargs = kwargs
+        # requests_count is the number of requests being processed by the server
+        # for the lifetime of the server.
         self.requests_count = 0
         self.request_handler = handler
         self.request_factory = request_factory or self._make_request  # type: ignore[assignment]
