76cdf9482d8ba69ef72198010741f6c65deff763
J. Nick Koston <nick@koston.org>
2024-12-03 14:37:40 -0600

    add benchmark with no sendfile

--- tests/test_benchmarks_web_fileresponse.py
+++ tests/test_benchmarks_web_fileresponse.py
@@ -33,3 +33,31 @@ def test_simple_web_file_response(
     @benchmark
     def _run() -> None:
         loop.run_until_complete(run_file_resonse_benchmark())
+
+
+def test_simple_web_file_sendfile_fallback_response(
+    loop: asyncio.AbstractEventLoop,
+    aiohttp_client: AiohttpClient,
+    benchmark: BenchmarkFixture,
+) -> None:
+    """Benchmark creating 100 simple web.FileResponse without sendfile."""
+    response_count = 100
+    filepath = pathlib.Path(__file__).parent / "sample.txt"
+
+    async def handler(request: web.Request) -> web.FileResponse:
+        transport = request.transport
+        transport._sendfile_compatible = False
+        return web.FileResponse(path=filepath)
+
+    app = web.Application()
+    app.router.add_route("GET", "/", handler)
+
+    async def run_file_resonse_benchmark() -> None:
+        client = await aiohttp_client(app)
+        for _ in range(response_count):
+            await client.get("/")
+        await client.close()
+
+    @benchmark
+    def _run() -> None:
+        loop.run_until_complete(run_file_resonse_benchmark())
