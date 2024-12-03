4397fd8646861c35f1c6d1b61ff26c0f3e1c5a36
J. Nick Koston <nick@koston.org>
2024-12-03 13:43:58 -0600

    Add a benchmark for web.FileResponse

---
+++ tests/test_benchmarks_web_fileresponse.py
@@ -0,0 +1,35 @@
+"""codspeed benchmarks for the web responses."""
+
+import asyncio
+import pathlib
+
+from pytest_codspeed import BenchmarkFixture
+
+from aiohttp import web
+from aiohttp.pytest_plugin import AiohttpClient
+
+
+def test_simple_web_response(
+    loop: asyncio.AbstractEventLoop,
+    aiohttp_client: AiohttpClient,
+    benchmark: BenchmarkFixture,
+) -> None:
+    """Benchmark creating 100 simple web.Response."""
+    response_count = 100
+    filepath = pathlib.Path(__file__).parent / "sample.txt"
+
+    async def handler(request: web.Request) -> web.FileResponse:
+        return web.FileResponse(path=filepath)
+
+    app = web.Application()
+    app.router.add_route("GET", "/", handler)
+
+    async def run_file_resonse_benchmark() -> None:
+        client = await aiohttp_client(app)
+        for _ in range(response_count):
+            await client.get("/")
+        await client.close()
+
+    @benchmark
+    def _run() -> None:
+        loop.run_until_complete(run_file_resonse_benchmark())
