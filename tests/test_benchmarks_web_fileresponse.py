bf3db1df84c50ebe1ded9fd98adb3096a6612e2f
J. Nick Koston <nick@koston.org>
2024-12-05 09:20:35 -0600

    Add 304 benchmark for FileResponse
    related issue #10112

--- tests/test_benchmarks_web_fileresponse.py
+++ tests/test_benchmarks_web_fileresponse.py
@@ -3,8 +3,10 @@
 import asyncio
 import pathlib
 
+from multidict import CIMultiDict
 from pytest_codspeed import BenchmarkFixture
 
+import aiohttp
 from aiohttp import web
 from aiohttp.pytest_plugin import AiohttpClient
 
@@ -24,7 +26,7 @@ def test_simple_web_file_response(
     app = web.Application()
     app.router.add_route("GET", "/", handler)
 
-    async def run_file_resonse_benchmark() -> None:
+    async def run_file_response_benchmark() -> None:
         client = await aiohttp_client(app)
         for _ in range(response_count):
             await client.get("/")
@@ -32,7 +34,7 @@ def test_simple_web_file_response(
 
     @benchmark
     def _run() -> None:
-        loop.run_until_complete(run_file_resonse_benchmark())
+        loop.run_until_complete(run_file_response_benchmark())
 
 
 def test_simple_web_file_sendfile_fallback_response(
@@ -53,7 +55,7 @@ def test_simple_web_file_sendfile_fallback_response(
     app = web.Application()
     app.router.add_route("GET", "/", handler)
 
-    async def run_file_resonse_benchmark() -> None:
+    async def run_file_response_benchmark() -> None:
         client = await aiohttp_client(app)
         for _ in range(response_count):
             await client.get("/")
@@ -61,4 +63,44 @@ def test_simple_web_file_sendfile_fallback_response(
 
     @benchmark
     def _run() -> None:
-        loop.run_until_complete(run_file_resonse_benchmark())
+        loop.run_until_complete(run_file_response_benchmark())
+
+
+def test_simple_web_file_response_not_modified(
+    loop: asyncio.AbstractEventLoop,
+    aiohttp_client: AiohttpClient,
+    benchmark: BenchmarkFixture,
+) -> None:
+    """Benchmark creating 100 simple web.FileResponse."""
+    response_count = 100
+    filepath = pathlib.Path(__file__).parent / "sample.txt"
+
+    async def handler(request: web.Request) -> web.FileResponse:
+        return web.FileResponse(path=filepath)
+
+    app = web.Application()
+    app.router.add_route("GET", "/", handler)
+
+    async def make_last_modified_header() -> CIMultiDict:
+        client = await aiohttp_client(app)
+        resp = await client.get("/")
+        last_modified = resp.headers["Last-Modified"]
+        headers = CIMultiDict({"If-Modified-Since": last_modified})
+        return headers
+
+    async def run_file_response_benchmark(
+        headers: CIMultiDict,
+    ) -> aiohttp.ClientResponse:
+        client = await aiohttp_client(app)
+        for _ in range(response_count):
+            resp = await client.get("/", headers=headers)
+
+        await client.close()
+        return resp
+
+    headers = loop.run_until_complete(make_last_modified_header())
+
+    @benchmark
+    def _run() -> None:
+        resp = loop.run_until_complete(run_file_response_benchmark(headers))
+        assert resp.status == 304
