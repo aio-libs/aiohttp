85b21f7520661a2218f0119ded73365cb85cfca1
J. Nick Koston <nick@koston.org>
2024-12-06 10:49:09 -0600

    naming

--- aiohttp/http_writer.py
+++ aiohttp/http_writer.py
@@ -27,8 +27,8 @@ __all__ = ("StreamWriter", "HttpVersion", "HttpVersion10", "HttpVersion11")
 
 MIN_PAYLOAD_FOR_WRITELINES = 2048
 IS_PY313_BEFORE_313_2 = (3, 13, 0) <= sys.version_info < (3, 13, 2)
-IS_BEFORE_312_9 = sys.version_info < (3, 12, 9)
-SKIP_WRITELINES = IS_PY313_BEFORE_313_2 or IS_BEFORE_312_9
+IS_PY_BEFORE_312_9 = sys.version_info < (3, 12, 9)
+SKIP_WRITELINES = IS_PY313_BEFORE_313_2 or IS_PY_BEFORE_312_9
 # writelines is not safe for use
 # on Python 3.12+ until 3.12.9
 # on Python 3.13+ until 3.13.2
