be953436cbd00c4a2b02f834332df727150fd20e
J. Nick Koston <nick@koston.org>
2024-12-08 12:06:31 -0600

    Raise ValueError when setting StreamResponse.last_modified to an unsupported type
    Currently this would silently fail and discard the new
    value.
    
    fixes #10143

--- aiohttp/web_response.py
+++ aiohttp/web_response.py
@@ -267,6 +267,9 @@ class StreamResponse(BaseClass, HeadersMixin, CookieMixin):
             )
         elif isinstance(value, str):
             self._headers[hdrs.LAST_MODIFIED] = value
+        else:
+            msg = f"Unsupported type for last_modified: {type(value).__name__}"
+            raise ValueError(msg)
 
     @property
     def etag(self) -> Optional[ETag]:
