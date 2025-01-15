68c23eaac6d515bd9c1adcf648bc7ae6ab26c166
Sam Bull <git@sambull.org>
2025-01-15 14:54:09 +0000

    Update tests/test_client_functional.py

--- tests/test_client_functional.py
+++ tests/test_client_functional.py
@@ -186,9 +186,7 @@ async def test_keepalive_after_empty_body_status_stream_response(
     assert cnt_conn_reuse == 1
 
 
-pytest.mark.xfail(reason="Connection should be reused (#10325)")
-
-
+@pytest.mark.xfail(reason="Connection should be reused (#10325)")
 async def test_keepalive_post_empty_bytes(aiohttp_client: AiohttpClient) -> None:
     async def handler(request: web.Request) -> web.Response:
         return web.Response(body=await request.read())
