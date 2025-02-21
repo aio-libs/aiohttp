e3357c1123d28336f5a41d8d9aaf8238806e22dd
Tim Menninger <tmenninger22@gmail.com>
2025-02-21 13:19:27 -0800

    Expose setsockopt in TCPConnector API (#10474)
    (cherry picked from commit 7379a866bb9fa41986a01aa6fff2a73a210b66de)

--- aiohttp/connector.py
+++ aiohttp/connector.py
@@ -19,6 +19,7 @@ from typing import (
     DefaultDict,
     Deque,
     Dict,
+    Iterable,
     Iterator,
     List,
     Literal,
@@ -59,6 +60,11 @@ from .helpers import (
     set_result,
 )
 from .resolver import DefaultResolver
+
+if sys.version_info >= (3, 12):
+    from collections.abc import Buffer
+else:
+    Buffer = Union[bytes, bytearray, "memoryview[int]", "memoryview[bytes]"]
 
 if TYPE_CHECKING:
     import ssl
@@ -828,6 +834,8 @@ class TCPConnector(BaseConnector):
                            the happy eyeballs algorithm, set to None.
     interleave - “First Address Family Count” as defined in RFC 8305
     loop - Optional event loop.
+    tcp_sockopts - List of tuples of sockopts applied to underlying
+                   socket
     """
 
     allowed_protocol_schema_set = HIGH_LEVEL_SCHEMA_SET | frozenset({"tcp"})
@@ -853,6 +861,7 @@ class TCPConnector(BaseConnector):
         timeout_ceil_threshold: float = 5,
         happy_eyeballs_delay: Optional[float] = 0.25,
         interleave: Optional[int] = None,
+        tcp_sockopts: Iterable[Tuple[int, int, Union[int, Buffer]]] = [],
     ):
         super().__init__(
             keepalive_timeout=keepalive_timeout,
@@ -879,6 +888,7 @@ class TCPConnector(BaseConnector):
         self._happy_eyeballs_delay = happy_eyeballs_delay
         self._interleave = interleave
         self._resolve_host_tasks: Set["asyncio.Task[List[ResolveResult]]"] = set()
+        self._tcp_sockopts = tcp_sockopts
 
     def close(self) -> Awaitable[None]:
         """Close all ongoing DNS calls."""
@@ -1120,6 +1130,8 @@ class TCPConnector(BaseConnector):
                     interleave=self._interleave,
                     loop=self._loop,
                 )
+                for sockopt in self._tcp_sockopts:
+                    sock.setsockopt(*sockopt)
                 connection = await self._loop.create_connection(
                     *args, **kwargs, sock=sock
                 )
