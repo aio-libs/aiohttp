4064a0b41d82f394a6cddf413d6337fd528b0580
J. Nick Koston <nick@koston.org>
2025-03-15 14:32:04 -1000

    add test for leak

--- tests/test_leaks.py
+++ tests/test_leaks.py
@@ -4,6 +4,10 @@ import sys
 
 
 def test_client_response_does_not_leak_on_server_disconnected_error() -> None:
+    """Test that ClientResponse is collected after server disconnects.
+
+    https://github.com/aio-libs/aiohttp/issues/10535
+    """
     leak_test_script = pathlib.Path(__file__).parent.joinpath(
         "isolated", "check_for_client_response_leak.py"
     )
