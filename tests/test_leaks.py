756af74c0b8c0ec1fc1d6aa6ba0a98d653b038ad
J. Nick Koston <nick@koston.org>
2025-03-16 08:53:35 -1000

    Break cyclic references at connection close when there was a traceback (#10556)
    <!-- Thank you for your contribution! -->
    
    ## What do these changes do?
    
    Clears the exception on the `DataQueue` and `WebSocketDataQueue` when
    the connection is closed to break cyclic references.
    
    ## Are there changes in behavior for the user?
    
    bugfix
    
    ## Is it a substantial burden for the maintainers to support this?
    
    no
    
    ## Related issue number
    
    fixes #10535
    
    ## Checklist
    
    - [ ] I think the code is well written
    - [ ] Unit tests for the changes exist
    - [ ] Documentation reflects the changes
    - [ ] If you provide code modification, please add yourself to
    `CONTRIBUTORS.txt`
      * The format is &lt;Name&gt; &lt;Surname&gt;.
      * Please keep alphabetical order, the file is sorted by names.
    - [ ] Add a new news fragment into the `CHANGES/` folder
      * name it `<issue_or_pr_num>.<type>.rst` (e.g. `588.bugfix.rst`)
      * if you don't have an issue number, change it to the pull request
        number after creating the PR
        * `.bugfix`: A bug fix for something the maintainers deemed an
          improper undesired behavior that got corrected to match
          pre-agreed expectations.
        * `.feature`: A new behavior, public APIs. That sort of stuff.
        * `.deprecation`: A declaration of future API removals and breaking
          changes in behavior.
        * `.breaking`: When something public is removed in a breaking way.
          Could be deprecated in an earlier release.
        * `.doc`: Notable updates to the documentation structure or build
          process.
        * `.packaging`: Notes for downstreams about unobvious side effects
          and tooling. Changes in the test invocation considerations and
          runtime assumptions.
        * `.contrib`: Stuff that affects the contributor experience. e.g.
          Running tests, building the docs, setting up the development
          environment.
        * `.misc`: Changes that are hard to assign to any of the above
          categories.
      * Make sure to use full sentences with correct case and punctuation,
        for example:
        ```rst
        Fixed issue with non-ascii contents in doctest text files
        -- by :user:`contributor-gh-handle`.
        ```
    
        Use the past tense or the present tense a non-imperative mood,
        referring to what's changed compared to the last released version
        of this project.

---
+++ tests/test_leaks.py
@@ -0,0 +1,25 @@
+import pathlib
+import platform
+import subprocess
+import sys
+
+import pytest
+
+IS_PYPY = platform.python_implementation() == "PyPy"
+
+
+@pytest.mark.skipif(IS_PYPY, reason="gc.DEBUG_LEAK not available on PyPy")
+def test_client_response_does_not_leak_on_server_disconnected_error() -> None:
+    """Test that ClientResponse is collected after server disconnects.
+
+    https://github.com/aio-libs/aiohttp/issues/10535
+    """
+    leak_test_script = pathlib.Path(__file__).parent.joinpath(
+        "isolated", "check_for_client_response_leak.py"
+    )
+
+    with subprocess.Popen(
+        [sys.executable, "-u", str(leak_test_script)],
+        stdout=subprocess.PIPE,
+    ) as proc:
+        assert proc.wait() == 0, "ClientResponse leaked"
