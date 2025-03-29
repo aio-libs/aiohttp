1efe72f2f8b8051e231136102bd86aeae33da700
layday <layday@protonmail.com>
2025-03-30 01:04:57 +0200

    Replace `asyncio.iscoroutinefunction` with `inspect.iscoroutinefunction`
    `asyncio.iscoroutinefunction` was deprecated in https://github.com/python/cpython/pull/122875.

--- aiohttp/web_urldispatcher.py
+++ aiohttp/web_urldispatcher.py
@@ -4,6 +4,7 @@ import base64
 import functools
 import hashlib
 import html
+import inspect
 import keyword
 import os
 import re
@@ -174,7 +175,7 @@ class AbstractRoute(abc.ABC):
         if expect_handler is None:
             expect_handler = _default_expect_handler
 
-        assert asyncio.iscoroutinefunction(
+        assert inspect.iscoroutinefunction(
             expect_handler
         ), f"Coroutine is expected, got {expect_handler!r}"
 
@@ -182,7 +183,7 @@ class AbstractRoute(abc.ABC):
         if not HTTP_METHOD_RE.match(method):
             raise ValueError(f"{method} is not allowed HTTP method")
 
-        if asyncio.iscoroutinefunction(handler):
+        if inspect.iscoroutinefunction(handler):
             pass
         elif isinstance(handler, type) and issubclass(handler, AbstractView):
             pass
--- aiohttp/worker.py
+++ aiohttp/worker.py
@@ -1,6 +1,7 @@
 """Async gunicorn worker for aiohttp.web"""
 
 import asyncio
+import inspect
 import os
 import re
 import signal
@@ -68,7 +69,7 @@ class GunicornWebWorker(base.Worker):  # type: ignore[misc,no-any-unimported]
         runner = None
         if isinstance(self.wsgi, Application):
             app = self.wsgi
-        elif asyncio.iscoroutinefunction(self.wsgi):
+        elif inspect.iscoroutinefunction(self.wsgi):
             wsgi = await self.wsgi()
             if isinstance(wsgi, web.AppRunner):
                 runner = wsgi
