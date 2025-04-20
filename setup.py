cb9ac7d0e1e88e7f137b78e4e36238c0e4bdf16a
Michał Górny <mgorny@gentoo.org>
2025-04-20 20:56:23 +0200

    Support using system llhttp library (#10759)
    Add a new `USE_SYSTEM_DEPS` environment variable that can be used
    to enable building against the system install of llhttp library rather
    than the vendored one.
    
    This is implemented using the `pkgconfig` package to query llhttp
    install via pkg-config.  The package is only used when `USE_SYSTEM_DEPS`
    is enabled, however due to limitations of `pyproject.toml` it is listed
    unconditionally as a build dependency.  That said, flake8 claims that
    the dependency is not listed and I don't really know how to solve that:
    
    ```
    setup.py:36:5: I900 'pkgconfig' not listed as a requirement
    ```
    
    I have also updated `aiohttp/_cparser.pxd` to reference the `llhttp.h`
    header via its name rather than full path.  I have poisoned the system
    header to confirm that the correct header is used both when building
    against the system library and the vendored library.

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ if sys.version_info < (3, 9):
     raise RuntimeError("aiohttp 4.x requires Python 3.9+")
 
 
+USE_SYSTEM_DEPS: bool = bool(os.environ.get("USE_SYSTEM_DEPS"))
 NO_EXTENSIONS: bool = bool(os.environ.get("AIOHTTP_NO_EXTENSIONS"))
 HERE = pathlib.Path(__file__).parent
 IS_GIT_REPO = (HERE / ".git").exists()
@@ -17,7 +18,11 @@ if sys.implementation.name != "cpython":
     NO_EXTENSIONS = True
 
 
-if IS_GIT_REPO and not (HERE / "vendor/llhttp/README.md").exists():
+if (
+    not USE_SYSTEM_DEPS
+    and IS_GIT_REPO
+    and not (HERE / "vendor/llhttp/README.md").exists()
+):
     print("Install submodules when building from git clone", file=sys.stderr)
     print("Hint:", file=sys.stderr)
     print("  git submodule update --init", file=sys.stderr)
@@ -26,6 +31,27 @@ if IS_GIT_REPO and not (HERE / "vendor/llhttp/README.md").exists():
 
 # NOTE: makefile cythonizes all Cython modules
 
+if USE_SYSTEM_DEPS:
+    import shlex
+
+    import pkgconfig
+
+    llhttp_sources = []
+    llhttp_kwargs = {
+        "extra_compile_args": shlex.split(pkgconfig.cflags("libllhttp")),
+        "extra_link_args": shlex.split(pkgconfig.libs("libllhttp")),
+    }
+else:
+    llhttp_sources = [
+        "vendor/llhttp/build/c/llhttp.c",
+        "vendor/llhttp/src/native/api.c",
+        "vendor/llhttp/src/native/http.c",
+    ]
+    llhttp_kwargs = {
+        "define_macros": [("LLHTTP_STRICT_MODE", 0)],
+        "include_dirs": ["vendor/llhttp/build"],
+    }
+
 extensions = [
     Extension("aiohttp._websocket.mask", ["aiohttp/_websocket/mask.c"]),
     Extension(
@@ -33,12 +59,9 @@ extensions = [
         [
             "aiohttp/_http_parser.c",
             "aiohttp/_find_header.c",
-            "vendor/llhttp/build/c/llhttp.c",
-            "vendor/llhttp/src/native/api.c",
-            "vendor/llhttp/src/native/http.c",
+            *llhttp_sources,
         ],
-        define_macros=[("LLHTTP_STRICT_MODE", 0)],
-        include_dirs=["vendor/llhttp/build"],
+        **llhttp_kwargs,
     ),
     Extension("aiohttp._http_writer", ["aiohttp/_http_writer.c"]),
     Extension("aiohttp._websocket.reader_c", ["aiohttp/_websocket/reader_c.c"]),
