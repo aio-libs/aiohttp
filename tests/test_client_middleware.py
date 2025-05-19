8aa271a8a762ace0ba58cf6095da4c8b6ac6fd49
J. Nick Koston <nick@koston.org>
2025-05-19 12:11:27 -0400

    Fix flakey middleware connection reuse test

--- tests/test_client_middleware.py
+++ tests/test_client_middleware.py
@@ -847,11 +847,12 @@ async def test_client_middleware_retry_reuses_connection(
             retry_count = 0
             while True:
                 self.attempt_count += 1
+                response = await handler(request)
                 if retry_count == 0:
                     retry_count += 1
-                    await handler(request)
+                    response.close()  # Close the response to release the connection
                     continue
-                return await handler(request)
+                return response
 
     app = web.Application()
     app.router.add_get("/", handler)
