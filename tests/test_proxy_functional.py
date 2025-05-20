7fe35da3a5e02d63ca1dde1b45fd1a471445f42e
J. Nick Koston <nick@koston.org>
2025-05-20 13:17:50 -0400

    Fix flakey tests to ensure connector cleanup and resource management
    The proxy tests was a bit flakey. Make sure it cleans up as well as the connector tests

--- tests/test_proxy_functional.py
+++ tests/test_proxy_functional.py
@@ -257,14 +257,18 @@ async def test_uvloop_secure_https_proxy(
     """Ensure HTTPS sites are accessible through a secure proxy without warning when using uvloop."""
     conn = aiohttp.TCPConnector()
     sess = aiohttp.ClientSession(connector=conn)
-    url = URL("https://example.com")
+    try:
+        url = URL("https://example.com")
 
-    async with sess.get(url, proxy=secure_proxy_url, ssl=client_ssl_ctx) as response:
-        assert response.status == 200
-
-    await sess.close()
-    await conn.close()
-    await asyncio.sleep(0.1)
+        async with sess.get(
+            url, proxy=secure_proxy_url, ssl=client_ssl_ctx
+        ) as response:
+            assert response.status == 200
+    finally:
+        await sess.close()
+        await conn.close()
+        await asyncio.sleep(0)
+        await asyncio.sleep(0.1)
 
 
 @pytest.fixture
