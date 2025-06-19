4872fce3426119e63e1a892c39b474786dafddac
KGuillaume-chaps <kguillaume@chapsvision.com>
2025-06-20 00:36:51 +0200

    Add support for ZSTD compression (#11161)

--- aiohttp/client_reqrep.py
+++ aiohttp/client_reqrep.py
@@ -44,7 +44,7 @@ from .client_exceptions import (
     InvalidURL,
     ServerFingerprintMismatch,
 )
-from .compression_utils import HAS_BROTLI
+from .compression_utils import HAS_BROTLI, HAS_ZSTD
 from .formdata import FormData
 from .hdrs import CONTENT_TYPE
 from .helpers import (
@@ -105,7 +105,15 @@ _CONTAINS_CONTROL_CHAR_RE = re.compile(r"[^-!#$%&'*+.^_`|~0-9a-zA-Z]")
 
 
 def _gen_default_accept_encoding() -> str:
-    return "gzip, deflate, br" if HAS_BROTLI else "gzip, deflate"
+    encodings = [
+        "gzip",
+        "deflate",
+    ]
+    if HAS_BROTLI:
+        encodings.append("br")
+    if HAS_ZSTD:
+        encodings.append("zstd")
+    return ", ".join(encodings)
 
 
 @frozen_dataclass_decorator
