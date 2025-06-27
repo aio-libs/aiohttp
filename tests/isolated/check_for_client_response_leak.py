3efd8f6cf6dc41b34a9058622c2cab621885de6d
Sam Bull <git@sambull.org>
2025-06-27 15:49:09 +0100

    Cherry-pick some pytest-asyncio fixes

--- tests/isolated/check_for_client_response_leak.py
+++ tests/isolated/check_for_client_response_leak.py
@@ -1,10 +1,11 @@
 import asyncio
 import contextlib
 import gc
+import socket
 import sys
 
 from aiohttp import ClientError, ClientSession, web
-from aiohttp.test_utils import get_unused_port_socket
+from aiohttp.test_utils import REUSE_ADDRESS
 
 gc.set_debug(gc.DEBUG_LEAK)
 
@@ -18,30 +19,30 @@ async def main() -> None:
         return web.Response()
 
     app.router.add_get("/stream", stream_handler)
-    sock = get_unused_port_socket("127.0.0.1")
-    port = sock.getsockname()[1]
-
-    runner = web.AppRunner(app)
-    await runner.setup()
-    site = web.SockSite(runner, sock)
-    await site.start()
-
-    session = ClientSession()
-
-    async def fetch_stream(url: str) -> None:
-        """Fetch a stream and read a few bytes from it."""
-        with contextlib.suppress(ClientError):
-            await session.get(url)
-
-    client_task = asyncio.create_task(fetch_stream(f"http://localhost:{port}/stream"))
-    await client_task
-    gc.collect()
-    client_response_present = any(
-        type(obj).__name__ == "ClientResponse" for obj in gc.garbage
-    )
-    await session.close()
-    await runner.cleanup()
-    sys.exit(1 if client_response_present else 0)
+    with socket.create_server(("127.0.0.1", 0), reuse_port=REUSE_ADDRESS) as sock:
+        port = sock.getsockname()[1]
+    
+        runner = web.AppRunner(app)
+        await runner.setup()
+        site = web.SockSite(runner, sock)
+        await site.start()
+    
+        session = ClientSession()
+    
+        async def fetch_stream(url: str) -> None:
+            """Fetch a stream and read a few bytes from it."""
+            with contextlib.suppress(ClientError):
+                await session.get(url)
+    
+        client_task = asyncio.create_task(fetch_stream(f"http://localhost:{port}/stream"))
+        await client_task
+        gc.collect()
+        client_response_present = any(
+            type(obj).__name__ == "ClientResponse" for obj in gc.garbage
+        )
+        await session.close()
+        await runner.cleanup()
+        sys.exit(1 if client_response_present else 0)
 
 
 asyncio.run(main())
