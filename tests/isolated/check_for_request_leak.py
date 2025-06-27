54e1820df6dd9474d3f653b9458a4f14edd1db48
Sam Bull <git@sambull.org>
2025-06-27 15:51:07 +0100

    Update check_for_request_leak.py

--- tests/isolated/check_for_request_leak.py
+++ tests/isolated/check_for_request_leak.py
@@ -1,10 +1,11 @@
 import asyncio
 import gc
+import socket
 import sys
 from typing import NoReturn
 
 from aiohttp import ClientSession, web
-from aiohttp.test_utils import get_unused_port_socket
+from aiohttp.test_utils import REUSE_ADDRESS
 
 gc.set_debug(gc.DEBUG_LEAK)
 
@@ -17,24 +18,24 @@ async def main() -> None:
         assert False
 
     app.router.add_route("GET", "/json", handler)
-    sock = get_unused_port_socket("127.0.0.1")
-    port = sock.getsockname()[1]
-
-    runner = web.AppRunner(app)
-    await runner.setup()
-    site = web.SockSite(runner, sock)
-    await site.start()
-
-    async with ClientSession() as session:
-        async with session.get(f"http://127.0.0.1:{port}/json") as resp:
-            await resp.read()
-
-    # Give time for the cancelled task to be collected
-    await asyncio.sleep(0.5)
-    gc.collect()
-    request_present = any(type(obj).__name__ == "Request" for obj in gc.garbage)
-    await session.close()
-    await runner.cleanup()
+    with socket.create_server(("127.0.0.1", 0), reuse_port=REUSE_ADDRESS) as sock:
+        port = sock.getsockname()[1]
+    
+        runner = web.AppRunner(app)
+        await runner.setup()
+        site = web.SockSite(runner, sock)
+        await site.start()
+    
+        async with ClientSession() as session:
+            async with session.get(f"http://127.0.0.1:{port}/json") as resp:
+                await resp.read()
+    
+        # Give time for the cancelled task to be collected
+        await asyncio.sleep(0.5)
+        gc.collect()
+        request_present = any(type(obj).__name__ == "Request" for obj in gc.garbage)
+        await session.close()
+        await runner.cleanup()
     sys.exit(1 if request_present else 0)
 
 
