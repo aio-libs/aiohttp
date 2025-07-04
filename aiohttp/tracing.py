ee4e9e47c0c78758702e371053d6dd5f23eac4a7
Sam Bull <git@sambull.org>
2025-07-04 12:52:24 +0100

    Update tracing.py

--- aiohttp/tracing.py
+++ aiohttp/tracing.py
@@ -1,5 +1,5 @@
 from types import SimpleNamespace
-from typing import TYPE_CHECKING, Any, Awaitable, Generic, Protocol, TypeVar, overload
+from typing import TYPE_CHECKING, Any, Generic, Protocol, TypeVar, overload
 
 from aiosignal import Signal
 from multidict import CIMultiDict
