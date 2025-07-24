229dde8f20f8afc8218f48de7bf57cd230a97489
Sam Bull <git@sambull.org>
2025-07-24 20:46:25 +0100

    Fix backwards compatibility for .write_bytes() (#11336)
    (cherry picked from commit 63c4d0da685736b4041ed8fffd59b832ba477039)

--- aiohttp/client_reqrep.py
+++ aiohttp/client_reqrep.py
@@ -1326,7 +1326,7 @@ class ClientRequest:
         self,
         writer: AbstractStreamWriter,
         conn: "Connection",
-        content_length: Optional[int],
+        content_length: Optional[int] = None,
     ) -> None:
         """
         Write the request body to the connection stream.
