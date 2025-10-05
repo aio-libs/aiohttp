baf4fcde90564c10a45af56dbaccdd5028e95171
Sam Bull <git@sambull.org>
2025-10-05 21:14:30 +0100

    Re-generated pickled cookie for python 3.14 compatibility (#11533) (#11558)
    (cherry picked from commit 281479aac518304751c7a608993615df7c505549)
    
    ---------
    
    Co-authored-by: Kirill <ajiadb9@bgd.team>

--- tests/test_connector.py
+++ tests/test_connector.py
@@ -492,10 +492,11 @@ async def test_release(loop, key) -> None:
     conn._acquired_per_host[key].add(proto)
 
     conn._release(key, proto)
+    loop_time = loop.time()
     assert conn._release_waiter.called
     assert conn._cleanup_handle is not None
     assert conn._conns[key][0][0] == proto
-    assert conn._conns[key][0][1] == pytest.approx(loop.time(), abs=0.1)
+    assert conn._conns[key][0][1] == pytest.approx(loop_time, abs=0.1)
     assert not conn._cleanup_closed_transports
     await conn.close()
 
@@ -1604,10 +1605,11 @@ async def test_release_not_started(loop) -> None:
     key = 1
     conn._acquired.add(proto)
     conn._release(key, proto)
+    loop_time = loop.time()
     # assert conn._conns == {1: [(proto, 10)]}
     rec = conn._conns[1]
     assert rec[0][0] == proto
-    assert rec[0][1] == pytest.approx(loop.time(), abs=0.05)
+    assert rec[0][1] == pytest.approx(loop_time, abs=0.05)
     assert not proto.close.called
     await conn.close()
 
