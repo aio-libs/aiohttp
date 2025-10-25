11a923220c08c2f9133d47253f586d847a40e3e7
Sam Bull <git@sambull.org>
2025-10-25 12:34:47 +0100

    Update client_reqrep.py

--- aiohttp/client_reqrep.py
+++ aiohttp/client_reqrep.py
@@ -59,16 +59,12 @@ from .http import (
 from .streams import StreamReader
 from .typedefs import DEFAULT_JSON_DECODER, JSONDecoder, Query, RawHeaders
 
-if TYPE_CHECKING:
+try:
     import ssl
     from ssl import SSLContext
-else:
-    try:
-        import ssl
-        from ssl import SSLContext
-    except ImportError:  # pragma: no cover
-        ssl = None  # type: ignore[assignment]
-        SSLContext = object  # type: ignore[misc,assignment]
+except ImportError:  # pragma: no cover
+    ssl = None  # type: ignore[assignment]
+    SSLContext = object  # type: ignore[misc,assignment]
 
 
 __all__ = ("ClientRequest", "ClientResponse", "RequestInfo", "Fingerprint")
