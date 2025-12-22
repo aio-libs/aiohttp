535e161b27187a49bba326d555bfb8f3a98708cf
Aditya Nath <adityanath@MacBookPro.home>
2025-12-21 19:51:07 -0800

    Fixing test for Continuation frame without intial frame

--- tests/test_websocket_parser.py
+++ tests/test_websocket_parser.py
@@ -236,12 +236,11 @@ def test_parse_frame_header_control_frame(
         parser.parse_frame(struct.pack("!BB", 0b00001000, 0b00000000))
 
 
-@pytest.mark.xfail()
 def test_parse_frame_header_new_data_err(
     out: WebSocketDataQueue, parser: PatchableWebSocketReader
 ) -> None:
     with pytest.raises(WebSocketError):
-        parser.parse_frame(struct.pack("!BB", 0b000000000, 0b00000000))
+        parser._feed_data(struct.pack("!BB", 0b000000000, 0b00000000))
 
 
 def test_parse_frame_header_payload_size(
