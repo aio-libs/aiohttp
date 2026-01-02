480c768fc3eac21ce5536b1d2b4e812a6672cfa1
Sam Bull <git@sambull.org>
2026-01-02 03:07:41 +0000

    Fixing test for Continuation frame without intial frame (#11862) (#11881)
    ---------
    
    
    (cherry picked from commit 86b30e3e60efff8881fab92ecd54ed93dbaf6ac4)
    
    Co-authored-by: Aditya Nath <928.aditya@gmail.com>

--- tests/test_websocket_parser.py
+++ tests/test_websocket_parser.py
@@ -231,13 +231,11 @@ def test_parse_frame_header_control_frame(
         raise out.exception()
 
 
-@pytest.mark.xfail()
-def test_parse_frame_header_new_data_err(
-    out: WebSocketDataQueue, parser: PatchableWebSocketReader
-) -> None:
-    with pytest.raises(WebSocketError):
-        parser.parse_frame(struct.pack("!BB", 0b000000000, 0b00000000))
-        raise out.exception()
+def test_parse_frame_header_new_data_err(parser: PatchableWebSocketReader) -> None:
+    with pytest.raises(WebSocketError) as msg:
+        parser._feed_data(struct.pack("!BB", 0b00000000, 0b00000000))
+    assert msg.value.code == WSCloseCode.PROTOCOL_ERROR
+    assert str(msg.value) == "Continuation frame for non started message"
 
 
 def test_parse_frame_header_payload_size(
