58c992779380fd95501889c3f8ed997c4e178321
Sam Bull <git@sambull.org>
2026-01-11 19:06:46 +0000

    Update http_parser.py

--- aiohttp/http_parser.py
+++ aiohttp/http_parser.py
@@ -163,9 +163,8 @@ class HeadersParser:
                 while continuation:
                     header_length += len(line)
                     if header_length > self.max_field_size:
-                        raise LineTooLong(
-                            b"request header field: " + bname, self.max_field_size
-                        )
+                        l = bname + b": " + b"".join(bvalue_lst)
+                        raise LineTooLong(l[:100] + b"...", self.max_field_size)
                     bvalue_lst.append(line)
 
                     # next line
