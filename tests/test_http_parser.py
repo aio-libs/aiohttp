6728c394dd518716dace8cf90b6e3e5a711fb7c6
Sam Bull <git@sambull.org>
2026-01-12 15:48:19 +0000

    Update test_http_parser.py

--- tests/test_http_parser.py
+++ tests/test_http_parser.py
@@ -769,7 +769,9 @@ def test_max_header_combined_size(parser: HttpRequestParser) -> None:
 async def test_max_trailer_size(parser: HttpRequestParser, size: int) -> None:
     value = b"t" * size
     text = (
-        b"GET /test HTTP/1.1\r\nTransfer-Encoding: chunked\r\n\r\n4000\r\n"
+        b"GET /test HTTP/1.1\r\nTransfer-Encoding: chunked\r\n\r\n"
+        + hex(4000).encode()
+        + b"\r\n"
         + b"b" * 4000
         + b"\r\n0\r\ntest: "
         + value
