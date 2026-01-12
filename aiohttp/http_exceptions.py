0c2e9da51126238a421568eb7c5b53e5b5d17b36
Sam Bull <git@sambull.org>
2026-01-12 18:49:19 +0000

    Add max_headers parameter (#11955) (#11959) (#11960)
    (cherry picked from commit ed6440ca49ef4907ab9d99ba7e329aab702b7173)
    (cherry picked from commit 30ec25f8a58c5dc3f8fdb3eec31f555eeaabd30a)

--- aiohttp/http_exceptions.py
+++ aiohttp/http_exceptions.py
@@ -80,11 +80,12 @@ class DecompressSizeError(PayloadEncodingError):
 
 class LineTooLong(BadHttpMessage):
     def __init__(
-        self, line: str, limit: str = "Unknown", actual_size: str = "Unknown"
+        self,
+        line: Union[str, bytes],
+        limit: Union[str, int] = "Unknown",
+        actual_size: str = "Unknown",
     ) -> None:
-        super().__init__(
-            f"Got more than {limit} bytes ({actual_size}) when reading {line}."
-        )
+        super().__init__(f"Got more than {limit} bytes when reading: {line!r}.")
         self.args = (line, limit, actual_size)
 
 
