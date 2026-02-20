4a85db0890fce48ec9893e14f350c9f8f273ffb6
Sam Bull <git@sambull.org>
2026-02-20 22:32:53 +0000

    Fix multipart injection (#12104)
    Co-authored-by: mingi jung <mingijung.grape@gmail.com>

--- tests/test_formdata.py
+++ tests/test_formdata.py
@@ -75,12 +75,18 @@ async def test_formdata_textio_charset(buf: bytearray, writer: StreamWriter) -> 
     assert b"\x93\xfa\x96{" in buf
 
 
-def test_invalid_formdata_content_type() -> None:
+@pytest.mark.parametrize("val", (0, 0.1, {}, [], b"foo"))
+def test_invalid_type_formdata_content_type(val: object) -> None:
     form = FormData()
-    invalid_vals = [0, 0.1, {}, [], b"foo"]
-    for invalid_val in invalid_vals:
-        with pytest.raises(TypeError):
-            form.add_field("foo", "bar", content_type=invalid_val)  # type: ignore[arg-type]
+    with pytest.raises(TypeError):
+        form.add_field("foo", "bar", content_type=val)  # type: ignore[arg-type]
+
+
+@pytest.mark.parametrize("val", ("\r", "\n", "a\ra\n", "a\na\r"))
+def test_invalid_value_formdata_content_type(val: str) -> None:
+    form = FormData()
+    with pytest.raises(ValueError):
+        form.add_field("foo", "bar", content_type=val)
 
 
 def test_invalid_formdata_filename() -> None:
