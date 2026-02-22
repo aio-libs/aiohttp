97096f3d62741cb62ae15b1651672e80c09279da
Sam Bull <git@sambull.org>
2026-02-22 00:42:25 +0000

    Update test_web_response.py

--- tests/test_web_response.py
+++ tests/test_web_response.py
@@ -3,7 +3,6 @@ import datetime
 import gzip
 import io
 import json
-import re
 import sys
 from collections.abc import AsyncIterator, Iterator
 from concurrent.futures import ThreadPoolExecutor
@@ -15,7 +14,6 @@ from multidict import CIMultiDict, CIMultiDictProxy, MultiDict
 from re_assert import Matches
 
 from aiohttp import HttpVersion, HttpVersion10, HttpVersion11, hdrs, web
-from aiohttp.abc import AbstractStreamWriter
 from aiohttp.helpers import ETag
 from aiohttp.http_writer import StreamWriter, _serialize_headers
 from aiohttp.multipart import BodyPartReader, MultipartWriter
