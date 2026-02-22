abd78adaa82344e4946dcfabe2aae18bf1a1a382
Sam Bull <git@sambull.org>
2026-02-22 14:59:26 +0000

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -96,9 +96,14 @@ def blockbuster(request: pytest.FixtureRequest) -> Iterator[None]:
                 "aiohttp/web_urldispatcher.py", "add_static"
             )
         # save/load is not async, so we must allow this:
-        bb.functions["io.TextIOWrapper.read"].can_block_in(
-            "aiohttp/cookiejar.py", "load"
-        )
+        for func in (
+            "io.TextIOWrapper.read",
+            "io.BufferedReader.read",
+            "io.TextIOWrapper.write",
+        ):
+            bb.functions[func].can_block_in(
+                "aiohttp/cookiejar.py", "load"
+            )
         # Note: coverage.py uses locking internally which can cause false positives
         # in blockbuster when it instruments code. This is particularly problematic
         # on Windows where it can lead to flaky test failures.
