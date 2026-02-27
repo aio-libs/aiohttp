8e6be0568a4d4922eb935753cba983545002738e
Sam Bull <git@sambull.org>
2026-02-27 00:27:16 +0000

    Drop additional headers on redirect (#12146)
    (cherry picked from commit 6e8f393330f9bd6d7b24a146124ebc42eaa727b9)

--- tests/test_client_functional.py
+++ tests/test_client_functional.py
@@ -3445,6 +3445,8 @@ async def test_drop_auth_on_redirect_to_other_host(
     async def srv_to(request):
         assert request.host == url_to.host
         assert "Authorization" not in request.headers, "Header wasn't dropped"
+        assert "Proxy-Authorization" not in request.headers
+        assert "Cookie" not in request.headers
         return web.Response()
 
     server_from = await create_server_for_url_and_handler(url_from, srv_from)
@@ -3487,11 +3489,16 @@ async def test_drop_auth_on_redirect_to_other_host(
         resp = await client.get(
             url_from,
             auth=aiohttp.BasicAuth("user", "pass"),
+            headers={"Proxy-Authorization": "Basic dXNlcjpwYXNz", "Cookie": "a=b"},
         )
         assert resp.status == 200
         resp = await client.get(
             url_from,
-            headers={"Authorization": "Basic dXNlcjpwYXNz"},
+            headers={
+                "Authorization": "Basic dXNlcjpwYXNz",
+                "Proxy-Authorization": "Basic dXNlcjpwYXNz",
+                "Cookie": "a=b",
+            },
         )
         assert resp.status == 200
 
