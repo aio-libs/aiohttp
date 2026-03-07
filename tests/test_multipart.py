4113d1d235068dc13124c0f3bda868cf95409ae1
Sam Bull <git@sambull.org>
2026-03-07 16:21:31 +0000

    Update test_multipart.py

--- tests/test_multipart.py
+++ tests/test_multipart.py
@@ -85,7 +85,7 @@ class Stream(StreamReader):
     def at_eof(self) -> bool:
         return self.content.tell() == len(self.content.getbuffer())
 
-    async def readline(self) -> bytes:
+    async def readline(self, *, max_line_length: int | None = None) -> bytes:
         return self.content.readline()
 
     def unread_data(self, data: bytes) -> None:
@@ -789,7 +789,7 @@ class TestMultipartReader:
             def at_eof(self) -> bool:
                 return not self.content
 
-            async def readline(self) -> bytes:
+            async def readline(self, *, max_line_length: int | None = None) -> bytes:
                 line = b""
                 while self.content and b"\n" not in line:
                     line += self.content.pop(0)
