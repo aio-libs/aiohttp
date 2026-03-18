a57d0609b9bcc4031f69c258fe76e54508f3f746
Sam Bull <git@sambull.org>
2026-03-18 02:05:07 +0000

    Apply suggestions from code review
    Co-authored-by: Sam Bull <aa6bs0@sambull.org>

--- tests/test_http_parser.py
+++ tests/test_http_parser.py
@@ -244,7 +244,7 @@ def test_bad_header_name(
         (("",), ()),
     ),
 )
-def test_list_headers(parser: Any, hdr_vals: List[str], expected: List[str]) -> None:
+def test_list_headers(parser: HttpRequestParser, hdr_vals: tuple[str], expected: tuple[str]) -> None:
     headers = "\r\n".join(f"Foo: {v}" for v in hdr_vals)
     text = f"POST / HTTP/1.1\r\n{headers}\r\n\r\n".encode()
     messages, upgrade, tail = parser.feed_data(text)
