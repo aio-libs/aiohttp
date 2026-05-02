acb7b785404bc2d5db5bb3913f9dc2c6394c72a0
Sam Bull <git@sambull.org>
2026-05-02 15:40:02 +0100

    Apply suggestion from @Dreamsorcerer

--- tests/test_test_utils.py
+++ tests/test_test_utils.py
@@ -84,9 +84,6 @@ async def test_aiohttp_client_close_is_idempotent() -> None:
     await client.close()
 
 
-pytest.skip(allow_module_level=True)
-
-
 class TestCaseStartup(AioHTTPTestCase):
     on_startup_called: bool
 
