3c07dc23c22cb62e0545ba9c5aa4101f69b6f5a4
Sam Bull <git@sambull.org>
2026-05-19 14:17:42 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -93,7 +93,10 @@ myst_fence_as_directive = ["mermaid"]  # render ```mermaid fences as diagrams
 # TODO: Remove this option once THREAT_MODEL.md is complete.
 suppress_warnings = ["myst.xref_missing"]
 
-mermaid_params = ["--backgroundColor", "transparent"]
+mermaid_init_config = {
+    "startOnLoad": False,  # From the default config
+    "themeVariables": {"background": "transparent"}
+}
 
 intersphinx_mapping = {
     "pytest": ("http://docs.pytest.org/en/latest/", None),
