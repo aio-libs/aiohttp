fbba4de82b0bc0b532c894f4e7236838fc771c1c
taras <nomail@nomail>
2026-05-30 23:04:23 +0200

    Fix lint errors

--- aiohttp/net_helpers.py
+++ aiohttp/net_helpers.py
@@ -1,31 +1,32 @@
 from asyncio import AbstractEventLoop
 from .helpers import NO_EXTENSIONS
 
+
 if NO_EXTENSIONS:
     HAS_AIOFASTNET = False
 else:
     try:
         import aiofastnet
         HAS_AIOFASTNET = True
+
+        create_connection = aiofastnet.create_connection
+        start_tls = aiofastnet.start_tls
+        create_server = aiofastnet.create_server
+        sendfile = aiofastnet.sendfile
     except ImportError:
         HAS_AIOFASTNET = False
 
 
-if HAS_AIOFASTNET:
-    create_connection = aiofastnet.create_connection
-    start_tls = aiofastnet.start_tls
-    create_server = aiofastnet.create_server
-    sendfile = aiofastnet.sendfile
-else:
-    async def create_connection(loop: AbstractEventLoop, *args, **kwargs):
+if not HAS_AIOFASTNET:
+    async def create_connection(loop: AbstractEventLoop, *args, **kwargs): # type: ignore[no-untyped-def]
         return await loop.create_connection(*args, **kwargs)
 
-    async def start_tls(loop: AbstractEventLoop, *args, **kwargs):
+    async def start_tls(loop: AbstractEventLoop, *args, **kwargs): # type: ignore[no-untyped-def]
         return await loop.start_tls(*args, **kwargs)
 
-    async def create_server(loop: AbstractEventLoop, *args, **kwargs):
+    async def create_server(loop: AbstractEventLoop, *args, **kwargs): # type: ignore[no-untyped-def]
         return await loop.create_server(*args, **kwargs)
 
-    async def sendfile(loop: AbstractEventLoop, *args, **kwargs):
+    async def sendfile(loop: AbstractEventLoop, *args, **kwargs): # type: ignore[no-untyped-def]
         return await loop.sendfile(*args, **kwargs)
 
