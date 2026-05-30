bce6eb057c1a8b3151ad5dc065faf059a237f28a
taras <nomail@nomail>
2026-05-30 23:12:49 +0200

    Fix mypy errors

--- aiohttp/connector.py
+++ aiohttp/connector.py
@@ -1260,7 +1260,7 @@ class TCPConnector(BaseConnector):
                     and sys.version_info >= (3, 11)
                 ):
                     kwargs["ssl_shutdown_timeout"] = self._ssl_shutdown_timeout
-                return await create_connection(self._loop, *args, **kwargs, sock=sock)
+                return await create_connection(self._loop, *args, **kwargs, sock=sock) # type: ignore[no-any-return]
         except cert_errors as exc:
             raise ClientConnectorCertificateError(req.connection_key, exc) from exc
         except ssl_errors as exc:
