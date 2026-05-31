29613418283396bd9f91edb0896ef2561e274785
taras <nomail@nomail>
2026-05-31 03:27:35 +0200

    Fix flake8 complains, honestly false-positive, but let's pacify it anyway

--- aiohttp/net_helpers.py
+++ aiohttp/net_helpers.py
@@ -1,6 +1,23 @@
 import os
 from asyncio import AbstractEventLoop
 
+
+async def _create_connection(loop: AbstractEventLoop, *args, **kwargs):  # type: ignore[no-untyped-def]
+    return await loop.create_connection(*args, **kwargs)
+
+
+async def _start_tls(loop: AbstractEventLoop, *args, **kwargs):  # type: ignore[no-untyped-def]
+    return await loop.start_tls(*args, **kwargs)
+
+
+async def _create_server(loop: AbstractEventLoop, *args, **kwargs):  # type: ignore[no-untyped-def]
+    return await loop.create_server(*args, **kwargs)
+
+
+async def _sendfile(loop: AbstractEventLoop, *args, **kwargs):  # type: ignore[no-untyped-def]
+    return await loop.sendfile(*args, **kwargs)
+
+
 if os.environ.get("AIOHTTP_NO_EXTENSIONS"):
     HAS_AIOFASTNET = False
 else:
@@ -18,15 +35,7 @@ else:
 
 
 if not HAS_AIOFASTNET:
-
-    async def create_connection(loop: AbstractEventLoop, *args, **kwargs):  # type: ignore[no-untyped-def]
-        return await loop.create_connection(*args, **kwargs)
-
-    async def start_tls(loop: AbstractEventLoop, *args, **kwargs):  # type: ignore[no-untyped-def]
-        return await loop.start_tls(*args, **kwargs)
-
-    async def create_server(loop: AbstractEventLoop, *args, **kwargs):  # type: ignore[no-untyped-def]
-        return await loop.create_server(*args, **kwargs)
-
-    async def sendfile(loop: AbstractEventLoop, *args, **kwargs):  # type: ignore[no-untyped-def]
-        return await loop.sendfile(*args, **kwargs)
+    create_connection = _create_connection
+    start_tls = _start_tls
+    create_server = _create_server
+    sendfile = _sendfile
