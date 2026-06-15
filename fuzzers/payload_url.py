74b62f468c6beebae8b48c8e4e9c9c60e3bb451c
Sam Bull <git@sambull.org>
2026-06-16 00:06:55 +0100

    Create payload_url.py

---
+++ fuzzers/payload_url.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+# Copyright 2022-2025 Google LLC, 2026 aio-libs contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+from contextlib import suppress
+
+import atheris
+
+with atheris.instrument_imports():
+    from aiohttp.payload import StringPayload
+    from yarl import URL
+
+@atheris.instrument_func
+def TestOneInput(data: bytes) -> None:
+    fdp = atheris.FuzzedDataProvider(data)
+    original = fdp.ConsumeString(sys.maxsize)
+
+    with suppress(UnicodeEncodeError):
+        p = StringPayload(original)
+    with suppress(ValueError):
+        u = URL(original)
+
+if __name__ == "__main__":
+    atheris.Setup(sys.argv, TestOneInput, enable_python_coverage=True)
+    atheris.Fuzz()
