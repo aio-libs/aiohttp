58bae08b7e4831c6c184fe22233bfc19941c700b
Rodrigo Nogueira <rodrigo.b.nogueira@gmail.com>
2026-08-03 10:11:36 -0300

    Add METH_QUERY and treat QUERY as an idempotent method (#13301)

--- aiohttp/client.py
+++ aiohttp/client.py
@@ -216,7 +216,10 @@ class _WSConnectOptions(TypedDict, total=False):
 
 
 # https://www.rfc-editor.org/rfc/rfc9110#section-9.2.2
-IDEMPOTENT_METHODS = frozenset({"GET", "HEAD", "OPTIONS", "TRACE", "PUT", "DELETE"})
+# https://www.rfc-editor.org/info/rfc10008/#section-1-12
+IDEMPOTENT_METHODS = frozenset(
+    {"GET", "HEAD", "OPTIONS", "TRACE", "PUT", "DELETE", "QUERY"}
+)
 
 _RetType_co = TypeVar(
     "_RetType_co",
