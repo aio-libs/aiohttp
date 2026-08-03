d2ea0a5e62305abf91753b9a3ec56cf9a9bdd1a6
Sam Bull <git@sambull.org>
2026-08-03 02:34:07 +0100

    Apply suggestion from @Dreamsorcerer

--- aiohttp/client.py
+++ aiohttp/client.py
@@ -216,7 +216,6 @@ class _WSConnectOptions(TypedDict, total=False):
 
 
 # https://www.rfc-editor.org/rfc/rfc9110#section-9.2.2
-# QUERY is explicitly safe and idempotent:
 # https://www.rfc-editor.org/info/rfc10008/#section-1-12
 IDEMPOTENT_METHODS = frozenset(
     {"GET", "HEAD", "OPTIONS", "TRACE", "PUT", "DELETE", "QUERY"}
