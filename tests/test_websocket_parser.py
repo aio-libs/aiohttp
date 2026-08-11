f90d48bac13d8d6b107b2997813dc6c26169cfa7
Sam Bull <git@sambull.org>
2026-08-11 20:08:25 +0100

    Fix

--- tests/test_websocket_parser.py
+++ tests/test_websocket_parser.py
@@ -677,8 +677,9 @@ def test_compressed_multi_block_message(out: WebSocketDataQueue) -> None:
     )
 
     assert error is False
-    assert out._buffer[0] == WSMessageBinary(
-        data=expected, size=len(expected), extra=""
+    assert out._buffer[0] == (
+        WSMessage(WSMsgType.BINARY, expected, ""),
+        len(expected),
     )
 
 
@@ -701,8 +702,9 @@ def test_compressed_multi_member_message(out: WebSocketDataQueue) -> None:
     )
 
     assert error is False
-    assert out._buffer[0] == WSMessageBinary(
-        data=expected, size=len(expected), extra=""
+    assert out._buffer[0] == (
+        WSMessage(WSMsgType.BINARY, expected, ""),
+        len(expected),
     )
 
 
